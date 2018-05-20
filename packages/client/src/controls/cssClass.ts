c478f744ad35f09fde83c04d857ccc5239a6f506
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2018-05-20 12:53:05 +0200

    fixed class control

--- packages/client/src/controls/cssClass.ts
+++ packages/client/src/controls/cssClass.ts
@@ -64,19 +64,87 @@ export class CssClass extends BaseControl<any>
 
         else
         {
-
-            Object.keys(parameter).forEach(function (key)
+            if (parameter instanceof Binding)
             {
-                if (parameter[key] instanceof Binding)
+                parameter.onChanged(function (ev)
                 {
-                    parameter[key].onChanged(function (ev)
+
+                    if (Array.isArray(parameter))
+                        parameter = new ObservableArray(parameter);
+                    if (parameter instanceof ObservableArray)
+                        parameter.on('collectionChanged', function (arg: ObservableArrayEventArgs<any>)
+                        {
+                            arg.newItems.forEach(function (item)
+                            {
+                                if (typeof (item) == 'string')
+                                    element.classList.add(item);
+                                else if (item instanceof ParsedString)
+                                    element.classList.add(item.value);
+                                else if (item instanceof Binding)
+                                {
+                                    var oldValue = null;
+                                    item.onChanged(function (ev)
+                                    {
+                                        if (oldValue)
+                                            element.classList.remove(oldValue);
+                                        if (isPromiseLike(ev.eventArgs.value))
+                                            ev.eventArgs.value.then(function (value)
+                                            {
+                                                oldValue = value;
+                                                element.classList.add(value)
+                                            });
+                                        else
+                                        {
+                                            element.classList.add(ev.eventArgs.value);
+                                            oldValue = ev.eventArgs.value;
+                                        }
+                                    });
+                                }
+                                else
+                                    Object.keys(item).forEach(function (key)
+                                    {
+                                        if (item[key] instanceof Binding)
+                                        {
+                                            item[key].onChanged(function (ev)
+                                            {
+                                                element.classList.toggle(key, ev.eventArgs.value);
+                                            });
+                                        }
+                                        else
+                                            element.classList.toggle(key, item[key]);
+                                    })
+
+                            })
+                        }).init();
+                    else
+
+                        akala.each(parameter, function (cl, key)
+                        {
+                            if (cl instanceof Binding)
+                            {
+                                cl.onChanged(function (ev)
+                                {
+                                    element.classList.toggle(key, ev.eventArgs.value);
+                                });
+                            }
+                            else
+                                element.classList.toggle(key, cl);
+                        })
+                });
+            }
+            else
+                akala.each(parameter, function (cl, key)
+                {
+                    if (cl instanceof Binding)
                     {
-                        element.classList.toggle(key, ev.eventArgs.value);
-                    });
-                }
-                else
-                    element.classList.toggle(key, parameter[key]);
-            })
+                        cl.onChanged(function (ev)
+                        {
+                            element.classList.toggle(key, ev.eventArgs.value);
+                        });
+                    }
+                    else
+                        element.classList.toggle(key, cl);
+                })
         }
 
     }
