eb55615dfbc468421d590ae1da21ea829b562a7b
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2018-06-05 07:11:01 +0200

    added event/events control made click inherit event control

--- packages/client/src/controls/click.ts
+++ packages/client/src/controls/click.ts
@@ -1,37 +1,14 @@
 import * as di from '@akala/core'
-import { control, BaseControl } from './control'
+import { control } from './control'
 import { Promisify, Binding, isPromiseLike } from '@akala/core'
 import { IScope } from '../scope';
+import { Event } from './event';
 
 @control()
-export class Click extends BaseControl<Function>
+export class Click extends Event
 {
     constructor()
     {
-        super('click', 400)
-    }
-
-    public link(scope: IScope<any>, element: Element, parameter: Binding | Function)
-    {
-        element.addEventListener('click', function ()
-        {
-            if (parameter instanceof Binding)
-            {
-                var value = parameter.getValue();
-                if (isPromiseLike(value))
-                {
-                    value.then(function (value)
-                    {
-                        if (value instanceof Function)
-                            return scope.$inject(value);
-                    })
-                }
-                if (value instanceof Function)
-                    return scope.$inject(value);
-            }
-            else
-                return scope.$inject(<Function>parameter);
-        });
-
+        super('click')
     }
 }
