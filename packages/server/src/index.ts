d72a1c0be7ff853cad4158bc3a140a8123a0e075
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2018-07-01 08:36:36 +0200

    added getXML implementation

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -6,7 +6,8 @@ import './translator';
 export { router, wrouter, Request, Response, HttpRouter, CallbackResponse } from './router';
 export * from './helpers/mkdirp';
 
-export { Http, server, client } from '@akala/core';
+export { server, client } from '@akala/core';
+export { Http } from './http'
 import * as worker from './worker-meta'
 export { worker };
 import * as master from './master-meta';
