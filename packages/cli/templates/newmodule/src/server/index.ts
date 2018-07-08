fdfd99ceb732b238e33a9e22b7d2c964fe509490
npenin <npenin@dragon-angel.fr>
2018-07-08 13:06:02 +0200

    added newmodule templates

---
+++ packages/cli/templates/newmodule/src/server/index.ts
@@ -0,0 +1,22 @@
+import * as akala from '@akala/server';
+import { AssetRegistration } from '@akala-modules/core';
+import { EventEmitter } from 'events';
+
+akala.injectWithName(['$isModule', '$master', '$worker'], function (isModule: akala.worker.IsModule, master: akala.worker.MasterRegistration, worker: EventEmitter)
+{
+    if (isModule('@domojs/cron'))
+    {
+        worker.on('ready', function ()
+        {
+            // Called when all modules have been initialized
+        });
+        master(__filename, './master');
+
+        akala.injectWithNameAsync([AssetRegistration.name], function (va: AssetRegistration)
+        {
+            va.register('/js/tiles.js', require.resolve('../tile'));
+            va.register('/js/routes.js', require.resolve('../routes'));
+        });
+
+    }
+})()
