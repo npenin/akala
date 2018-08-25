de0951a068032448cb198acd72d319d60e08dca1
npenin <npenin@dragon-angel.fr>
2018-08-25 13:49:56 +0200

    added css control

---
+++ packages/client/src/controls/style.ts
@@ -0,0 +1,32 @@
+import * as akala from '@akala/core'
+import { control, BaseControl } from './control'
+import { Promisify, Binding } from '@akala/core'
+import { Text } from './text'
+
+function setProperty(style, value)
+{
+    if (value)
+    {
+        akala.each(value, function (value, prop)
+        {
+            if (typeof (value) != 'object')
+                style[prop] = value;
+            else
+                setProperty(style[prop], value);
+        })
+    }
+}
+
+@control()
+export class Style extends Text
+{
+    constructor()
+    {
+        super('css');
+    }
+
+    protected setValue(element: HTMLElement, value)
+    {
+        setProperty(element.style, value);
+    }
+}
