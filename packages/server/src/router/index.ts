9ee3628a750a17a73ad49c271238e81a0976d473
npenin <nicolas.penin@dragon-angel.fr>
2019-06-10 09:54:39 +0200

    added more logs on deadend

--- packages/server/src/router/index.ts
+++ packages/server/src/router/index.ts
@@ -179,7 +179,7 @@ export class HttpRouter extends Router<requestHandlerWithNext, errorHandlerWithN
                     res.end();
                     return res;
                 }
-            self.handle(req, res, function () { console.error('deadend') });
+            self.handle(req, res, function () { console.error('deadend'); console.error(req); });
         });
     }
 
