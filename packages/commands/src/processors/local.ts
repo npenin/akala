e8096066b534caf84a0f14b60aaf6b181528cd9d
npenin <nicolas.penin@dragon-angel.fr>
2019-08-29 08:08:28 +0200

    made executor more approachable

--- packages/commands/src/processors/local.ts
+++ packages/commands/src/processors/local.ts
@@ -1,12 +1,12 @@
 import { CommandProxy } from '../command';
-import { Injector } from '@akala/core';
+import { Injector, Injectable } from '@akala/core';
 import { Processor, CommandProcessor } from '../processor'
 import { Container } from '../container';
 import assert = require('assert');
 
 export class Local<T> extends CommandProcessor<T>
 {
-    public static execute<T>(command: CommandProxy<T>, container: Container<T>, param: { param: any[], [key: string]: any }): any | PromiseLike<any>
+    public static execute<T>(inject: undefined | string[], command: Injectable<any>, container: Container<T>, param: { param: any[], [key: string]: any }): any | PromiseLike<any>
     {
         if (!container)
             assert.fail('container is undefined');
@@ -14,7 +14,7 @@ export class Local<T> extends CommandProcessor<T>
         {
             var injector = new Injector(container);
             Object.keys(param).forEach((key) => injector.register(key, param[key]));
-            return injector.injectWithName<any>(command.inject || [], command.handler)(container.state);
+            return injector.injectWithName(inject || [], command)(container.state);
         }
     }
 
@@ -24,7 +24,7 @@ export class Local<T> extends CommandProcessor<T>
         if (!this.container)
             assert.fail('container is undefined');
         else
-            Local.execute(command, this.container, param);
+            Local.execute(command.inject, command.handler, this.container, param);
     }
 
     constructor(container: Container<T>)
