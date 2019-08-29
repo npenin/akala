619c3367ab54f7406117f23cedf0ccd3ea2d750a
npenin <nicolas.penin@dragon-angel.fr>
2019-08-29 08:05:57 +0200

    exposed local executor

--- packages/commands/src/processors/local.ts
+++ packages/commands/src/processors/local.ts
@@ -6,16 +6,25 @@ import assert = require('assert');
 
 export class Local<T> extends CommandProcessor<T>
 {
+    public static execute<T>(command: CommandProxy<T>, container: Container<T>, param: { param: any[], [key: string]: any }): any | PromiseLike<any>
+    {
+        if (!container)
+            assert.fail('container is undefined');
+        else
+        {
+            var injector = new Injector(container);
+            Object.keys(param).forEach((key) => injector.register(key, param[key]));
+            return injector.injectWithName<any>(command.inject || [], command.handler)(container.state);
+        }
+    }
+
+
     public process(command: CommandProxy, param: { param: any[], [key: string]: any }): any | PromiseLike<any>
     {
         if (!this.container)
             assert.fail('container is undefined');
         else
-        {
-            var injector = new Injector(this.container);
-            Object.keys(param).forEach((key) => injector.register(key, param[key]));
-            return injector.injectWithName<any>(command.inject || [], command.handler)(this.container.state);
-        }
+            Local.execute(command, this.container, param);
     }
 
     constructor(container: Container<T>)
