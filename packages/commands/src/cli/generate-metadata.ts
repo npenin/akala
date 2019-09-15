8c4108ed232d21ae8c57f809a0d11fe1ed583b0a
npenin <nicolas.penin@dragon-angel.fr>
2019-09-15 17:34:21 +0200

    fixed metadata generation

--- packages/commands/src/cli/generate-metadata.ts
+++ packages/commands/src/cli/generate-metadata.ts
@@ -3,6 +3,8 @@ import * as core from '@akala/core'
 import { Container } from "../container";
 import * as path from 'path'
 import * as fs from 'fs';
+import { jsonObject } from "../metadata";
+import { FileSystemConfiguration } from "../processors/fs-browser";
 
 async function write(output: fs.WriteStream, content: string)
 {
@@ -18,6 +20,14 @@ async function write(output: fs.WriteStream, content: string)
     })
 }
 
+function camelCase(s: string)
+{
+    return s.replace(/-[A-Z]/g, function (s)
+    {
+        return s[1].toUpperCase();
+    });
+}
+
 export default async function generate(name: string, folder?: string, outputFile?: string)
 {
     folder = folder || process.cwd();
@@ -30,12 +40,29 @@ export default async function generate(name: string, folder?: string, outputFile
 
     var meta = akala.metadata(container);
 
+    var hasFs = false;
+    await core.eachAsync(meta.commands, async function (cmd)
+    {
+        if (cmd.config.fs)
+        {
+            hasFs = true;
+        }
+    });
+
+
+    if (hasFs)
+    {
+        await write(output, 'declare type Arguments<T> = T extends ((...x: infer X) => any) ? X : never;\n');
+    }
+
     await write(output, 'declare module "@akala/commands" {\n');
     await write(output, '\texport namespace description \n\t{\n');
 
     await write(output, `\t\texport interface ${name} \n\t\t{\n`);
+
     await core.eachAsync(meta.commands, async function (cmd)
     {
+
         await write(output, `\t\t\tdispatch (cmd:'${cmd.name}'`);
         if (cmd.inject && cmd.inject.length)
         {
@@ -43,6 +70,11 @@ export default async function generate(name: string, folder?: string, outputFile
             {
                 await write(output, `, ${i}`);
             });
+        }
+        else if (cmd.config.fs)
+        {
+            let config = cmd.config.fs as jsonObject & FileSystemConfiguration;
+            await write(output, `, ...args:Arguments<typeof import('./${config.path.replace(/\\/g, '/')}').default>`);
         }
         else
             await write(output, `, ...args:any[]`);
