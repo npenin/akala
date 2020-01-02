8586640b987e92ba93af893ebf2f8cb26b569e81
npenin <nicolas.penin@dragon-angel.fr>
2020-01-02 08:26:04 +0100

    moved to vscode-jsonrpc for jsonrpc processing

--- packages/commands/src/processors/jsonrpc.ts
+++ packages/commands/src/processors/jsonrpc.ts
@@ -3,50 +3,49 @@ import { CommandNameProcessor } from '../processor'
 import { Writable, Readable } from 'stream';
 import { v4 } from 'uuid'
 import debug from 'debug'
+import * as rpc from 'vscode-jsonrpc'
+import * as rpcMessages from 'vscode-jsonrpc/lib/messages'
 
 const log = debug('akala:commands:jsonrpc')
 
 export class JsonRPC<T> extends CommandNameProcessor<T>
 {
+    private writer: rpc.StreamMessageWriter;
+    private reader?: rpc.StreamMessageReader;
+
     public process(command: string, params: { param: jsonrpcws.SerializableObject[], [key: string]: jsonrpcws.SerializableObject | jsonrpcws.SerializableObject[] | string | number })
     {
         return new Promise<void>((resolve, reject) =>
         {
             var messageId = v4();
-            var waitingReply = this.stream instanceof Readable;
+            var waitingReply = !!this.reader;
             if (waitingReply)
             {
-                this.stream.on('data', function (replyString: Buffer)
+                this.reader?.listen(function (reply: rpcMessages.ResponseMessage)
                 {
-                    try
+                    log(reply);
+                    if (reply.jsonrpc != '2.0')
+                        reject('invalid json rpc reply');
+
+
+                    if (typeof reply.id != 'undefined' && reply.id == messageId)
                     {
-                        log(replyString);
-                        var reply = JSON.parse(replyString.toString());
-                        if (reply.jsonrpc != '2.0')
-                            reject('invalid json rpc reply');
-                        if (reply.id == messageId)
-                        {
-                            if (reply.error)
-                                reject(reply.error);
-                            else
-                                resolve(reply.result);
-                        }
+                        if (reply.error)
+                            reject(reply.error);
+                        else
+                            resolve(reply.result);
                     }
-                    catch (e)
-                    {
-                        reject(e);
-                    }
-                })
+                } as rpc.DataCallback);
             }
 
-            this.stream.write(JSON.stringify({ jsonrpc: '2.0', id: messageId, method: command, params: params }), function (error)
+            try
             {
-
-                if (error)
-                    reject(error);
-                else if (!waitingReply)
-                    resolve();
-            });
+                this.writer.write({ jsonrpc: '2.0', id: messageId, method: command, params: params } as rpc.RequestMessage);
+            }
+            catch (e)
+            {
+                reject(e);
+            }
             this.stream.on('error', reject);
         });
     }
@@ -54,5 +53,8 @@ export class JsonRPC<T> extends CommandNameProcessor<T>
     constructor(private stream: Writable)
     {
         super('jsonrpc');
+        this.writer = new rpc.StreamMessageWriter(stream);
+        if (stream instanceof Readable)
+            this.reader = new rpc.StreamMessageReader(stream);
     }
 }
