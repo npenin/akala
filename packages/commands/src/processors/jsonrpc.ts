f436cb4fe8fc56ef6b1744f6e61b29ab3a40f13e
npenin <nicolas.penin@dragon-angel.fr>
2020-01-30 19:00:03 +0100

    normalized getConnection

--- packages/commands/src/processors/jsonrpc.ts
+++ packages/commands/src/processors/jsonrpc.ts
@@ -1,9 +1,40 @@
 import * as jsonrpcws from '@akala/json-rpc-ws'
 import { CommandProcessor } from '../processor'
 import { Command } from '../metadata';
+import { Container } from '../container';
+import { IDebugger } from 'debug';
 
 export class JsonRpc<T> extends CommandProcessor<T>
 {
+    public static getConnection(socket: jsonrpcws.SocketAdapter, container: Container<any>, log?: IDebugger): jsonrpcws.Connection
+    {
+        return new jsonrpcws.Connection(socket, {
+            type: 'client',
+            browser: false,
+            disconnected()
+            {
+
+            },
+            getHandler(method: string)
+            {
+                return async function (params, reply)
+                {
+                    try
+                    {
+                        var result = await container.dispatch(method, Object.assign(params ?? { param: [] }, { _trigger: 'jsonrpc' }))
+                        reply(null, result);
+                    }
+                    catch (error)
+                    {
+                        if (log)
+                            log(error);
+                        reply(error);
+                    }
+                }
+            }
+        })
+    }
+
     public process(command: Command, params: { param: jsonrpcws.SerializableObject[], [key: string]: jsonrpcws.SerializableObject | jsonrpcws.SerializableObject[] | string | number })
     {
         return new Promise<any>((resolve, reject) =>
