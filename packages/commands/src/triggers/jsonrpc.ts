c88f563e85f2e085128ced27556f28832b53b3d5
npenin <nicolas.penin@dragon-angel.fr>
2020-01-30 19:00:03 +0100

    normalized getConnection

--- packages/commands/src/triggers/jsonrpc.ts
+++ packages/commands/src/triggers/jsonrpc.ts
@@ -2,30 +2,13 @@ import { Trigger } from '../trigger'
 import { Container } from '../container';
 import debug from 'debug'
 import { Connection, SocketAdapter } from '@akala/json-rpc-ws';
+import { JsonRpc } from '../processors';
 
 
 export var trigger = new Trigger('jsonrpc', function register<T>(container: Container<T>, media: SocketAdapter)
 {
     // assert.ok(media instanceof ws.SocketAdapter, 'to be attached, the media must be an instance of @akala/json-rpc-ws.Connection');
     const log = debug('akala:commands:jsonrpcws:' + container.name)
-    new Connection(media, {
-        type: 'client', browser: false, getHandler(method: string)
-        {
-            return async function (params, reply)
-            {
-                try
-                {
-                    var result = await container.dispatch(method, Object.assign(params ?? { param: [] }, { _trigger: trigger.name }))
-                    reply(null, result);
-                }
-                catch (error)
-                {
-                    log(error);
-                    reply(error);
-                }
-            }
-        },
-        disconnected() { }
-    });
+    JsonRpc.getConnection(media, container, log);
 })
 
