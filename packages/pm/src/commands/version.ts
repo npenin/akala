2701f6e88258b4d104bbb6492d6f528e181006a8
npenin <nicolas.penin@dragon-angel.fr>
2020-02-16 10:57:36 +0100

    added version command updated command metadata

---
+++ packages/pm/src/commands/version.ts
@@ -0,0 +1,15 @@
+import State from "../state";
+import { join } from "path";
+
+
+export default async function version(this: State, packageName: string, folder: string): Promise<string>
+{
+    var path = folder || this.config.containers['pm'][0];
+
+    var packageConfig = require(join(path, 'node_modules', packageName, './package.json'));
+    delete require.cache[join(path, 'node_modules', packageName, './package.json')];
+
+    return packageConfig.version;
+}
+
+exports.default.$inject = ['param.0', 'param.1']
