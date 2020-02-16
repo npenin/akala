66dd5ea405479a4bce1335dca1930a6b17086137
npenin <nicolas.penin@dragon-angel.fr>
2020-02-16 10:32:14 +0100

    updated tests

--- packages/commands/src/test/calculator/index.ts
+++ packages/commands/src/test/calculator/index.ts
@@ -11,14 +11,14 @@ var cmd = calculator.register(new cmds.Command<state>(function increment(step: n
         step = Number(step);
     this.value += step || 1;
 }, 'increment', ['param.0']));
-cmd.config['http'] = { method: 'post', route: '/increment/:step?', inject: ['route.step'] }
+cmd.config.http = { method: 'post', route: '/increment/:step?', inject: ['route.step'] }
 
 
 cmd = calculator.register(new cmds.Command<state>(function reset()
 {
     this.value = 0;
 }, 'reset'));
-cmd.config['http'] = { method: 'post', route: '/reset' }
+cmd.config.http = { method: 'post', route: '/reset' }
 
 cmd = configure('http', { method: 'post', route: '/decrement/:step?', inject: ['$state', 'route.step'] })(
     calculator.register(new cmds.Command<state>(function decrement(state: state, step: number)
