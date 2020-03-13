f80d04722dd33ac06751167fb657a0933e10c84c
npenin <nicolas.penin@dragon-angel.fr>
2020-03-13 08:28:10 +0100

    improved update command

--- packages/pm/src/commands/update.ts
+++ packages/pm/src/commands/update.ts
@@ -12,12 +12,14 @@ import discover from './discover'
 
 export default async function update(this: State, packageName: string, folder: string, pm: description.pm): Promise<string>
 {
+    var version = await pm.dispatch('version', packageName, folder);
+
     var path = folder || this.config.containers['pm'][0];
     await npmHelper.update(packageName, path);
 
     await pm.dispatch('discover', packageName, folder)
 
-    return await pm.dispatch('version', packageName, folder);
+    return 'updated from ' + version + ' to ' + await pm.dispatch('version', packageName, folder);
 };
 
 exports.default.$inject = ['param.0', 'param.1', 'container']
