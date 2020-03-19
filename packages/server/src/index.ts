661908982a42ae97b043062247fc1d0e38d2ee69
npenin <nicolas.penin@dragon-angel.fr>
2020-03-19 19:19:37 +0100

    added connect function

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -8,9 +8,19 @@ import * as worker from './worker-meta'
 export { worker };
 import * as master from './master-meta';
 export { master };
+import * as net from 'net'
 
 export type resolve = worker.resolve;
 
 export { description } from './commands'
+
+import * as commands from '@akala/commands'
+import { Container } from '@akala/commands';
+export function connect(socket: net.Socket)
+{
+    var container = new Container('', null);
+    commands.proxy(require('../commands.json'), new commands.Processors.JsonRpc(commands.Processors.JsonRpc.getConnection(new commands.NetSocketAdapter(socket), container)));
+    return container;
+}
 
 export { Logger, logger, log } from './logger'
