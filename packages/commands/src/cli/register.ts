2d62fde697ba1ff1a5ced59ca9df26c005bba422
npenin <nicolas.penin@dragon-angel.fr>
2020-03-19 19:02:50 +0100

    added more logging added register command

---
+++ packages/commands/src/cli/register.ts
@@ -0,0 +1,81 @@
+import { Writable } from "stream";
+import fs from 'fs';
+import path from 'path';
+import { promisify } from "util";
+import * as yargs from 'yargs-parser'
+import * as os from 'os'
+
+
+export default async function register(commandsPath?: string, name?: string, force?: boolean)
+{
+    if (typeof (commandsPath) == 'undefined')
+        if (fs.existsSync('./commands.json'))
+            commandsPath = './commands.json';
+        else
+            throw new Error('please specify commands path');
+
+    var packagePath = 'package.json';
+    while (!fs.existsSync(packagePath))
+    {
+        packagePath = '../' + packagePath;
+    }
+
+    if (!fs.existsSync(packagePath))
+    {
+        var depth = process.cwd().length - process.cwd().replace('/', '').length;
+        if (os.platform() != "win32")
+            depth - 1;
+        for (; depth > 0; depth--, packagePath = '../' + packagePath)
+        {
+            if (fs.existsSync(packagePath))
+                break;
+        }
+        if (!fs.existsSync(packagePath))
+            throw new Error('Unable to find package.json file in current folder or any of its parent');
+    }
+
+    var packageFile = require(path.join(process.cwd(), packagePath));
+    var dirty = false;
+    if (typeof (name) != 'undefined' && name !== packageFile.name)
+    {
+        if (typeof (packageFile.commands) == 'string' || typeof (packageFile.commands) == 'undefined')
+        {
+            // if (commandsPath == packageFile.commands)
+            //     console.log('no change to perform; this path is already registered');
+            // else
+            // {
+            var commands: any = {};
+            if (packageFile.commands)
+                commands[packageFile.name] = packageFile.commands;
+            commands[name] = commandsPath
+            packageFile.commands = commands;
+            dirty = true;
+            // }
+        }
+        else
+        {
+            if (packageFile.commands[name] === commandsPath)
+                console.log('no change to perform; this path is already registered');
+            else if (typeof packageFile.commands[name] != 'undefined' && !force)
+                throw new Error(`${name} already is registered as commands. Please use -f flag or find another name`);
+            else
+                packageFile.commands[name] = commandsPath, dirty = true;
+        }
+    }
+    else
+    {
+        if (typeof (packageFile.commands) == 'string' || typeof packageFile.commands == 'undefined')
+        {
+            if (packageFile.commands === commandsPath)
+                console.log('no change to perform; this path is already registered');
+            else if (typeof packageFile.commands != 'undefined' && !force)
+                throw new Error(`There already is registered as commands. Please use -f flag or give it a name using --name`);
+            else
+                packageFile.commands = commandsPath, dirty = true;
+        }
+        else
+        {
+
+        }
+    }
+};
