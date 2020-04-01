4eab629a9c5959efd3c8fd0d92169771cc16dcce
npenin <nicolas.penin@dragon-angel.fr>
2020-04-01 11:14:01 +0200

    handled $stop command updated dependencies

--- packages/pm/src/commands/stop.ts
+++ packages/pm/src/commands/stop.ts
@@ -5,15 +5,17 @@ export default async function stop(this: State, name: string, container: Contain
 {
     await Promise.all(this.processes.filter(p => name == 'pm' || !name || p.name == name && p.process).map(cp =>
     {
-        return new Promise((resolve) =>
+        return new Promise(async (resolve) =>
         {
             if (cp.process && cp.running)
             {
+                if (cp.commandable && cp.resolve('$stop'))
+                    await cp.dispatch('$stop')
                 var timeout = setTimeout(function ()
                 {
                     cp.process.kill();
                 }, 5000)
-                cp.process.on('exit', (code, signal) =>
+                cp.process.on('exit', (_code, signal) =>
                 {
                     container.unregister(cp.name);
                     clearTimeout(timeout);
