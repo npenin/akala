42e87559a769ccad1d1e0d7bce3118795e430efc
npenin <nicolas.penin@dragon-angel.fr>
2020-04-29 08:55:38 +0200

    fix for part control

--- packages/client/src/controls/part.ts
+++ packages/client/src/controls/part.ts
@@ -17,21 +17,23 @@ export class Part extends BaseControl<string | { [property: string]: Binding }>
     {
         var partService = this.partService;
         if (typeof parameter != 'string')
-            if (parameter.template instanceof Binding)
-                parameter.template.onChanged(function (ev)
-                {
-                    if (parameter.controller instanceof Binding)
-                        partService.apply(function () { return { scope, element } }, { controller: parameter.controller.getValue(), template: ev.eventArgs.value }, {}, noop);
-                    else
-                        partService.apply(function () { return { scope, element } }, { controller: <any>parameter.controller, template: ev.eventArgs.value }, {}, noop);
-                });
-            else
-                if (parameter.controller instanceof Binding)
-                    partService.apply(function () { return { scope, element } }, { controller: parameter.controller.getValue(), template: parameter.template }, {}, noop);
+        {
+            if (typeof parameter != 'undefined')
+                if (parameter.template instanceof Binding)
+                    parameter.template.onChanged(function (ev)
+                    {
+                        if (parameter.controller instanceof Binding)
+                            partService.apply(function () { return { scope, element } }, { controller: parameter.controller.getValue(), template: ev.eventArgs.value }, {}, noop);
+                        else
+                            partService.apply(function () { return { scope, element } }, { controller: <any>parameter.controller, template: ev.eventArgs.value }, {}, noop);
+                    });
                 else
-                    partService.apply(function () { return { scope, element } }, parameter as PartDefinition<typeof scope>, {}, noop);
-
-        else if (typeof parameter != 'undefined')
+                    if (parameter.controller instanceof Binding)
+                        partService.apply(function () { return { scope, element } }, { controller: parameter.controller.getValue(), template: parameter.template }, {}, noop);
+                    else
+                        partService.apply(function () { return { scope, element } }, parameter as PartDefinition<typeof scope>, {}, noop);
+        }
+        else
             partService.register(parameter, { scope, element });
     }
 
