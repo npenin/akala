c5b6a24a0b0dfb26ce4cbff809295b617a06240b
npenin <nicolas.penin@dragon-angel.fr>
2020-05-04 09:16:39 +0200

    dependency cleanup fixed pm cli install should always install in cwd

--- packages/pm/src/commands/install.ts
+++ packages/pm/src/commands/install.ts
@@ -2,13 +2,13 @@ import State from "../state";
 import npmHelper from "../npm-helper";
 import { Container } from "@akala/commands";
 import { join } from "path";
+import { createRequire } from 'module'
 
 
-
-export default async function install(this: State, packageName: string, folder: string, pm: Container<State>)
+export default async function install(this: State, packageName: string, pm: Container<State>)
 {
-    var path = folder || process.cwd();
+    var path = process.cwd();
     await npmHelper.install(packageName, path);
 
-    return await pm.dispatch('discover', packageName, join(path, 'node_modules'))
+    return await pm.dispatch('discover', packageName, createRequire(path))
 };
