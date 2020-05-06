61b5d0d19317face05dbb92b12187b26af09a227
npenin <nicolas.penin@dragon-angel.fr>
2020-05-06 06:21:04 +0200

    fixed quiet mode

--- packages/pm/src/yarn-helper.ts
+++ packages/pm/src/yarn-helper.ts
@@ -10,16 +10,16 @@ export default
         async install(packageName: string, path: string)
         {
             var cli = Cli.from(EssentialPlugin.commands)
-            await cli.run(['add', packageName, '--quiet'], { ...Cli.defaultContext, cwd: fslib.npath.toPortablePath(path), quiet: false, plugins: getPluginConfiguration() });
+            await cli.run(['add', packageName], { ...Cli.defaultContext, cwd: fslib.npath.toPortablePath(path), quiet: true, plugins: getPluginConfiguration() });
         },
         async update(packageName: string, path: string)
         {
             var cli = Cli.from(EssentialPlugin.commands)
-            await cli.run(['up', packageName, '--quiet'], { ...Cli.defaultContext, cwd: fslib.npath.toPortablePath(path), quiet: false, plugins: getPluginConfiguration() });
+            await cli.run(['up', packageName], { ...Cli.defaultContext, cwd: fslib.npath.toPortablePath(path), quiet: true, plugins: getPluginConfiguration() });
         },
         async link(packageName: string, path: string)
         {
             var cli = Cli.from(EssentialPlugin.commands)
-            await cli.run(['link', packageName, '--quiet'], { ...Cli.defaultContext, cwd: fslib.npath.toPortablePath(process.cwd()), quiet: false, plugins: getPluginConfiguration() });
+            await cli.run(['link', packageName], { ...Cli.defaultContext, cwd: fslib.npath.toPortablePath(process.cwd()), quiet: true, plugins: getPluginConfiguration() });
         }
     }
