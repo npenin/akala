2a08a72403c4d76c6cd4a46b88f5c54348ec3401
npenin <nicolas.penin@dragon-angel.fr>
2020-05-06 06:35:14 +0200

    fixed install

--- packages/pm/src/commands/install.ts
+++ packages/pm/src/commands/install.ts
@@ -1,12 +1,10 @@
 import State from "../state";
 import helper from "../yarn-helper";
 import { Container } from "@akala/commands";
-import { createRequire } from 'module'
-
 
 export default async function install(this: State, packageName: string, pm: Container<State>)
 {
     var path = process.cwd();
     await helper.install(packageName, path);
-    return await pm.dispatch('discover', packageName, createRequire(path))
+    return await pm.dispatch('discover', packageName, path)
 };
