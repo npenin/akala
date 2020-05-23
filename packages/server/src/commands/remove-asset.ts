d580852db4b87cd23c316aac9a86c7878adbd64b
npenin <nicolas.penin@dragon-angel.fr>
2020-05-23 10:52:54 +0000

    fixed remove-asset command

--- packages/server/src/commands/remove-asset.ts
+++ packages/server/src/commands/remove-asset.ts
@@ -5,16 +5,10 @@ import { serveStatic } from "../master-meta";
 import { targetWatchers, Asset } from "./asset";
 
 
-export default async function unregister(this: State, container: Container<State>, route: string, path: string)
+export default async function unregister(this: State, route: string, path: string)
 {
-    await this.assets.injectWithName([route], async (asset: Asset) =>
-    {
-        if (typeof (asset) == 'undefined')
-            return;
+    console.log(this.webpack.config.entry[route])
 
-        console.log(this.webpack.config.entry[route])
+    if (this.webpack.config.entry[route])
         this.webpack.config.entry[route].splice(this.webpack.config.entry[route].indexOf(path), 1);
-
-        await container.dispatch('compile', asset.output);
-    })();
 }
