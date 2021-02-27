56b08edac9b5822d26add31c1729d1dd46288d33
npenin <nicolas.penin@dragon-angel.fr>
2021-02-27 12:42:15 +0000

    added sidecar interface for config module

--- packages/configuration/src/index.ts
+++ packages/configuration/src/index.ts
@@ -2,3 +2,14 @@ import { Configuration } from './configuration'
 
 export default Configuration;
 export { Configuration };
+
+import Commander from './commander'
+
+import * as pm from '@akala/pm';
+declare module '@akala/pm'
+{
+    export interface SidecarMap
+    {
+        '@akala/configuration': Commander;
+    }
+}
