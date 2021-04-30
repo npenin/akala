59fd396fc5b5887271e0477122e857e1f5fe5400
npenin <nicolas.penin@dragon-angel.fr>
2021-04-30 11:43:43 +0000

    map is not initializing by default anymore

--- packages/pm/src/commands/map.ts
+++ packages/pm/src/commands/map.ts
@@ -1,12 +1,11 @@
 import State from '../state.js';
 import { isAbsolute, resolve } from "path";
-import { serveMetadata } from "@akala/commands";
 
 export default async function map<TName extends string>(this: State, name: TName, targetPath: string, cwd?: string, commandable?: boolean): Promise<State['config']['mapping'][TName]>
 {
     if (!isAbsolute(targetPath))
         targetPath = resolve(cwd || process.cwd(), targetPath);
-    this.config.mapping[name] = { path: targetPath, commandable: !!commandable, connect: serveMetadata(name, { options: {}, args: ['local'] }) };
+    this.config.mapping[name] = { path: targetPath, commandable: !!commandable, connect: {} };
     await this.config.save();
     return this.config.mapping[name]
 }
