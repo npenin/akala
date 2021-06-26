d31587048885b8a20c827f1383351ebad743e807
npenin <nicolas.penin@dragon-angel.fr>
2021-06-26 14:42:02 +0000

    renamed package folders added command name property on unknown command error

--- packages/commands/src/model/error-unknowncommand.ts
+++ packages/commands/src/model/error-unknowncommand.ts
@@ -2,7 +2,7 @@ export class UnknownCommandError extends Error
 {
     public readonly code = 'INVALID_CMD';
 
-    constructor(commandName: string)
+    constructor(public readonly commandName: string)
     {
         super(`Command with name ${commandName} could not be found`)
     }
