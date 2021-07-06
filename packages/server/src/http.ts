711ec5cb1d95e18e60927e2138f43a38db6fc565
npenin <nicolas.penin@dragon-angel.fr>
2021-07-06 17:07:20 +0000

    fixed http

--- packages/server/src/http.ts
+++ packages/server/src/http.ts
@@ -1,5 +1,5 @@
 import { FetchHttp, Http as coreHttp } from '@akala/core';
-import * as xml2js from 'xml'
+import * as xml2js from 'xml2js'
 
 FetchHttp.prototype['getXML'] = function (this: FetchHttp, url: string)
 {
