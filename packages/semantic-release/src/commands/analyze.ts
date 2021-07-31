aa455f980c29a46426261159bcbd0347b5ca54e1
npenin <nicolas.penin@dragon-angel.fr>
2021-07-31 22:33:44 +0000

    feat: improve commit analysis

--- packages/semantic-release/src/commands/analyze.ts
+++ packages/semantic-release/src/commands/analyze.ts
@@ -1,23 +1,57 @@
 import changelog = require('conventional-changelog-angular');
+import { Commit as ParserCommit } from 'conventional-commits-parser';
+import regex from 'conventional-commits-parser/lib/regex';
+import { Commit } from './parse';
 
 
-export default async function (commits: { hash: string, message: string }[])
+export default async function (commits: Commit[]): Promise<ParserCommit[]>
 {
     const parserOpts = (await changelog).parserOpts;
+    const regexParser = regex(parserOpts);
+    const headerPattern: RegExp = typeof parserOpts.headerPattern == 'string' ? new RegExp(parserOpts.headerPattern) : parserOpts.headerPattern;
+    const headerCorrespondence: string[] = typeof parserOpts.headerCorrespondence == 'string' ? [parserOpts.headerCorrespondence] : parserOpts.headerCorrespondence;
+    const noteKeywords: RegExp = regexParser.notes;
+
     return commits.map(c =>
     {
-        const result: Record<string, string> & { breaks?: true } = { hash: c.hash } as any;
-        var match = parserOpts.headerPattern.exec(c.message);
-        if (match)
-            parserOpts.headerCorrespondence.forEach((h, i) => result[h] = match[i + 1]);
+        const lines = c.message.split('\n').map(l => l.trim());
 
-        if (parserOpts.noteKeywords.reduce((previous, note) =>
+        var isBody = true;
+        const result = { hash: c.hash, notes: [], body: '', merge: '', header: '', footer: '', references: [], mentions: [], revert: null } as unknown as ParserCommit;
+        const body: string[] = [];
+        const footer: string[] = [];
+
+        lines.forEach((l, i) =>
         {
-            if (previous !== -1)
-                return previous;
-            return c.message.indexOf(note)
-        }, -1) !== -1)
-            result.breaks = true;
+            if (i == 0)
+            {
+                result.header = l;
+                const match = headerPattern.exec(c.message);
+                if (match)
+                    headerCorrespondence.forEach((h, i) => result[h] = match[i + 1]);
+
+                return;
+            }
+
+            const notesMatch = c.message.match(noteKeywords);
+            if (notesMatch)
+            {
+                isBody = false;
+            }
+
+            if (isBody)
+                body.push(l);
+            else
+            {
+                footer.push(l);
+
+                result.notes.push({
+                    title: notesMatch[1],
+                    text: notesMatch[2]
+                });
+            }
+
+        })
         return result;
     });
 }
