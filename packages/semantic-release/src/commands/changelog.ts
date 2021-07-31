8271d11a8aad8cc4d0ac947913df58a5855719ea
npenin <nicolas.penin@dragon-angel.fr>
2021-07-31 22:34:23 +0000

    feat: add changelog support

---
+++ packages/semantic-release/src/commands/changelog.ts
@@ -0,0 +1,9 @@
+import conventionalChangelogWriter from "conventional-changelog-writer";
+import angular from 'conventional-changelog-angular'
+import { Commit } from "conventional-commits-parser";
+import fs from 'fs/promises'
+
+export default async function changelog(commits: Commit[], changelogPath: string)
+{
+    await fs.writeFile(changelogPath, conventionalChangelogWriter.parseArray(commits, {}, (await angular).writerOpts));
+}
