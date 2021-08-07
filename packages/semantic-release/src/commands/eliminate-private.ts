9c291ca3c561c31f99e15a0a49c934c6f3f751b5
npenin <nicolas.penin@dragon-angel.fr>
2021-08-07 06:26:14 +0000

    fix: yarn publish will ignore private

---
+++ packages/semantic-release/src/commands/eliminate-private.ts
@@ -0,0 +1,11 @@
+import { readFile } from "fs/promises";
+import { resolve } from "path";
+
+export default async function (workspaces: { name: string, location: string }[])
+{
+    return Promise.all(workspaces.map(async workspace =>
+    {
+        const pkg = JSON.parse(await readFile(resolve(workspace.location, './package.json'), 'utf-8'));
+        return Object.assign(workspace, { private: pkg.private });
+    })).then(workspaces => workspaces.filter(w => !w.private));
+}
