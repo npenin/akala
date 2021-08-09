2f156a3b9be5adeae303250c7cfe3d91e5d3c646
npenin <nicolas.penin@dragon-angel.fr>
2021-08-09 15:56:01 +0000

    fix: improve get-version

--- packages/semantic-release/src/commands/get-version.ts
+++ packages/semantic-release/src/commands/get-version.ts
@@ -1,8 +1,13 @@
 import { readFile } from "fs/promises";
 import { resolve } from "path";
 
-export default async function (path: string)
+export default async function getVersion(workspace: string): Promise<string>
+export default async function getVersion(workspace: { location: string, name: string }): Promise<{ location: string, name: string, version: string }>
+export default async function getVersion(workspace: string | { location: string, name: string })
 {
+    if (typeof workspace !== 'string')
+        return Object.assign(workspace, { version: await getVersion(workspace.location) });
+    const path = workspace;
     const pkg = JSON.parse(await readFile(resolve(path, './package.json'), 'utf-8'));
     return pkg.version;
 }
