461c1a42467d8fca4f228a1ef5467ed9cd2c5e7f
npenin <nicolas.penin@dragon-angel.fr>
2021-08-13 02:19:57 +0000

    feat: add middleware with priority handling

--- packages/core/src/router/composite.ts
+++ packages/core/src/router/composite.ts
@@ -54,7 +54,7 @@ export class MiddlewareComposite<T extends unknown[]> implements Middleware<T>, 
 
     private readonly stack: AnyMiddleware<T>[] = [];
 
-    public useMiddleware(...middlewares: Middleware<T>[]): this
+    public useMiddleware(...middlewares: AnyMiddleware<T>[]): this
     {
         this.stack.push(...middlewares);
         return this;
@@ -182,16 +182,147 @@ export class MiddlewareComposite<T extends unknown[]> implements Middleware<T>, 
 }
 
 
-export interface MiddlewareComposite<T extends unknown[]> extends Middleware<T>, ErrorMiddleware<T>, ExtendableCompositeMiddleware<T>
+export class MiddlewareCompositeWithPriority<T extends unknown[]> implements Middleware<T>, ExtendableCompositeMiddleware<T>
 {
-    readonly name?: string;
-    useMiddleware(...middlewares: AnyMiddleware<T>[]): this;
-    use(...middlewares: ((...args: T) => Promise<unknown>)[]): this;
-    useError(...middlewares: ((error: Error | OptionsResponse, ...args: T) => Promise<unknown>)[]): this;
-    process<X = unknown>(...req: T): Promise<X>;
-    handleError(error: Error | OptionsResponse, ...req: T): MiddlewarePromise;
-    handle(...req: T): MiddlewarePromise;
+    public readonly name?: string
+
+    constructor(name?: string)
+    {
+        this.name = name;
+    }
+
+    private readonly stack: [number, AnyMiddleware<T>][] = [];
+
+    public useMiddleware(priority: number, ...middlewares: AnyMiddleware<T>[]): this
+    {
+        this.stack.push(...middlewares.map(m => [priority, m] as [number, Middleware<T>]));
+        return this;
+    }
+
+    public use(priority: number, ...middlewares: ((...args: T) => Promise<unknown>)[]): this
+    {
+        return this.useMiddleware(priority, ...middlewares.map(convertToMiddleware));
+    }
+
+    public useError(priority: number, ...middlewares: ((error: Error | OptionsResponse, ...args: T) => Promise<unknown>)[]): this
+    {
+        return this.useMiddleware(priority, ...middlewares.map(convertToErrorMiddleware));
+    }
+
+    public process<X = unknown>(...req: T): Promise<X>
+    {
+        return this.handle(...req).then(x => Promise.reject(x), x => Promise.resolve(x));
+    }
+
+    public handleError(error: Error | OptionsResponse, ...req: T): MiddlewarePromise
+    {
+        return new Promise<Error | SpecialNextParam | OptionsResponse>((resolve, reject) =>
+        {
+            let failed: boolean = !!error;
+            this.stack.sort((a, b) => a[0] - b[0]);
+            eachAsync(this.stack, (middleware, _i, next) =>
+            {
+                if (failed && isErrorMiddleware(middleware[1]))
+                    middleware[1].handleError(error as Error, ...req).then(err =>
+                    {
+                        if (err === 'break')
+                            return next(err);
+                        if (typeof err != 'string' && typeof err != 'undefined')
+                            error = err;
+
+                        failed = true;
+                        next();
+                    }, x => next({ success: x }));
+                else
+                    next();
+            }, function (err?: MiddlewareSuccess | SpecialNextParam)
+            {
+                switch (typeof err)
+                {
+                    case 'string':
+                        resolve();
+                        return;
+                    case 'undefined':
+                        resolve(error);
+                        return;
+                    default:
+                        reject(err.success);
+                        return;
+                }
+            }, true);
+        });
+
+    }
+
+    public handle(...req: T): MiddlewarePromise
+    {
+        return new Promise<Error | SpecialNextParam | OptionsResponse>((resolve, reject) =>
+        {
+            let error: Error | OptionsResponse = undefined;
+            let failed: boolean = undefined;
+            this.stack.sort((a, b) => a[0] - b[0]);
+            eachAsync(this.stack, (middleware, _i, next) =>
+            {
+                if (failed && isErrorMiddleware(middleware[1]))
+                    middleware[1].handleError(error as Error, ...req).then(err =>
+                    {
+                        if (err === 'break')
+                            return next(err);
+                        if (typeof err != 'string' && typeof err != 'undefined')
+                            error = err;
+
+                        failed = true;
+                        next();
+                    }, x => next({ success: x }));
+                else
+                    try
+                    {
+                        if (!isStandardMiddleware(middleware[1]))
+                            next();
+                        else
+                            middleware[1].handle(...req).then(err =>
+                            {
+                                if (err === 'break')
+                                    return next(err);
+                                if (typeof err != 'string' && typeof err != 'undefined')
+                                {
+                                    if (err['allow'])
+                                        error['allow'].push(...err['allow']);
+                                    else
+                                        error = err;
+                                }
+                                failed = err instanceof Error;
+                                next();
+                            }, x => next({ success: x })
+                            );
+                    }
+                    catch (e)
+                    {
+                        error = e;
+                        failed = true;
+                        next();
+                    }
+
+            }, function (err?: MiddlewareSuccess | SpecialNextParam)
+            {
+                switch (typeof err)
+                {
+                    case 'string':
+                        resolve();
+                        return;
+                    case 'undefined':
+                        resolve(error);
+                        return;
+                    default:
+                        reject(err.success);
+                        return;
+                }
+            }, true);
+        });
+    }
 }
+
+
 function isErrorMiddleware<T extends unknown[]>(middleware: AnyMiddleware<T>): middleware is ErrorMiddleware<T>
 {
     return middleware && middleware['handleError'];
