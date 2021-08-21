c094bac38582d159d52437ddd6ae746bf9d8f658
npenin <nicolas.penin@dragon-angel.fr>
2021-08-21 16:26:17 +0000

    fix: bump dependent

--- packages/semantic-release/src/commands/bump-dependents.ts
+++ packages/semantic-release/src/commands/bump-dependents.ts
@@ -1,10 +1,11 @@
 import { Levels } from "./recommend-bump";
 
+type Workspace = { location: string, name: string, workspaceDependencies: string[], bump: keyof Levels };
 
-export default function (workspaces: { location: string, name: string, workspaceDependencies: string[] }[], versions: { [key: string]: string }, rules?: { [key: string]: keyof Levels })
+export default function (workspaces: Workspace[], rules?: { [key: string]: string })
 {
-    const translatedVersions: Record<string, Levels> = Object.fromEntries(Object.entries(versions).map(entry => [entry[0], Levels[entry[1]]]));
-    const translatedRules: Record<Levels, Levels> = Object.fromEntries(Object.entries(versions).map(entry => [Levels[entry[0]], Levels[entry[1]]]));
+    const translatedVersions: Record<string, Levels> = Object.fromEntries(workspaces.map(entry => [entry.name, Levels[entry.bump]]));
+    const translatedRules: Record<Levels, Levels> = Object.fromEntries(Object.entries(rules).map(entry => [Levels[entry[0]], Levels[entry[1]]]));
 
     var loopCounts = workspaces.length;
     do
@@ -22,17 +23,15 @@ export default function (workspaces: { location: string, name: string, workspace
             {
                 hasChange = true;
                 translatedVersions[w.name] = newVersion;
-                versions[w.name] = Levels[newVersion];
             }
         });
         loopCounts--;
     }
     while (hasChange && loopCounts > 0);
 
-    console.log(versions);
     if (loopCounts == 0)
         throw new Error('too many loops');
     else
         console.debug(`completed in ${workspaces.length - loopCounts} loops`);
-    return Object.entries(translatedVersions).map(entry => Object.assign({ bump: Levels[entry[1]] }, workspaces.find(w => w.name == entry[0]))).filter(e => e.bump != 'decline');
+    return Object.entries(translatedVersions).filter(e => e[1] != Levels.decline).map(entry => Object.assign(workspaces.find(w => w.name == entry[0]), { bump: Levels[entry[1]] }));
 }
