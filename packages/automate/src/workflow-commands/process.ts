9227e2957ca96bc43809bb57763363d8e45ded7e
npenin <nicolas.penin@dragon-angel.fr>
2021-08-22 07:42:56 +0000

    fix: avoids workflow leaks in context

--- packages/automate/src/workflow-commands/process.ts
+++ packages/automate/src/workflow-commands/process.ts
@@ -40,5 +40,5 @@ export default function process<U extends object>(this: CliContext<{ file: strin
     const container = new Container<object>(workflow.name + '-' + new Date().toISOString(), this);
     container.register('loader', self);
 
-    return automate<U, JobStepDispatch | JobStepRun | JobStepJob>(workflow, runnerMiddleware(container, this, self), Object.assign(this, inputs), 'ignore');
+    return automate<U, JobStepDispatch | JobStepRun | JobStepJob>(workflow, runnerMiddleware(container, this, self), Object.assign({}, this, inputs), 'ignore');
 }
