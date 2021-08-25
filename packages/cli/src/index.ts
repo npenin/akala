43fdcbad033147f3c07ecaa27cbfac3ac908292c
npenin <nicolas.penin@dragon-angel.fr>
2021-08-25 11:40:28 +0000

    fix: logger as non enumerable property

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -12,11 +12,30 @@ export * from './router/index'
 export default program;
 export function buildCliContext<T extends Record<string, string | boolean | string[] | number> = Record<string, string | boolean | string[] | number>>(logger: winston.Logger, ...args: string[]): CliContext<T>
 {
-    return { args: args, argv: args, options: {} as T, currentWorkingDirectory: undefined, logger }
+    const result: CliContext<T> = { args: args, argv: args, options: {} as T, currentWorkingDirectory: undefined } as any;
+    Object.defineProperty(result, 'logger', { enumerable: false, value: logger });
+    return result;
 }
 export function buildCliContextFromProcess<T extends Record<string, string | boolean | string[] | number> = Record<string, string | boolean | string[] | number>>(logger?: winston.Logger): CliContext<T>
 {
-    return { args: process.argv.slice(2), argv: process.argv, commandPath: process.argv0, options: {} as T, currentWorkingDirectory: process.cwd(), logger: logger || winston.createLogger({ levels: winston.config.cli.levels, format: winston.format.combine(winston.format.splat(), winston.format.colorize(), winston.format.simple()), level: 'error', transports: [new winston.transports.Console()] }) }
+    logger = logger || winston.createLogger({
+        levels: winston.config.cli.levels,
+        format: winston.format.combine(
+            winston.format.splat(),
+            winston.format.colorize(),
+            winston.format.simple()),
+        level: 'error',
+        transports: [new winston.transports.Console()]
+    });
+    const result: CliContext<T> = {
+        args: process.argv.slice(2),
+        argv: process.argv,
+        commandPath: process.argv0,
+        options: {} as T,
+        currentWorkingDirectory: process.cwd(),
+    } as any;
+    Object.defineProperty(result, 'logger', { enumerable: false, value: logger });
+    return result;
 }
 
 export function unparseOptions(options: CliContext['options']): string[]
