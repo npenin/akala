220953a07a219edf565869f9c5e102af45630568
npenin <nicolas.penin@dragon-angel.fr>
2021-10-02 22:35:29 +0200

    fix: workflow runner

--- packages/automate/src/workflow-commands/$init.ts
+++ packages/automate/src/workflow-commands/$init.ts
@@ -1,4 +1,4 @@
-import { CliContext } from "@akala/cli";
+import { buildCliContextFromProcess, CliContext } from "@akala/cli";
 import { Container, serve, serveMetadata, ServeOptions } from "@akala/commands";
 import { Container as pmContainer } from "@akala/pm";
 import workflow from "../workflow";
@@ -6,6 +6,7 @@ import workflow from "../workflow";
 export default async function $init(this: CliContext, name: string, pm: pmContainer, options: ServeOptions, self: workflow.container & Container<void>)
 {
     this.currentWorkingDirectory = process.cwd();
+    this.logger = buildCliContextFromProcess().logger;
     if (pm)
         await pm.dispatch('connect', name, options);
     else
