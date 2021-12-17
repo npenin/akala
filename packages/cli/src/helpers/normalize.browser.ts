4815fd548959cf0b53b4271605e96acdaa7ff3c0
npenin <nicolas.penin@dragon-angel.fr>
2021-12-17 08:52:37 +0100

    fix: client compilation

---
+++ packages/cli/src/helpers/normalize.browser.ts
@@ -0,0 +1,15 @@
+import { createRequire } from 'module'
+import * as path from 'path'
+
+export default function normalize(mode: 'require' | 'requireMeta' | true, currentWorkingDirectory: string, value: string)
+{
+    switch (mode)
+    {
+        case 'require':
+        case 'requireMeta':
+            throw new Error('require or requireMeta are not supported on client side');
+        default:
+        case true:
+            return path.resolve(currentWorkingDirectory, value.toString());
+    }
+}
