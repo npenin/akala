04ba410926a03034c9d68c7981e9548f22a9c426
npenin <nicolas.penin@dragon-angel.fr>
2022-02-06 23:56:30 +0100

    refactor: remove extra logging

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 find packages -name node_modules -exec rm -R {} \;
 yarn
-echo "pwet"
 find packages -name dist -exec rm -R {} \;
 find packages -name lib -exec rm -R {} \;
 find packages -name tsconfig.tsbuildinfo -exec rm -R {} \;
