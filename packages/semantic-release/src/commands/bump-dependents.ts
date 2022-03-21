e19a4e71a4768abb3549ea73972bcf1248b2411d
npenin <nicolas.penin@dragon-angel.fr>
2022-03-21 07:48:57 +0100

    refactor: switched to new log mechanism

--- packages/semantic-release/src/commands/bump-dependents.ts
+++ packages/semantic-release/src/commands/bump-dependents.ts
@@ -1,14 +1,15 @@
+import { CliContext } from "@akala/cli";
 import { Levels } from "./recommend-bump";
 
 type Workspace = { location: string, name: string, workspaceDependencies: string[], bump: keyof Levels };
 
-export default function (workspaces: Workspace[], rules?: { [key: string]: string })
+export default function (this: CliContext, workspaces: Workspace[], rules?: { [key: string]: string })
 {
     const translatedVersions: Record<string, Levels> = Object.fromEntries(workspaces.map(entry => [entry.location, Levels[entry.bump]]));
     const translatedRules: Record<Levels, Levels> = Object.fromEntries(Object.entries(rules).map(entry => [Levels[entry[0]], Levels[entry[1]]]));
 
-    console.log(translatedRules)
-    console.log(translatedVersions)
+    this.logger.debug(translatedRules)
+    this.logger.debug(translatedVersions)
     var loopCounts = workspaces.length;
     do
     {
@@ -17,7 +18,7 @@ export default function (workspaces: Workspace[], rules?: { [key: string]: strin
         {
             var newVersion: Levels = (w.workspaceDependencies && w.workspaceDependencies.reduce((previous: Levels, c) =>
             {
-                console.log(`${c} is ${translatedVersions[c]} and ${w.name} is ${previous}`);
+                this.logger.debug(`${c} is ${translatedVersions[c]} and ${w.name} is ${previous}`);
 
                 if (translatedRules[translatedVersions[c]] < previous)
                     return translatedRules[translatedVersions[c]];
@@ -33,10 +34,10 @@ export default function (workspaces: Workspace[], rules?: { [key: string]: strin
     }
     while (hasChange && loopCounts > 0);
 
-    console.log(translatedVersions);
+    this.logger.debug(translatedVersions);
     if (loopCounts == 0)
         throw new Error('too many loops');
     else
-        console.debug(`completed in ${workspaces.length - loopCounts} loops`);
+        this.logger.verbose(`completed in ${workspaces.length - loopCounts} loops`);
     return Object.entries(translatedVersions).filter(e => e[1] != Levels.decline).map(entry => Object.assign(workspaces.find(w => w.location == entry[0]), { bump: Levels[entry[1]] }));
 }
