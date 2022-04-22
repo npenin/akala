ffd2a1724e6f0073956bd0e293dd35a7e6830c18
npenin <nicolas.penin@dragon-angel.fr>
2022-04-22 09:04:31 +0200

    fix: improved ready command for remote containers

--- packages/pm/src/commands/ready.ts
+++ packages/pm/src/commands/ready.ts
@@ -1,6 +1,17 @@
+import { Container, Metadata, registerCommands, SelfDefinedCommand } from '@akala/commands';
+import pm from '../container';
 import State, { RunningContainer } from '../state';
 
-export default function ready(this: State, container: RunningContainer): void
+export default async function ready(this: State, pm: pm.container & Container<State>, container: RunningContainer): Promise<void>
 {
+    if (!container.name && !container.connect && !container.commandable)
+    {
+        container.commandable = true;
+        const metadata: Metadata.Container = await container.dispatch('$metadata');
+        container.name = metadata.name;
+        registerCommands(metadata.commands, container.processor, container);
+        pm.register(container.name, container, true);
+        container.register(new SelfDefinedCommand(() => { pm.unregister(container.name) }, '$disconnect'));
+    }
     container?.ready?.resolve();
 }
