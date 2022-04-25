823810d5cb59fc6f7a1ac0da6afcfbb2b8f6fa43
npenin <nicolas.penin@dragon-angel.fr>
2022-04-25 04:49:05 +0000

    fix: update ready command for remote containers

--- packages/pm/src/commands/ready.ts
+++ packages/pm/src/commands/ready.ts
@@ -2,15 +2,19 @@ import { Container, Metadata, registerCommands, SelfDefinedCommand } from '@akal
 import pm from '../container';
 import State, { RunningContainer } from '../state';
 
-export default async function ready(this: State, pm: pm.container & Container<State>, container: RunningContainer): Promise<void>
+export default async function ready(this: State, pm: pm.container & Container<State>, container: RunningContainer, remoteContainer: Container<void>): Promise<void>
 {
-    if (!container.name && !container.connect && !container.commandable)
+    if (!container)
     {
-        container.commandable = true;
-        container.register<Metadata.Command>({ name: '$metadata', config: {}, inject: [] }, true);
-        const metadata: Metadata.Container = await container.dispatch('$metadata');
-        container.name = metadata.name;
+        remoteContainer.register<Metadata.Command>({ name: '$metadata', config: {}, inject: [] }, true);
+        const metadata: Metadata.Container = await remoteContainer.dispatch('$metadata');
+        remoteContainer.name = metadata.name;
+        this.processes[remoteContainer.name] = container = Object.assign(remoteContainer, { process: null, commandable: true, path: '', container: null, stateless: true, running: true });
         registerCommands(metadata.commands, container.processor, container);
+        remoteContainer.register(new SelfDefinedCommand(() =>
+        {
+            container.running = false;
+        }));
         pm.register(container.name, container, true);
         container.register(new SelfDefinedCommand(() => { pm.unregister(container.name) }, '$disconnect'));
     }
