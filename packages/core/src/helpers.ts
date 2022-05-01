4a66e5a1bf4b75152216268fe78c44a6166049ed
npenin <nicolas.penin@dragon-angel.fr>
2022-05-01 07:52:09 +0000

    feat: removing problematic extend function
    BREAKING CHANGE: extend helper no more is

--- packages/core/src/helpers.ts
+++ packages/core/src/helpers.ts
@@ -1,7 +1,6 @@
 import { Module } from './module';
 import { onResolve } from './global-injector'
 import * as jsonrpc from '@akala/json-rpc-ws'
-import { off } from 'process';
 export { Module };
 export * from './promiseHelpers';
 export { each as eachAsync, NextFunction, map as mapAsync, AggregateErrors } from './eachAsync';
@@ -9,32 +8,6 @@ export { each, grep, Proxy, map } from './each';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-empty-function
 export function noop() { }
-
-export function extend<T, U>(target: T, other: U): T & U;
-export function extend<T, U, V>(target: T, other1: U, other2: V): T & U & V;
-export function extend<T, U, V, W>(target: T, other1: U, other2: V, other3: W): T & U & V & W;
-export function extend<T, U, V, W, X>(target: T, other1: U, other2: V, other3: W, other43: X): T & U & V & W & X;
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
-export function extend(target: any, ...args)
-{
-    args.forEach(function (arg)
-    {
-        if (typeof (arg) == 'object' && arg)
-            Object.getOwnPropertyNames(arg).forEach(function (key)
-            {
-                switch (typeof (target[key]))
-                {
-                    case 'object':
-                        extend(target[key], arg[key]);
-                        break;
-                    default:
-                        target[key] = arg[key];
-                        break;
-                }
-            });
-    });
-    return target;
-}
 
 export function module(name: string, ...dependencies: string[]): Module
 export function module(name: string, ...dependencies: Module[]): Module
