0f33d1540d6925d6dcc2b7909b261d4fece8af31
npenin <nicolas.penin@dragon-angel.fr>
2022-05-13 22:46:55 +0000

    fix: sort excludes non workspace dependencies

--- packages/semantic-release/src/tests/sort.ts
+++ packages/semantic-release/src/tests/sort.ts
@@ -2,7 +2,7 @@ import { sort } from "../commands/bump-dependents";
 import { Levels } from "../commands/recommend-bump";
 
 console.log(sort([
-    { name: '@domojs/devices', workspaceDependencies: [], location: '', bump: 'decline' },
+    { name: '@domojs/devices', workspaceDependencies: ['@akala/core'], location: '', bump: 'decline' },
     { name: '@domojs/rfx-parsers', workspaceDependencies: ['@domojs/devices'], location: '', bump: 'decline' },
     { name: '@domojs/rfx', workspaceDependencies: ['@domojs/rfx-parsers', '@domojs/devices'], location: '', bump: 'decline' },
     { name: '@domojs/zigate-parsers', workspaceDependencies: ['@domojs/devices'], location: '', bump: 'decline' },
