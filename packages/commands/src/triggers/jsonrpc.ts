815e03a975306077c1fec2e67ccb8323d34a1f17
npenin <nicolas.penin@dragon-angel.fr>
2022-05-18 07:16:49 +0000

    fix: logging namespace in jsonrpc

--- packages/commands/src/triggers/jsonrpc.ts
+++ packages/commands/src/triggers/jsonrpc.ts
@@ -8,7 +8,7 @@ import { logger } from '@akala/core';
 export const trigger = new Trigger('jsonrpc', function register<T>(container: Container<T>, media: SocketAdapter)
 {
     // assert.ok(media instanceof ws.SocketAdapter, 'to be attached, the media must be an instance of @akala/json-rpc-ws.Connection');
-    const log = logger('akala:commands:jsonrpcws:' + container.name)
+    const log = logger('akala:commands:jsonrpc:' + container.name)
     return JsonRpc.getConnection(media, container, null, log);
 })
 
