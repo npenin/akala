dc28cb037e17a5b39fd0e4af3a451181682815ba
npenin <nicolas.penin@dragon-angel.fr>
2022-10-26 08:27:02 +0000

    fix: properly generate module from hierarchy

--- packages/commands/src/cli/new/module.ts
+++ packages/commands/src/cli/new/module.ts
@@ -8,7 +8,7 @@ type Folder = { name: string, children: (File | Folder)[] };
 
 export async function mkHierarchy(destination: string, hierarchy: (File | Folder)[])
 {
-    await fs.mkdir(destination);
+    await fs.mkdir(destination, { recursive: true });
     await Promise.all(hierarchy.map(i =>
     {
         if ('children' in i)
@@ -20,44 +20,46 @@ export async function mkHierarchy(destination: string, hierarchy: (File | Folder
 
 export default async function (name: string, context: CliContext<{ force?: boolean }>, destination?: string)
 {
-    var hierarchy = [{
-        name: 'src',
-        children: [
-            { name: 'index.ts', context: '' }
-        ],
+    var hierarchy: Folder = {
+        name, children: [{
+            name: 'src',
+            children: [
+                { name: 'index.ts', content: '' }
+            ],
 
-    }, {
-        name: '.gitignore', content: `.nyc_output
-    node_modules
-    dist
-    coverage
-    yarn-error.log
-    *._ts`},
-    {
-        name: '.npmignore', content: `.!dist
-    test`},
-    {
-        name: 'package.json', content: JSON.stringify({
-            "name": name,
-            "version": "0.0.0",
-            "main": "dist/index.js",
-            "types": "dist/index.d.ts",
-            "scripts": {
-                "test": "echo \"Error: no test specified\""
-            },
-        }, null, 4)
-    },
-    {
-        name: 'tsconfig.json', content: JSON.stringify({
-            "extends": "../tsconfig.settings.json",
-            "compileOnSave": true,
-            "compilerOptions": {
-                "rootDir": "src",
-                "outDir": "dist"
-            },
-            "references": []
-        })
-    }];
+        }, {
+            name: '.gitignore', content: `.nyc_output
+node_modules
+dist
+coverage
+yarn-error.log
+*._ts`},
+        {
+            name: '.npmignore', content: `.!dist
+test`},
+        {
+            name: 'package.json', content: JSON.stringify({
+                "name": name,
+                "version": "0.0.0",
+                "main": "dist/index.js",
+                "types": "dist/index.d.ts",
+                "scripts": {
+                    "test": "echo \"Error: no test specified\""
+                },
+            }, null, 4)
+        },
+        {
+            name: 'tsconfig.json', content: JSON.stringify({
+                "extends": "../tsconfig.settings.json",
+                "compileOnSave": true,
+                "compilerOptions": {
+                    "rootDir": "src",
+                    "outDir": "dist"
+                },
+                "references": []
+            })
+        }]
+    };
 
-    await mkHierarchy(destination, hierarchy);
+    await mkHierarchy(destination, [hierarchy]);
 }
