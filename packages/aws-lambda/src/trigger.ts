969dd687fc778a4a4490df83977644fad7dfdf2c
npenin <nicolas.penin@dragon-angel.fr>
2022-11-08 08:29:57 +0000

    feat: new aws-lambda module

---
+++ packages/aws-lambda/src/trigger.ts
@@ -0,0 +1,73 @@
+import aws from 'aws-sdk';
+import { Metadata, Processors, Trigger } from '@akala/commands'
+import { Injector, mapAsync } from '@akala/core';
+import { HttpRouter, trigger as httpTrigger } from '@akala/server';
+import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda'
+import http from 'http'
+
+aws.config.apiVersions = {
+    lambda: '2015-03-31',
+    // other service API versions
+};
+
+export const trigger = new Trigger('aws', (container, config: { [key: string]: string } | string) =>
+{
+    return (event: { Records: { eventSource: string }[] } | APIGatewayEvent, context: Context) =>
+    {
+        if ('Records' in event)
+        {
+            const ctxInjector = new Injector();
+            ctxInjector.register('context', context);
+            return mapAsync(event.Records, async (record) =>
+            {
+                var cmdInjector = new Injector(ctxInjector);
+                cmdInjector.register('event', record);
+                const cmd: Metadata.Command | void = cmdInjector.inject((cmdName: string) => container.resolve(cmdName), typeof config == 'string' ? config : config[record.eventSource])(this);
+
+                if (!cmd)
+                    return Promise.reject(new Error('command not found'));
+
+                if (cmd.config[record.eventSource])
+                    return Processors.Local.execute(cmd, (...args) => container.dispatch(cmd, { _trigger: record.eventSource, context, event, param: args }), container, { context, event, param: [], _trigger: record.eventSource })
+
+                if (cmd.config.aws)
+                    return Processors.Local.execute(cmd, (...args) => container.dispatch(cmd, { _trigger: 'aws', context, event, param: args }), container, { context, event, param: [], _trigger: 'aws' })
+
+                return Promise.reject(new Error('AWS command mapping not found for command ' + cmd.name));
+
+            }, false)
+        }
+        else if ('path' in event)
+        {
+            const router = container.attach(httpTrigger, new HttpRouter());
+            router.formatters.useMiddleware({
+                handle(_req, res, result)
+                {
+                    return Promise.reject({ statusCode: res.statusCode || 200, body: JSON.stringify(result) })
+                },
+                handleError(err, req, res, result)
+                {
+                    return Promise.reject({ statusCode: 500, body: err && err.toString() || 'An unexpected error occurred' });
+                }
+            });
+            const req = HttpRouter.makeRequest(event as unknown as http.IncomingMessage);
+            const res = HttpRouter.extendResponse<Partial<APIGatewayProxyResult>>({});
+            return router.handle(req, res).then((err) =>
+            {
+                if (err && err !== 'break')
+                {
+                    router.formatError(req, res, err);
+                    return;
+                }
+                console.error('deadend');
+                console.error({ url: req.url, headers: req.headers, ip: req.ip });
+                res.statusCode = 404;
+                res.statusMessage = 'Not Found';
+                res.body = 'Not Found';
+                return res;
+            },
+                (result) => result !== res ? router.format(req, res, result).then(e => router.formatError(req, res, e), x => x) : res);
+        }
+        throw new Error('Not supported')
+    }
+});
