12615a2c3711dbee39929d7823e77c79235be0e3
npenin <nicolas.penin@dragon-angel.fr>
2022-12-03 08:11:32 +0000

    feat: implement support typings

--- packages/commands/src/cli/implement.ts
+++ packages/commands/src/cli/implement.ts
@@ -19,7 +19,17 @@ export default async function implement(pathToCommandFile: string, destination: 
     }
     await Promise.all(metadata.commands.map(async c =>
     {
-        await command(c.name, options, join(destination, metadata.name));
+        if (!c.config)
+            c.config = {};
+        var paramConfig: { name: string, type?: string }[] = c.config["types"] && c.config["types"].inject && c.config["types"].inject.map((a, i) => ({ name: a, type: c.config["types"].types && c.config["types"].types[i] }));
+        if (!paramConfig)
+            paramConfig = c.config[""] && c.config[""].inject && c.config[""].inject?.map(p => ({ name: p.replace(/\./g, '') }));
+
+        await command(c.name, options, join(destination, metadata.name), paramConfig);
+        if (!c.config.fs)
+            c.config.fs = { inject: c.config[""]?.inject || [] } as any;
+        c.config.fs.path = join(destination, metadata.name, c.name + '.ts');
+        c.config.fs.source = join(destination, metadata.name, c.name + '.ts');
         await newCommandConfiguration(c, options, join(destination, metadata.name));
     }
     ));
--- packages/commands/src/cli/new/command.ts
+++ packages/commands/src/cli/new/command.ts
@@ -1,10 +1,10 @@
 import { CliContext } from "@akala/cli";
 import { outputHelper, write } from "../new";
 
-export default async function (name: string, options: CliContext<{ force?: boolean }>['options'], destination?: string)
+export default async function (name: string, options: CliContext<{ force?: boolean }>['options'], destination?: string, args?: { name: string, type?: string }[])
 {
     var { output } = await outputHelper(destination, name + '.ts', options && options.force);
-    await write(output, `export default async function ${name}()
+    await write(output, `export default async function ${name}(${args?.map(a => a.type ? a.name + ': ' + a.type : a.name)?.join(', ') || ''})
 {
 
 }`);
