eecf82ed3f77b408078264f265570c64344adabb
npenin <nicolas.penin@dragon-angel.fr>
2023-01-30 05:12:26 +0000

    fix: connectByPreference not working with sockets

--- packages/commands/src/serve-metadata.ts
+++ packages/commands/src/serve-metadata.ts
@@ -52,9 +52,9 @@ export async function connectByPreference<T = unknown>(options: ServeMetadata, s
         {
             if (order === 'socket' || order == 'ssocket')
                 if (settings?.preferRemote)
-                    return options[order].find(s => !isIpcConnectOption(s)) || options[order].find(s => isIpcConnectOption(s));
+                    return options[order].filter(s => !isIpcConnectOption(s)) || options[order].find(s => isIpcConnectOption(s));
                 else
-                    return options[order].find(s => isIpcConnectOption(s)) || options[order].find(s => !isIpcConnectOption(s));
+                    return options[order].filter(s => isIpcConnectOption(s)) || options[order].find(s => !isIpcConnectOption(s));
             return options[order];
         }
     });
