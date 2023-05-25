c760ee3d56fcd978bc6db2ebf9bfa57b95e3d500
npenin <github@dragon-angel.fr>
2023-05-25 12:55:40 +0200

    fix: compilation

--- packages/nocode/src/layouts/columns.ts
+++ packages/nocode/src/layouts/columns.ts
@@ -1,18 +1,18 @@
-import { CompositeTag, Div, FlowContentTags } from "../dom.js";
+import { dom } from "@akala/pages";
 
-export default function (columns: 1): CompositeTag<'div', [FlowContentTags]>
-export default function (columns: 2): CompositeTag<'div', [FlowContentTags, FlowContentTags]>
-export default function (columns: 3): CompositeTag<'div', [FlowContentTags, FlowContentTags, FlowContentTags]>
-export default function (columns: 4): CompositeTag<'div', [FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags]>
-export default function (columns: 5): CompositeTag<'div', [FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags]>
-export default function (columns: 6): CompositeTag<'div', [FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags]>
-export default function (columns: 7): CompositeTag<'div', [FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags]>
-export default function (columns: 8): CompositeTag<'div', [FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags]>
-export default function (columns: 9): CompositeTag<'div', [FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags]>
-export default function (columns: 10): CompositeTag<'div', [FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags]>
-export default function (columns: 11): CompositeTag<'div', [FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags]>
-export default function (columns: 12): CompositeTag<'div', [FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags, FlowContentTags]>
-export default function <T extends number>(columns: T): Div
+export default function (columns: 1): dom.CompositeTag<'div', [dom.FlowContentTags]>
+export default function (columns: 2): dom.CompositeTag<'div', [dom.FlowContentTags, dom.FlowContentTags]>
+export default function (columns: 3): dom.CompositeTag<'div', [dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags]>
+export default function (columns: 4): dom.CompositeTag<'div', [dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags]>
+export default function (columns: 5): dom.CompositeTag<'div', [dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags]>
+export default function (columns: 6): dom.CompositeTag<'div', [dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags]>
+export default function (columns: 7): dom.CompositeTag<'div', [dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags]>
+export default function (columns: 8): dom.CompositeTag<'div', [dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags]>
+export default function (columns: 9): dom.CompositeTag<'div', [dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags]>
+export default function (columns: 10): dom.CompositeTag<'div', [dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags]>
+export default function (columns: 11): dom.CompositeTag<'div', [dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags]>
+export default function (columns: 12): dom.CompositeTag<'div', [dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags, dom.FlowContentTags]>
+export default function <T extends number>(columns: T): dom.Div
 {
     return { type: 'div', classes: ['columns-' + columns], content: new Array(columns) };
 }
