7c1b792c073e7fcfc0c32d59ee68215d2fcdff7e
neonp <nicolas.penin@dragon-angel.fr>
2023-07-11 02:04:35 +0000

    fix: improve spanasync resilience

--- packages/pm/src/cli-helper.ts
+++ packages/pm/src/cli-helper.ts
@@ -1,22 +1,30 @@
-import * as cp from 'child_process'
+import * as child_process from 'child_process'
 
-export function spawnAsync(program: string, options: cp.SpawnOptions, ...args: string[]): Promise<void>
+export function spawnAsync(program: string, options: child_process.SpawnOptions, ...args: string[]): Promise<void>
 {
     options = Object.assign({ stdio: ['ignore', 'ignore', 'pipe'], shell: false, windowsHide: true }, options);
+
+    const stderrInherit = options.stdio[2] == 'inherit';
+    if (stderrInherit)
+        options.stdio = [options.stdio[0], options.stdio[1], 'pipe', ...options.stdio.slice(3)] as child_process.StdioOptions;
 
     return new Promise<void>((resolve, reject) =>
     {
         let err = ''
-        cp
+        const cp = child_process
             .spawn(program, args, options)
             .on('error', reject).on('exit', (code) =>
             {
                 if (code == 0)
                     resolve()
                 reject(new Error(program + ' exited with error code ' + code + '\n' + err))
-            }).stderr.on('data', (chunk) =>
-            {
-                err += chunk;
             });
+        cp.stderr.on('data', (chunk) =>
+        {
+            err += chunk;
+        });
+
+        if (stderrInherit)
+            cp.stderr.pipe(process.stderr);
     })
 }
