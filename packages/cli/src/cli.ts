beb7ddf1a90abbebb4954f662bcb2e6a0eaa7a4e
neonp <nicolas.penin@dragon-angel.fr>
2023-07-14 09:54:21 +0000

    fix: relative to config file cli imports

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -107,7 +107,7 @@ process.emit = function (name, data, ...args)
                     let packageName: string;
                     try
                     {
-                        packageName = fileURLToPath(normalize({ mode: 'import', relativeTo: '.' }, context.currentWorkingDirectory, plugin));
+                        packageName = fileURLToPath(normalize({ mode: 'import', relativeTo: context.options.configFile }, context.currentWorkingDirectory, plugin));
                     }
                     catch (e)
                     {
