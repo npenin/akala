f1d4c853d798e3160f3ac1b5d643876bfc15cb85
neonp <nicolas.penin@dragon-angel.fr>
2023-12-14 05:41:46 +0000

    fix: add source map flag when running cli

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env -S node --enable-source-maps
 import program, { ErrorMessage } from './router/index.js';
 import fs from 'fs/promises'
 import { fileURLToPath, pathToFileURL } from 'url'
