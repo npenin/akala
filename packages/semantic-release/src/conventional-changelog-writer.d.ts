942e4712fe706fa666a334d50423808915741ebc
neonp <nicolas.penin@dragon-angel.fr>
2023-12-15 19:27:59 +0000

    fix: update after the breaking change in conventional changelog

--- packages/semantic-release/src/conventional-changelog-writer.d.ts
+++ packages/semantic-release/src/conventional-changelog-writer.d.ts
@@ -4,5 +4,5 @@ import { Commit } from "conventional-commits-parser";
 
 declare module 'conventional-changelog-writer'
 {
-    export function parseArray(commits: Commit[], ...args: Parameters<typeof conventionalChangelogWriter>): string
+    export function parseArray(commits: Commit[], ...args: Parameters<typeof conventionalChangelogWriter>): Promise<string>
 }
