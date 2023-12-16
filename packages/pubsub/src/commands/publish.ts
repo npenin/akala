fc8465c9989b457a5d90fb72316e5acb435ffd15
neonp <nicolas.penin@dragon-angel.fr>
2023-12-16 08:23:36 +0000

    fix: build issue after update to @akala/core

--- packages/pubsub/src/commands/publish.ts
+++ packages/pubsub/src/commands/publish.ts
@@ -3,5 +3,5 @@ import { State } from "../state.js";
 
 export default function publish(this: State, topic: string, data: unknown)
 {
-    return mapAsync(this[topic], el => el.container.dispatch(el.commandToDispatch, data))
+    return mapAsync(this[topic], el => el.container.dispatch(el.commandToDispatch, data), true)
 }
