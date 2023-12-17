3a6dbe569562d8777b4c20e7448614f99589db84
neonp <nicolas.penin@dragon-angel.fr>
2023-12-17 08:21:31 +0000

    feat: switch from yarn version to manual bump

---
+++ packages/semantic-release/src/commands/apply-bump.ts
@@ -0,0 +1,22 @@
+import { CliContext } from "@akala/cli";
+import { Levels } from "./recommend-bump.js";
+import semver from 'semver'
+import { readFile, writeFile } from "fs/promises";
+import { resolve } from "path";
+
+type Workspace = { location: string, version: string, name: string, workspaceDependencies: string[], bump: keyof typeof Levels };
+
+
+
+export default async function (this: CliContext, workspace: Workspace)
+{
+    const version = semver.coerce(workspace.version);
+    if (workspace.bump != 'decline')
+        version[workspace.bump]++;
+
+    const path = workspace.location;
+    const pkg = JSON.parse(await readFile(resolve(path, './package.json'), 'utf-8'));
+    pkg.version = version.format();
+    await writeFile(resolve(path, './package.json'), JSON.stringify(pkg), 'utf-8');
+
+}
--- packages/semantic-release/src/commands/eliminate-private.ts
+++ packages/semantic-release/src/commands/eliminate-private.ts
@@ -6,7 +6,6 @@ export default async function (workspaces: { name: string, location: string }[])
     return Promise.all(workspaces.map(async workspace =>
     {
         const pkg = JSON.parse(await readFile(resolve(workspace.location, './package.json'), 'utf-8'));
-        console.log(pkg)
         return Object.assign(workspace, { private: pkg.private });
     })).then(workspaces => workspaces.filter(w => !w.private));
 }
