daa6af1d3080f4dbfc2778a7de0b75421854224d
neonp <nicolas.penin@dragon-angel.fr>
2024-01-30 07:27:08 +0000

    feat: allow more string "errors" in router

--- packages/core/src/router/shared.ts
+++ packages/core/src/router/shared.ts
@@ -1,20 +1,20 @@
 /* A middleware promise is the opposite of a promise which will fail when there is a result 
  * and which will succeed as long as there is no middleware that handled the context
  */
-export type MiddlewarePromise = Promise<MiddlewareResult>;
-export type MiddlewareResult = Error | SpecialNextParam | OptionsResponse;
+export type MiddlewarePromise<T extends string | void = SpecialNextParam> = Promise<MiddlewareResult<T>>;
+export type MiddlewareResult<T extends string | void = SpecialNextParam> = Error | T | OptionsResponse;
 
-export interface Middleware<T extends unknown[]> 
+export interface Middleware<T extends unknown[], TSpecialNextParam extends string | void = SpecialNextParam> 
 {
-    handle(...context: T): MiddlewarePromise;
+    handle(...context: T): MiddlewarePromise<TSpecialNextParam>;
 }
 
-export interface ErrorMiddleware<T extends unknown[]> 
+export interface ErrorMiddleware<T extends unknown[], U extends string | void = SpecialNextParam> 
 {
-    handleError(error: Error | OptionsResponse, ...context: T): MiddlewarePromise;
+    handleError(error: Exclude<MiddlewareResult<U>, U>, ...context: T): MiddlewarePromise<U>;
 }
 
-export type AnyMiddleware<T extends unknown[]> = Middleware<T> | ErrorMiddleware<T>;
+export type AnyMiddleware<T extends unknown[], TSpecialNextParam extends string | void = SpecialNextParam> = Middleware<T, TSpecialNextParam> | ErrorMiddleware<T, TSpecialNextParam>;
 
 export type OptionsResponse = { allow: string[] };
 
