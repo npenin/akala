aaec2abff5f80cfa09c89644eb56e204d1037a2d
neonp <nicolas.penin@dragon-angel.fr>
2024-02-24 10:30:39 +0000

    refactor: remove next in async each/map/grep
    BREAKING CHANGE: implementations using NextFunction will not work any longer.

--- packages/core/src/eachAsync.ts
+++ packages/core/src/eachAsync.ts
@@ -12,137 +12,42 @@ export class AggregateErrors extends Error
     }
 }
 
-export function array<T, U extends unknown[]>(array: T[] | ArrayLike<T>, body: (element: T, i: number, next: NextFunction<unknown, U>) => void | PromiseLike<void>, complete: NextFunction<Error, U, void | Promise<void>>, waitForPrevious: boolean): Promise<void>
-{
-    const promises: PromiseLike<unknown>[] = [];
-    const deferred = new Deferred<void>();
-    if (complete)
-    {
-        const oldComplete = complete;
-        complete = (e: Error, ...args) =>
-        {
-            try
-            {
-                oldComplete(e, ...args);
-                if (e)
-                    deferred.reject(e);
-                else
-                    deferred.resolve();
-            }
-            catch (e)
-            {
-                deferred.reject(e);
-            }
-        };
-    }
-    else
-        //eslint-disable-next-line @typescript-eslint/no-unused-vars
-        complete = (e: Error, ..._args: unknown[]) =>
-        {
-            if (e)
-                deferred.reject(e);
-            else
-                deferred.resolve();
-        }
-
-    const errors: Error[] = [];
-
-    function loop(i)
-    {
-        if (i == array.length)
-        {
-            Promise.all(promises).then(async (res) =>
-            {
-                try
-                {
-                    if (errors.length > 0)
-                        throw new AggregateErrors(errors);
-
-                    await (complete as unknown as SimpleNextFunction<Error>)()
-                    return res;
-                }
-                catch (e)
-                {
-                    (complete as unknown as SimpleNextFunction<Error>)(e);
-                }
-            }, err => (complete as unknown as SimpleNextFunction<Error>)(err));
-            return;
-        }
-        else
-            try
-            {
-                const promise = body(array[i], i, function (error?: Error, ...args: U)
-                {
-                    if (error)
-                        if (waitForPrevious)
-                            complete(error, ...args);
-                        else
-                            errors[i] = error;
-                    else
-                        setImmediate(loop, i + 1)
-                });
-                if (promise && isPromiseLike(promise))
-                {
-                    if (!waitForPrevious)
-                    {
-                        promises.push(promise);
-                        setImmediate(loop, i + 1);
-                    }
-                    else
-                        promise.then(() => setImmediate(loop, i + 1), complete as unknown as SimpleNextFunction<unknown>);
-                }
-            }
-            catch (e)
-            {
-                if (waitForPrevious)
-                    promises.push(Promise.reject(e));
-                else
-                    (complete as unknown as SimpleNextFunction<unknown>)(e);
-            }
-    }
-    loop(0);
-    return deferred;
-}
-
-export function object<T, U extends unknown[] = []>(o: T, body: (element: T[keyof T], i: keyof T, next?: NextFunction<unknown, U, void>) => void, complete: NextFunction<unknown, U, void | Promise<void>>, waitForPrevious: boolean): void | Promise<void>
+export function array<T, U extends unknown[]>(array: T[] | ArrayLike<T>, body: (element: T, i: number) => Promise<void>, waitForPrevious: boolean): Promise<void>
 {
     if (typeof waitForPrevious == 'undefined')
         waitForPrevious = true;
-    return array<keyof T, U>(Object.keys(o) as (keyof T)[], (key, i, next) => body(o[key], key, next), complete, waitForPrevious);
+
+    if (waitForPrevious)
+        if (Array.isArray(array))
+            return array.reduce((previous: Promise<void>, item, i) => previous.then(() => body(item, i)), Promise.resolve());
+        else
+            return Array.prototype.reduce.call(array, (previous: Promise<void>, item, i) => previous.then(() => body(item, i)), Promise.resolve()) as Promise<void>;
+    else
+        if (Array.isArray(array))
+            return Promise.all(array.map(body)).then(() => { });
+        else
+            return Promise.all(Array.prototype.map.call(array, body)).then(() => { });
 }
 
-export function each<T, TError, U extends unknown[]>(array: T[] | ArrayLike<T>, body: (element: T, i?: number, next?: NextFunction<TError, U>) => PromiseLike<void>, complete: NextFunction, waitForPrevious?: boolean): void
-export function each<T, TError, U extends unknown[]>(array: T[] | ArrayLike<T>, body: (element: T, i?: number, next?: NextFunction<TError, U>) => PromiseLike<void>, waitForPrevious?: boolean): PromiseLike<void>
-export function each<T, TError, U extends unknown[]>(array: T[] | ArrayLike<T>, body: (element: T, i: number, next: NextFunction<TError, U>) => void, complete: NextFunction, waitForPrevious?: boolean): void
-export function each<TError, U extends unknown[]>(o: Record<string, unknown>, body: (element: unknown, i: string, next: NextFunction<TError, U>) => void, complete: NextFunction, waitForPrevious?: boolean): void
-export function each<T, TError, U extends unknown[]>(array: T[] | ArrayLike<T>, body: (element: T, i: number, next: NextFunction<TError, U>) => void, waitForPrevious?: boolean): PromiseLike<void>
-export function each<TError, U extends unknown[], T = Record<string, unknown>>(o: T, body: (element: T[typeof i], i: keyof T, next: NextFunction<TError, U>) => void, waitForPrevious?: boolean): PromiseLike<void>
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function each(it: unknown[] | ArrayLike<unknown> | Record<string, unknown>, body: <TError, U extends unknown[]>(element: unknown, i: any, next: NextFunction<TError, U>) => void, complete?: NextFunction | boolean, waitForPrevious?: boolean): void | PromiseLike<void>
+export function object<T, U extends unknown[] = []>(o: T, body: (element: T[keyof T], i: keyof T) => Promise<void>, waitForPrevious: boolean): Promise<void>
 {
-    if (typeof complete === 'boolean')
-    {
-        waitForPrevious = complete;
-        complete = undefined;
-    }
+    if (typeof waitForPrevious == 'undefined')
+        waitForPrevious = true;
+    return array<keyof T, U>(Object.keys(o) as (keyof T)[], (key, i) => body(o[key], key), waitForPrevious);
+}
 
+export function each<T, TError, U extends unknown[]>(array: T[] | ArrayLike<T>, body: (element: T, i?: number) => Promise<void>, waitForPrevious?: boolean): Promise<void>
+export function each<TError, U extends unknown[], T = Record<string, unknown>>(o: T, body: (element: T[typeof i], i: keyof T,) => Promise<void>, waitForPrevious?: boolean): Promise<void>
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function each(it: unknown[] | ArrayLike<unknown> | Record<string, unknown>, body: <TError, U extends unknown[]>(element: unknown, i: any) => Promise<void>, waitForPrevious?: boolean): void | PromiseLike<void>
+{
     if (typeof waitForPrevious === 'undefined')
         waitForPrevious = true;
 
 
-    if (typeof complete !== 'boolean' && typeof complete !== 'undefined')
-    {
-        if (isArrayLike(it))
-            return array(it, body, complete, waitForPrevious);
-        return object(it, body, complete, waitForPrevious);
-
-    }
-    else
-    {
-        if (Array.isArray(it) || isArrayLike(it))
-            return array<unknown, []>(it, body, null, waitForPrevious);
-        return object<Record<string, unknown>, []>(it, body, null, waitForPrevious);
-    }
+    if (Array.isArray(it) || isArrayLike(it))
+        return array<unknown, []>(it, body, waitForPrevious);
+    return object<Record<string, unknown>, []>(it, body, waitForPrevious);
 }
 
 
@@ -152,7 +57,7 @@ export async function mapArray<T, U>(it: T[] | ArrayLike<T>, body: (element: T, 
     await array(it, async function (el, i)
     {
         result.push(await body(el, i));
-    }, null, waitForPrevious);
+    }, waitForPrevious);
     return result;
 }
 
@@ -170,7 +75,7 @@ export async function mapObject<TIn, TResultValue>(o: TIn, body: (element: TIn[k
             resultArray.push(await body(el, i));
         else
             result[i] = await body(el, i);
-    }, null, waitForPrevious);
+    }, waitForPrevious);
     if (asArray)
         return resultArray;
     return result;
@@ -191,25 +96,25 @@ export function map(it: any, body: (element: any, i: any) => any, asArray?: bool
 
 
 
-export async function grepArray<T>(it: T[] | ArrayLike<T>, body: (element: T, i: number) => Promise<boolean>, complete: NextFunction, waitForPrevious: boolean): Promise<T[]>
+export async function grepArray<T>(it: T[] | ArrayLike<T>, body: (element: T, i: number) => Promise<boolean>, waitForPrevious: boolean): Promise<T[]>
 {
     const result = [];
     await array(it, async function (el, i)
     {
         if (await body(el, i))
             result.push(el);
-    }, complete, waitForPrevious);
+    }, waitForPrevious);
     return result;
 }
 
 // export type Partial<T> = {[P in keyof T]?: T[P]}
 export type Proxy<T, U> = { [P in keyof T]: U }
 
-export function grepObject<T>(o: T, body: (element: T[keyof T], i: keyof T) => Promise<boolean>, asArray: true, complete: NextFunction, waitForPrevious: boolean): Promise<T[keyof T][]>
-export function grepObject<T>(o: T, body: (element: T[keyof T], i: keyof T) => Promise<boolean>, asArray: false, complete: NextFunction, waitForPrevious: boolean): Promise<Partial<T>>
-export function grepObject<T>(o: T, body: (element: T[keyof T], i: keyof T) => Promise<boolean>, asArray: boolean, complete: NextFunction, waitForPrevious: boolean): Promise<Partial<T> | T[keyof T][]>
+export function grepObject<T>(o: T, body: (element: T[keyof T], i: keyof T) => Promise<boolean>, asArray: true, waitForPrevious: boolean): Promise<T[keyof T][]>
+export function grepObject<T>(o: T, body: (element: T[keyof T], i: keyof T) => Promise<boolean>, asArray: false, waitForPrevious: boolean): Promise<Partial<T>>
+export function grepObject<T>(o: T, body: (element: T[keyof T], i: keyof T) => Promise<boolean>, asArray: boolean, waitForPrevious: boolean): Promise<Partial<T> | T[keyof T][]>
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function grepObject<T>(o: T, body: (element: T[keyof T], i: keyof T) => Promise<boolean>, asArray: boolean, complete: NextFunction, waitForPrevious: boolean)
+export async function grepObject<T>(o: T, body: (element: T[keyof T], i: keyof T) => Promise<boolean>, asArray: boolean, waitForPrevious: boolean)
 {
     const result: Partial<T> = {};
     const resultArray: T[keyof T][] = [];
@@ -221,19 +126,19 @@ export async function grepObject<T>(o: T, body: (element: T[keyof T], i: keyof T
             else
                 result[i] = el;
 
-    }, complete, waitForPrevious);
+    }, waitForPrevious);
     if (asArray)
         return resultArray;
     return result;
 }
 
-export function grep<T>(array: T[] | ArrayLike<T>, body: (element: T, i: number) => Promise<boolean>, complete?: NextFunction, waitForPrevious?: boolean): Promise<T[]>
-export function grep<T>(o: T, body: <U extends keyof T>(element: T[U], i: U) => Promise<boolean>, complete?: NextFunction, waitForPrevious?: boolean): Promise<Partial<T>>
-export function grep<T, U extends keyof T>(o: T, body: (element: T[U], i: U) => Promise<boolean>, asArray: true, complete?: NextFunction, waitForPrevious?: boolean): Promise<T[U][]>
+export function grep<T>(array: T[] | ArrayLike<T>, body: (element: T, i: number) => Promise<boolean>, waitForPrevious?: boolean): Promise<T[]>
+export function grep<T>(o: T, body: <U extends keyof T>(element: T[U], i: U) => Promise<boolean>, waitForPrevious?: boolean): Promise<Partial<T>>
+export function grep<T, U extends keyof T>(o: T, body: (element: T[U], i: U) => Promise<boolean>, asArray: true, waitForPrevious?: boolean): Promise<T[U][]>
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
-export function grep(it: any, body: (element: any, i: any) => Promise<boolean>, asArray?: boolean | NextFunction, complete?: NextFunction | boolean, waitForPrevious?: boolean)
+export function grep(it: any, body: (element: any, i: any) => Promise<boolean>, asArray?: boolean, waitForPrevious?: boolean)
 {
     if (isArrayLike(it))
-        return grepArray(it, body, asArray as NextFunction, !!complete);
-    return grepObject(it, body, asArray as boolean, complete as NextFunction, !!waitForPrevious);
+        return grepArray(it, body, asArray);
+    return grepObject(it, body, asArray, waitForPrevious);
 }
--- packages/core/src/test/eachAsync.ts
+++ packages/core/src/test/eachAsync.ts
@@ -1,30 +1,17 @@
 import assert from "assert";
 import { each } from "../eachAsync.js";
+import { delay } from "../promiseHelpers.js";
 
 var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function promiseTimeout(delay: number)
-{
-    return new Promise((resolve) =>
-    {
-        setTimeout(resolve, delay);
-    })
-}
-
 describe('testing each async', function ()
 {
-    it('should accept callbacks', function (done)
+    it('should support mixing promises and callback return', function (done)
     {
         this.timeout(10000);
         const result = [];
-        each(array, (x, _i, next) => setTimeout(() =>
+        each(array, (x) => delay(Math.random() * 1000).then(() => { result.push(x + 1) })).then(done, e =>
         {
-            result.push(x + 1);
-            next();
-        }, Math.random() * 1000), function (e)
-        {
-            if (e)
-                return done(e);
             try
             {
                 assert.strictEqual(result.length, array.length);
@@ -35,48 +22,7 @@ describe('testing each async', function ()
             {
                 done(e);
             }
-        })
-    });
-
-    it('should work with callbacks and errors', function (done)
-    {
-        this.timeout(10000);
-        const result = [];
-        each(array, (x, _i, next) => setTimeout(() =>
-        {
-            result.push(x + 1);
-            if (x > 7)
-                next(new Error('expected'));
-            else
-                next();
-        }, Math.random() * 1000), function (e: Error)
-        {
-            if (e && e.message == 'expected')
-                return done();
-            done(e || new Error('no error received in callback'));
-        })
-    });
-
-    it('should support mixing promises and callback return', function (done)
-    {
-        this.timeout(10000);
-        const result = [];
-        each(array, (x) => promiseTimeout(Math.random() * 1000).then(() => result.push(x + 1)),
-            function (e)
-            {
-                if (e)
-                    return done(e);
-                try
-                {
-                    assert.strictEqual(result.length, array.length);
-                    assert.strictEqual(result.reduce((p, c) => p + c, 0), array.reduce((p, c) => p + 1 + c, 0));
-                    done();
-                }
-                catch (e)
-                {
-                    done(e);
-                }
-            });
+        });
     });
 
 
@@ -84,11 +30,10 @@ describe('testing each async', function ()
     {
         this.timeout(10000);
         const result = [];
-        await each(array, (x, _i, next) => setTimeout(() =>
+        await each(array, (x, _i) => delay(Math.random() * 1000).then(() =>
         {
             result.push(x + 1);
-            next();
-        }, Math.random() * 1000));
+        }));
 
         assert.strictEqual(result.length, array.length);
         assert.strictEqual(result.reduce((p, c) => p + c, 0), array.reduce((p, c) => p + 1 + c, 0));
@@ -99,7 +44,7 @@ describe('testing each async', function ()
         this.timeout(10000);
 
         const result = [];
-        await each(array, (x) => promiseTimeout(Math.random() * 1000).then(() => result.push(x + 1)))
+        await each(array, (x) => delay(Math.random() * 1000).then(() => { result.push(x + 1) }))
         assert.strictEqual(result.length, array.length);
         assert.strictEqual(result.reduce((p, c) => p + c, 0), array.reduce((p, c) => p + 1 + c, 0));
     })
@@ -111,7 +56,7 @@ describe('testing each async', function ()
         const result = [];
         await assert.rejects(new Promise((resolve, reject) =>
         {
-            each(array, (x) => promiseTimeout(Math.random() * 1000).then(() =>
+            each(array, (x) => delay(Math.random() * 1000).then(() =>
             {
                 result.push(x + 1)
                 if (x > 7)
