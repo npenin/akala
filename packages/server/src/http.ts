a20906337c8285441b67d2a98af2a751bb0db124
neonp <nicolas.penin@dragon-angel.fr>
2024-03-23 06:55:27 +0000

    fix: switch from xml2js to fast-xml-parser

--- packages/server/src/http.ts
+++ packages/server/src/http.ts
@@ -1,20 +1,11 @@
 import { FetchHttp, Http as coreHttp } from '@akala/core';
-import * as xml2js from 'xml2js'
+import xml from 'fast-xml-parser'
 
 FetchHttp.prototype['getXML'] = function (this: FetchHttp, url: string)
 {
     return this.call({ url: url, method: 'get' }).then(r => r.text().then(text =>
     {
-        return new Promise((resolve, reject) =>
-        {
-            xml2js.parseString(text, { async: true }, function (err, result)
-            {
-                if (err)
-                    reject(err);
-                else
-                    resolve(result);
-            });
-        })
+        return new xml.XMLParser().parse(text);
     }))
 }
 
