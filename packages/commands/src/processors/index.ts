bfde876d60ab75aaa33382187892bf2d67388b0a
neonp <nicolas.penin@dragon-angel.fr>
2024-04-09 17:50:21 +0000

    feat: allow both jsonrpc and jsonrpcbrowser to co-exist

--- packages/commands/src/processors/index.ts
+++ packages/commands/src/processors/index.ts
@@ -1,5 +1,4 @@
 // export * from './http-client'
 export * from './index.browser.js'
-import { JsonRpc, handler } from './jsonrpc.js'
-export { JsonRpc, handler }
+export * from './jsonrpc.js'
 export * from './fs.js'
