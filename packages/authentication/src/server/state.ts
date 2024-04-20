6e436ac61642c067599ce5f0e1ca8ff948390c1b
neonp <nicolas.penin@dragon-angel.fr>
2024-04-20 10:20:26 +0000

    fix: get hash has conterpart verify since the switch to sign

--- packages/authentication/src/server/state.ts
+++ packages/authentication/src/server/state.ts
@@ -3,7 +3,8 @@ import { AuthenticationStore } from './authentication-store.js';
 export interface State
 {
     router: import("@akala/server").HttpRouter;
-    getHash: (value: string) => Promise<string>;
+    getHash: (value: string, salt?: Uint8Array) => Promise<string>;
+    verifyHash: (value: string, signature: BufferSource, salt?: Uint8Array) => Promise<boolean>;
     store: AuthenticationStore;
     session: {
         slidingExpiration?: number
