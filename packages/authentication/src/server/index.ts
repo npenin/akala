6fd28e85c717767293e51f2fe179c8ff84d0f785
neonp <nicolas.penin@dragon-angel.fr>
2024-05-06 04:27:10 +0000

    fix: update after core breaking change

--- packages/authentication/src/server/index.ts
+++ packages/authentication/src/server/index.ts
@@ -1,14 +1,13 @@
-import * as akala from '@akala/core';
 import * as server from '@akala/server';
 import { NonPublicMiddleware } from './middlewares/authorize.js';
-
+import { module } from '@akala/core'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const moduleName = require('../../package.json').name;
 
 export const isAuthorized = new NonPublicMiddleware();
 
-akala.module(moduleName).ready(['$router'], function (router: server.HttpRouter)
+module(moduleName).ready(['$router'], function (router: server.HttpRouter)
 {
     router.get('/api/me', function (req)
     {
