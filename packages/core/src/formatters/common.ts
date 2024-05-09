883708fae46180d96b8beb28d1ea2a1ab6eb40ae
neonp <nicolas.penin@dragon-angel.fr>
2024-05-09 17:19:22 +0000

    feat: allow reversible formatters

--- packages/core/src/formatters/common.ts
+++ packages/core/src/formatters/common.ts
@@ -2,6 +2,7 @@ import { Injected } from "../injectors/shared.js";
 import { ParsedOneOf } from "../parser/parser.js";
 
 export type Formatter<TResult> = Injected<TResult>;// (value: unknown) => TResult;
+export type ReversibleFormatter<TResult, TOrigin> = Injected<TResult & { reverse: (value: TResult) => TOrigin }>;// (value: unknown) => TResult;
 
 export interface FormatterFactory<TResult, TSettings = ParsedOneOf>
 {
