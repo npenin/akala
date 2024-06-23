d581037943f02eb6bc70d298570899a7033c4234
npenin <github@dragon-angel.fr>
2024-06-23 08:35:29 +0200

    fix: update after base64 update

--- packages/authentication/src/server/state.ts
+++ packages/authentication/src/server/state.ts
@@ -4,7 +4,7 @@ export interface State
 {
     router: import("@akala/server").HttpRouter;
     getHash: (value: string, salt?: Uint8Array) => Promise<string>;
-    verifyHash: (value: string, signature: BufferSource, salt?: Uint8Array) => Promise<boolean>;
+    verifyHash: (value: string, signature: BufferSource, salt?: ArrayBuffer) => Promise<boolean>;
     store: AuthenticationStore;
     session: {
         slidingExpiration?: number
