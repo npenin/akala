b8613d5eb4e1f04eae9b2d751ee7bfe8ff5ceb61
npenin <github@dragon-angel.fr>
2024-07-18 08:19:44 +0200

    feat: add json formatter

--- packages/core/src/formatters/index.ts
+++ packages/core/src/formatters/index.ts
@@ -6,6 +6,13 @@ export * from './call.js';
 export * from './common.js'
 export { identity, negate, booleanize };
 import { module } from '../helpers.js';
+import json from './json.js';
+import date from './date.js';
+
 
 module('$formatters').register('#not', negate);
 module('$formatters').register('#bool', booleanize);
+module('$formatters').register('#json', json);
+module('$formatters').register('#date', date);
+
+export const formatters = module('$formatters');
