c2c620431e20d21727be8cc54a2487369cf47fda
npenin <github@dragon-angel.fr>
2024-07-26 03:15:15 +0200

    fix: improve session management based on socket lifetime

--- packages/authentication/src/server/commands/whoami.ts
+++ packages/authentication/src/server/commands/whoami.ts
@@ -1,9 +1,16 @@
 import { BinaryOperator } from "@akala/core/expressions";
 import { State } from "../state.js";
+import { validateSessionOwner } from "./login.js";
+import { Session } from "../../model/session.js";
 
-export default async function (this: State, sessionId: string)
+export default async function (this: State, trigger: string, sessionId: string, sessionSignature: string)
 {
-    const session = await this.store.Session.where('id', BinaryOperator.Equal, sessionId).firstOrDefault();
+    console.log(arguments);
+    let session: Session;
+    if (trigger != 'jsonrpc')
+        session = await validateSessionOwner(this, sessionId, sessionSignature);
+    else
+        session = await this.store.Session.where('id', BinaryOperator.Equal, sessionId).firstOrDefault();
 
     const user = await this.store.User.where('id', BinaryOperator.Equal, session.userId).firstOrDefault()
 
