82c92b4e1bc3f6c4f8268029ab5aac33736c78e8
npenin <github@dragon-angel.fr>
2024-07-31 07:40:56 +0200

    fix: improve call expression support
    BREAKING CHANGE: call expression now has a typedexpression as method and not a constant anymore

--- packages/core/src/parser/expressions/call-expression.ts
+++ packages/core/src/parser/expressions/call-expression.ts
@@ -7,7 +7,7 @@ export class CallExpression<T, TMethod extends keyof T> extends Expression
 {
     public get type(): ExpressionType.CallExpression { return ExpressionType.CallExpression; }
     public readonly arguments: Expressions[];
-    constructor(public readonly source: TypedExpression<T>, public readonly method: TMethod, args: StrictExpressions[])
+    constructor(public readonly source: TypedExpression<T>, public readonly method: TypedExpression<TMethod>, args: StrictExpressions[])
     {
         super();
         this.arguments = args;
