79f5e6eeaea26dcefcbaa1224325b57bc89823f0
npenin <github@dragon-angel.fr>
2024-08-01 22:04:12 +0200

    feat: add generate ts from schema command generated jsonschema, oas20, oas30 and oas31 from schema

--- packages/commands/src/cli/openapi.ts
+++ packages/commands/src/cli/openapi.ts
@@ -7,6 +7,9 @@ import { outputHelper, write } from './new.js';
 import { Writable } from 'stream';
 import { promisify } from 'util';
 import { redirectSchema, simplifySchema } from './generate-schema.js';
+import { OpenApi30 } from '../oas30.js';
+import { OpenApi31 } from '../oas31.js';
+import { OpenApi20 } from '../oas20.js';
 
 export default async function (pathToOpenApiFile: string | URL, outputFile?: string)
 {
@@ -37,10 +40,56 @@ export default async function (pathToOpenApiFile: string | URL, outputFile?: str
     }
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const openApi = typeof content == 'string' ? JSON.parse(content) : content;
+    const openApi: OpenApi30 | OpenApi31 | OpenApi20 = typeof content == 'string' ? JSON.parse(content) : content;
 
+    if (isOpenApi3(openApi))
+    {
+        openApi.components.schemas && Object.values(openApi.components.schemas).forEach(s => redirectSchema(s, '#/components/', '#/$defs/'));
+        // Object.values(openApi.components.requestBodies).forEach(s => Objects.content redirectSchema(, '#/components/', '#/$defs/'));
+        openApi.components.responses && Object.values(openApi.components.responses).forEach(s => redirectSchema(s, '#/components/', '#/$defs/'));
+        // const baseUri = new URL(openApi.schemes[0] + '://' + openApi.host + openApi.basePath);
+        const commands: Command[] = Object.entries(openApi.paths).flatMap(([path, requests]) =>
+            Object.entries(requests).map(([method, request]) =>
+            {
+                const parameters = Object.fromEntries(request.parameters.map(p => [p.in + '.' + p.name, p]))
+                const needsSchema = !!request.parameters.find(p => p.in == 'body') || request.responses['200'].schema;
 
-    if (openApi.swagger == '2.0')
+                return simplifySchema({
+                    name: request.operationId,
+                    config: {
+                        schema: {
+                            $defs: needsSchema ? { ...openApi.components, ...Object.fromEntries(request.parameters.filter(p => p.in == 'body').map(p => ['body.' + p.name, p.schema])) } : undefined,
+                            inject: request.parameters.map(p => p.in == 'body' ? 'body.' + p.name : p.type),
+                            resultSchema: redirectSchema(request.responses['200'].schema, '#/components/', '#/$defs/')
+                        },
+                        doc: {
+                            inject: Object.keys(parameters),
+                            description: request.description,
+                            options: Object.fromEntries(Object.entries(parameters).map(e => [e[0], e[1].description]))
+                        },
+                        http: {
+                            method,
+                            type: getType20(openApi, request),
+                            route: path,
+                            inject: Object.keys(parameters),
+                        },
+                        auth: {
+                            http: getAuth20(openApi, request)
+                        }
+                    }
+                } as Command)
+            })
+        );
+        const result = { commands, name: openApi.info.title.replace(/\./g, '-') } as Container;
+        if (outputFile)
+        {
+            await write(output, JSON.stringify(result, null, 4));
+            await promisify(cb => output.end(cb))();
+        }
+        else
+            return result;
+    }
+    else if (openApi.swagger == '2.0')
     {
         Object.values(openApi.definitions).forEach(s => redirectSchema(s, '#/definitions/', '#/$defs/'));
         // const baseUri = new URL(openApi.schemes[0] + '://' + openApi.host + openApi.basePath);
@@ -87,6 +136,11 @@ export default async function (pathToOpenApiFile: string | URL, outputFile?: str
     }
     else
         console.log(openApi);
+}
+
+function isOpenApi3(x: object): x is OpenApi30 | OpenApi31
+{
+    return 'openapi' in x && x.openapi > '3.';
 }
 
 function getType20(openApi: any, request: any): "json" | "xml" | "text" | "raw"
