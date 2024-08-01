cdf30c7b7725a3095233e7f0f65620af829ab50a
npenin <github@dragon-angel.fr>
2024-08-01 10:18:23 +0200

    refactor: update definitions

--- packages/client-compiler/src/container.ts
+++ packages/client-compiler/src/container.ts
@@ -1,3 +1,4 @@
+import {Metadata, ICommandProcessor, Container, registerCommands} from '@akala/commands';
 //eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-ignore 6133
 //eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -15,6 +16,13 @@ namespace commands
 		'add-loader'(...args: [Argument0<typeof import('./commands/add-loader.js').default>, Argument1<typeof import('./commands/add-loader.js').default>]): ReturnType<typeof import('./commands/add-loader.js').default>
 		'compile'(...args: []): ReturnType<typeof import('./commands/compile.js').default>
 	}
+   export const meta={"name":"@akala/client-compiler","commands":[{"name":"add-loader","config":{"fs":{"inject":["param.0","param.1"],"path":"dist/esm/commands/add-loader.js","source":"src/commands/add-loader.ts"},"":{"inject":["param.0","param.1"]},"cli":{"inject":["options.type","options.path"],"usage":"add-loader <type> <path>","options":{"path":{"normalize":true}}}}},{"name":"compile","config":{"fs":{"inject":["param"],"path":"dist/esm/commands/compile.js","source":"src/commands/compile.ts"},"":{"inject":["param"]},"cli":{"usage":"compile [...entrypoints]","inject":["context.state","options.entrypoints"],"options":{"entrypoints":{"normalize":true}}}}}]} as Metadata.Container;
+
+   export function connect(processor?:ICommandProcessor) {
+        const container = new Container<void>("commands", void 0);
+        registerCommands(meta.commands, processor, container);
+        return container as container & Container<void>;
+    }
 }
 
 export { commands as default };
--- packages/cron/src/container.ts
+++ packages/cron/src/container.ts
@@ -1,3 +1,4 @@
+import {Metadata, ICommandProcessor, Container, registerCommands} from '@akala/commands';
 //eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-ignore 6133
 //eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -27,6 +28,13 @@ namespace commands
 		'remove-job-schedule'(...args: [Argument0<typeof import('./commands/remove-job-schedule.js').default>, Argument1<typeof import('./commands/remove-job-schedule.js').default>]): ReturnType<typeof import('./commands/remove-job-schedule.js').default>
 		'wait'(...args: [Argument0<typeof import('./commands/wait.js').default>]): ReturnType<typeof import('./commands/wait.js').default>
 	}
+   export const meta={"name":"@akala/cron","commands":[{"name":"$init","config":{"fs":{"path":"dist/esm/commands/$init.js","source":"src/commands/$init.ts","inject":[]},"":{"inject":[]}}},{"name":"add-job","config":{"fs":{"path":"dist/esm/commands/add-job.js","source":"src/commands/add-job.ts","inject":["param.0","param.1","param.2"]},"":{"inject":["param.0","param.1","param.2"]}}},{"name":"add-job-schedule","config":{"fs":{"path":"dist/esm/commands/add-job-schedule.js","source":"src/commands/add-job-schedule.ts","inject":["param.0","param.1"]},"":{"inject":["param.0","param.1"]}}},{"name":"add-schedule","config":{"fs":{"path":"dist/esm/commands/add-schedule.js","source":"src/commands/add-schedule.ts","inject":["param.0","param.1"]},"":{"inject":["param.0","param.1"]}}},{"name":"list-jobs","config":{"fs":{"path":"dist/esm/commands/list-jobs.js","source":"src/commands/list-jobs.ts","inject":[]},"":{"inject":[]}}},{"name":"list-schedules","config":{"fs":{"path":"dist/esm/commands/list-schedules.js","source":"src/commands/list-schedules.ts","inject":[]},"":{"inject":[]}}},{"name":"remove-job-schedule","config":{"fs":{"path":"dist/esm/commands/remove-job-schedule.js","source":"src/commands/remove-job-schedule.ts","inject":["param.0","param.1"]},"":{"inject":["param.0","param.1"]}}},{"name":"wait","config":{"fs":{"path":"dist/esm/commands/wait.js","source":"src/commands/wait.ts","inject":["param.0"]},"":{"inject":[]},"cli":{"standalone":true,"inject":["options.riseSet","options.minutes","options.hour","options.day","options.month","options.lat","options.lng","options.tz"]}}}]} as Metadata.Container;
+
+   export function connect(processor?:ICommandProcessor) {
+        const container = new Container<void>("commands", void 0);
+        registerCommands(meta.commands, processor, container);
+        return container as container & Container<void>;
+    }
 }
 
 export { commands as default };
