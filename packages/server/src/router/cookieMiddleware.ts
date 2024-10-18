411f96b4de726a818ff7752dce1c97f387ae79af
npenin <github@dragon-angel.fr>
2024-10-18 06:16:19 +0200

    build: fix after upgrade

--- packages/server/src/router/cookieMiddleware.ts
+++ packages/server/src/router/cookieMiddleware.ts
@@ -4,7 +4,7 @@ import cookie from 'cookie'
 
 export class CookieMiddleware implements MiddlewareAsync<[Request, Response, ...unknown[]]>
 {
-    constructor(private options: cookie.CookieParseOptions)
+    constructor(private options: cookie.ParseOptions)
     {
 
     }
