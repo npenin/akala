46e449771ede6b0641eec2083c4019a7f3680de7
npenin <github@dragon-angel.fr>
2024-10-21 08:37:32 +0200

    feat: add env with -S support equivalent

--- docker/ash.sh
+++ docker/ash.sh
@@ -1,3 +1,59 @@
 #!/bin/sh
 
-$*
+cd=
+unset=
+
+die() { echo "$*" 1>&2 ; exit 1; }
+
+parseArgs() {
+    while [ ! -z $1 ]
+    do
+        case $1 in
+            -u)
+                unset $2
+                shift;
+            ;;
+            --unset=*)
+                unset ${1#--unset=}
+            ;;
+            -C)
+                cd=$2
+                shift;
+            ;;--chdir=*)
+                cd=${1#--chdir=}
+            ;;
+            -S)
+                shift;
+                cli=$*
+                break
+            ;;
+            --split-string=*)
+                cli=$cli ${1#--split-string=}
+                shift;
+                cli=$cli $*;
+                break
+                ;;
+            -*)
+                echo $1;
+                die 'not supported'
+                ;;
+            *)
+                cli="$cli $*";
+                break;
+                ;;
+        esac
+        shift;
+    done;
+}
+
+parseArgs $*
+
+# echo $cd
+# echo $cli
+# echo $PATH
+if [ -z "$cd" ]
+then
+    $cli
+else
+    (cd $cd && $cli)
+fi
