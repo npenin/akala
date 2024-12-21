80341367b99e86d7b7a5167479898ec4d43a2f4f
npenin <github@dragon-angel.fr>
2024-12-21 16:49:39 +0100

    chore: dependency cleanup

--- packages/vite/.postcssrc.mjs
+++ packages/vite/.postcssrc.mjs
@@ -1,5 +1,3 @@
-// import modules from "postcss-modules";
-// import dtcg from "@csstools/postcss-design-tokens";
 import akala from "@akala/web-ui/postcss";
 import fullCompose from "@akala/web-ui/postcss-full-compose";
 import customMedia from "postcss-custom-media";
