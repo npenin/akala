290dae22e360ea8c308efe7d40d234458c818870
npenin <github@dragon-angel.fr>
2025-02-19 05:46:39 +0100

    feat: infer Error message from HttpStatusCode

--- packages/core/src/errorWithStatus.ts
+++ packages/core/src/errorWithStatus.ts
@@ -4,8 +4,35 @@ export class ErrorWithStatus extends Error
 {
     constructor(public readonly statusCode: HttpStatusCode | number, message?: string)
     {
-        super(message);
+        super(message || getMessageFromStatusCode(statusCode));
     }
 }
 
 export default ErrorWithStatus;
+
+export function getMessageFromStatusCode(statusCode: HttpStatusCode): string
+{
+    switch (statusCode)
+    {
+        case HttpStatusCode.BadRequest:
+        case HttpStatusCode.MethodNotAllowed:
+            return 'Invalid operation';
+        case HttpStatusCode.Unauthorized:
+            return 'Unauthorized access'
+        case HttpStatusCode.Forbidden:
+            return 'Forbidden'
+        case HttpStatusCode.NotFound:
+            return 'Not found';
+        case HttpStatusCode.NotAcceptable:
+            return 'Not acceptable';
+        case HttpStatusCode.RequestTimeout:
+            return 'Timeout';
+        case HttpStatusCode.Conflict:
+        case HttpStatusCode.Locked:
+        case HttpStatusCode.InternalServerError:
+        case HttpStatusCode.NotImplemented:
+        case HttpStatusCode.ServiceUnavailable:
+        case HttpStatusCode.InsufficientStorage:
+            return HttpStatusCode[statusCode];
+    }
+}
