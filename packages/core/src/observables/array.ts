006290f5c2455d4fae172ac165929a405c31464d
npenin <github@dragon-angel.fr>
2025-02-19 05:37:41 +0100

    feat: add new methods on ObservableArray

--- packages/core/src/observables/array.ts
+++ packages/core/src/observables/array.ts
@@ -1,4 +1,4 @@
-import { Event } from "../event-emitter.js";
+import { Event, Subscription } from "../event-emitter.js";
 
 export type ObservableArrayPopEvent<T> =
     {
@@ -73,21 +73,22 @@ export class ObservableArray<T> extends Event<[ObservableArrayEventMap<T>], void
     }
 
 
-    public shift()
+    public shift(count: number = 1)
     {
-        const item = this.array.shift();
+        const items = this.array.splice(0, count);
         this.emit({
             action: 'shift',
-            oldItems: [item]
+            oldItems: items
         });
     }
-    public pop()
+    public pop(count: number = 1)
     {
-        const item = this.array.pop();
+        const items = this.array.splice(this.array.length - count, count);
         this.emit({
             action: 'pop',
-            oldItems: [item]
+            oldItems: items
         });
+        return items;
     }
     public unshift = function (...items)
     {
@@ -97,13 +98,23 @@ export class ObservableArray<T> extends Event<[ObservableArrayEventMap<T>], void
             newItems: items
         });
     };
-    public replace(index, item)
+
+    public replace(index: number, item: T)
     {
-        const oldItem = this.array[index];
-        this.array['replace'](index, item);
+        return this.replaceN([{ index, item }]);
+    }
+
+    public replaceN(x: { index: number, item: T }[])
+    {
         this.emit({
             action: 'replace',
-            replacedItems: [{ index: index, oldItem: oldItem, newItem: item }],
+            replacedItems:
+                x.map(({ index, item }) =>
+                {
+                    const oldItem = this.array[index];
+                    this.array.splice(index, 1, item);
+                    return { index: index, newItem: item, oldItem };
+                }),
         });
     }
 
@@ -115,6 +126,90 @@ export class ObservableArray<T> extends Event<[ObservableArrayEventMap<T>], void
         });
     }
 
+    private subcription?: Subscription;
+
+    public splice(start: number, deleteCount?: number, ...replaceItems: T[])
+    {
+        if (deleteCount === (replaceItems?.length ?? 0))
+        {
+            const oldItems = this.array.splice(start, deleteCount, ...replaceItems);
+            this.emit({
+                action: 'replace',
+                replacedItems: oldItems.map((oldItem, i) => ({
+                    oldItem,
+                    newItem: replaceItems[i],
+                    index: i + start
+                }))
+            })
+            return oldItems;
+        }
+        else if (deleteCount < (replaceItems?.length ?? 0))
+        {
+            const oldItems = this.splice(start, deleteCount, ...replaceItems.slice(0, deleteCount));
+            this.push(...replaceItems.slice(deleteCount));
+            return oldItems;
+        }
+        else
+        {
+            const oldItems = this.splice(start, replaceItems.length, ...replaceItems);
+            oldItems.push(...this.pop(deleteCount - replaceItems.length));
+            return oldItems;
+        }
+    }
+
+    public replaceArray(values: T[] | ObservableArray<T>)
+    {
+        this.subcription?.();
+
+        const array = Array.isArray(values) ? values : values.array;
+        this.splice(0, this.length, ...array);
+
+        // this.replaceN(array.filter((_, i) => i < this.array.length).map((v, i) => ({
+        //     index: i, item: v
+        // })))
+
+        // if (this.length < values.length)
+        // {
+        //     if (Array.isArray(values))
+        //         this.push(...values.slice(this.length));
+        //     else
+        //         this.push(...values.array.slice(this.length));
+        // }
+        // else if (this.length > values.length)
+        // {
+        //     this.pop(this.length - array.length)
+        // }
+
+        if (!Array.isArray(values))
+            this.subcription = values.addListener(ev =>
+            {
+                switch (ev.action)
+                {
+                    case "pop":
+                        for (let i = 0; i < ev.oldItems.length; i++)
+                            this.pop()
+                        break;
+                    case "push":
+                        this.push(...ev.newItems)
+                        break;
+                    case "shift":
+                        for (let i = 0; i < ev.oldItems.length; i++)
+                            this.shift()
+                        break;
+                    case "unshift":
+                        this.unshift(...ev.newItems)
+                        break;
+                    case "replace":
+                        for (let i = 0; i < ev.replacedItems.length; i++)
+                            this.replace(ev.replacedItems[i].index, ev.replacedItems[i].newItem);
+                        break;
+                    case "init":
+                        this.replaceArray(ev.newItems);
+                        break;
+                }
+            })
+    }
+
     public indexOf(searchElement: T, fromIndex?: number): number
     public indexOf(...args: Parameters<typeof Array.prototype.indexOf>): ReturnType<typeof Array.prototype.indexOf>
     {
@@ -124,5 +219,11 @@ export class ObservableArray<T> extends Event<[ObservableArrayEventMap<T>], void
     public toString(): string
     {
         return this.array.toString();
+    }
+
+    public [Symbol.dispose]()
+    {
+        this.subcription?.();
+        super[Symbol.dispose]();
     }
 }
