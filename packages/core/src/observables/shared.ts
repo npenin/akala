ce3b5383aa7c1b18d07816d8b198d56f379e0fb3
npenin <github@dragon-angel.fr>
2025-02-21 06:38:30 +0000

    refactor: move pipe and reply events to event-emitter

--- packages/core/src/observables/shared.ts
+++ packages/core/src/observables/shared.ts
@@ -1,78 +1,7 @@
-import { Event, IEventSink } from "../event-emitter.js";
+import { Event, IEventSink, PipeEvent, ReplayEvent } from "../event-emitter.js";
 
 function noop() { }
 
-export class PipeEvent<T extends unknown[], U extends unknown[], TReturnType, TOptions extends { once?: boolean }> extends Event<U, TReturnType, TOptions>
-{
-    protected subscription: ReturnType<IEventSink<T, TReturnType, TOptions>['addListener']>;
-
-    constructor(protected readonly source: IEventSink<T, TReturnType, TOptions>, private readonly map: (...args: T) => U, combineResults: (results: TReturnType[]) => TReturnType)
-    {
-        super(source.maxListeners, combineResults);
-    }
-
-    protected subscribeToSourceIfRequired()
-    {
-        if (!this.subscription)
-            this.subscription = this.source.addListener((...args) => super.emit(...this.map(...args)));
-    }
-
-    addListener(listener: (...args: U) => TReturnType, options?: TOptions): () => boolean
-    {
-        this.subscribeToSourceIfRequired();
-        return super.addListener(listener, options);
-    }
-
-    removeListener(listener: (...args: U) => TReturnType): boolean
-    {
-        const result = super.removeListener(listener);
-        if (result && !this.hasListeners && this.subscription)
-        {
-            this.subscription();
-            this.subscription = null;
-        }
-        return result;
-    }
-
-    // public pipe<V extends unknown[]>(map: (...args: U) => V,)
-    // {
-    //     return new PipeEvent(this, map, this.combineReturnTypes);
-    // }
-}
-
-export class ReplayEvent<T extends unknown[], TReturnType> extends Event<T, TReturnType>
-{
-    private buffer: T[];
-
-    constructor(private bufferLength: number, maxListeners: number, combineReturnTypes?: (args: TReturnType[]) => TReturnType)
-    {
-        super(maxListeners, combineReturnTypes);
-    }
-
-    emit(...args: T): TReturnType
-    {
-        this.buffer.push(args);
-        while (this.buffer.length > this.bufferLength)
-            this.buffer.shift();
-        return super.emit(...args);
-    }
-
-    addListener(listener: (...args: T) => TReturnType, options?: { once?: boolean; }): () => boolean
-    {
-        if (options.once && this.buffer.length > 0)
-        {
-            listener(...this.buffer[0]);
-            return () => true;
-        }
-        this.buffer.forEach(args => listener(...args));
-        return super.addListener(listener, options);
-    }
-
-    // public pipe<U extends unknown[]>(map: (...args: T) => U)
-    // {
-    //     return new PipeEvent(this, map, noop);
-    // }
-}
 
 export class ReplayAsyncEvent<T extends unknown[], TReturnType> extends ReplayEvent<T, Promise<TReturnType>>
 {
