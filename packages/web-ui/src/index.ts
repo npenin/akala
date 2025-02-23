dd2ae8f318e1f51d333b24f05bdf7e61dc4d65be
npenin <github@dragon-angel.fr>
2025-02-23 08:19:40 +0000

    feat: add bootstrap functions which registers all web-ui control with a predefined naming

--- packages/web-ui/src/index.ts
+++ packages/web-ui/src/index.ts
@@ -4,3 +4,8 @@ export * from './controls/tooltip.js'
 export * from './controls/dropdown.js'
 export * from './controls/mark.js'
 export * from './controls/table.js'
+
+import bootstrap from './default.js'
+
+export { bootstrap };
+export default bootstrap;
