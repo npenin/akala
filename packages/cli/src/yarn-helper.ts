384bf893eb443cb56718437861c5ffffdf4eb214
npenin <github@dragon-angel.fr>
2025-02-25 04:56:44 +0000

    feat: add cli npm and yarn helpers from pm

---
+++ packages/cli/src/yarn-helper.ts
@@ -0,0 +1,40 @@
+import { platform } from "os";
+import { stat } from 'fs';
+import { promisify } from 'util';
+import { spawnAsync } from './cli-helper.js';
+import { join } from 'path';
+
+
+// if (typeof (process.versions.pnp) != 'undefined')
+// {
+
+// }
+
+export function hasYarn(path?: string): Promise<boolean>
+{
+    return promisify(stat)(join(path || process.cwd(), './yarn.lock')).then(f => f.isFile())
+}
+
+let npm = 'yarn';
+if (platform() == 'win32')
+    npm = 'yarn.cmd';
+
+export default
+    {
+        async install(packageName: string, path?: string): Promise<void>
+        {
+            await spawnAsync(npm, { cwd: path }, 'add', packageName)
+        },
+        async uninstall(packageName: string, path?: string): Promise<void>
+        {
+            await spawnAsync(npm, { cwd: path }, 'remove', packageName)
+        },
+        async update(packageName: string, path?: string): Promise<void>
+        {
+            await spawnAsync(npm, { cwd: path }, 'upgrade', packageName)
+        },
+        async link(packageName: string, path?: string): Promise<void>
+        {
+            await spawnAsync(npm, { cwd: path }, 'link', packageName)
+        }
+    }
