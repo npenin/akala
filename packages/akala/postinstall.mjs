ac0b3909d64cb4e2de72944641025b269cc581eb
npenin <github@dragon-angel.fr>
2025-02-27 06:18:25 +0000

    fix: postinstall script

--- packages/akala/postinstall.mjs
+++ packages/akala/postinstall.mjs
@@ -1,13 +1,12 @@
-import { buildCliContext } from '@akala/cli';
 
 if (process.env.NO_AKALAPOSTINSTALL != '1')
-{
     postinstall();
-}
 
 async function postinstall()
 {
     const akala = await import('@akala/cli/cli').then(x => x.cli);
+    const { buildCliContext } = await import('@akala/cli');
+
     akala(buildCliContext(null, 'plugins', 'add', '@akala/config/akala'))
     akala(buildCliContext(null, 'plugins', 'add', '@akala/commands/akala'))
     akala(buildCliContext(null, 'commands', 'add', 'sdk', '@akala/commands/commands.json'))
