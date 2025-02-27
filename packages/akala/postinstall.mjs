f69a2f442126abb9757c7cd94f131f5484698d49
npenin <github@dragon-angel.fr>
2025-02-27 07:25:33 +0000

    fix: update after akala cli refactoring

--- packages/akala/postinstall.mjs
+++ packages/akala/postinstall.mjs
@@ -3,10 +3,11 @@ if (process.env.NO_AKALAPOSTINSTALL != '1')
 
 async function postinstall()
 {
-    const akala = await import('@akala/cli/cli').then(x => x.cli);
+    const akala = await import('@akala/cli/cli').then(x => x.cli({ plugins: [] }));
     const { buildCliContext } = await import('@akala/cli');
 
-    akala(buildCliContext(null, 'plugins', 'add', '@akala/config/akala'))
-    akala(buildCliContext(null, 'plugins', 'add', '@akala/commands/akala'))
-    akala(buildCliContext(null, 'commands', 'add', 'sdk', '@akala/commands/commands.json'))
+
+    await akala.process(buildCliContext(null, 'plugins', 'add', '@akala/config/akala'))
+    await akala.process(buildCliContext(null, 'plugins', 'add', '@akala/commands/akala'))
+    await akala.process(buildCliContext(null, 'commands', 'add', 'sdk', '@akala/commands/commands.json'))
 }
