186decb8a768afa72914b3f3efd77b9aa311de06
npenin <github@dragon-angel.fr>
2025-02-27 19:53:50 +0000

    fix: postinstall logger

--- packages/akala/postinstall.mjs
+++ packages/akala/postinstall.mjs
@@ -5,9 +5,11 @@ async function postinstall()
 {
     const akala = await import('@akala/cli/cli').then(x => x.cli({ plugins: [] }));
     const { buildCliContext } = await import('@akala/cli');
+    const { logger } = await import('@akala/core');
+    const cliLogger = logger('akala')
 
 
-    await akala.process(buildCliContext(null, 'plugins', 'add', '@akala/config/akala'))
-    await akala.process(buildCliContext(null, 'plugins', 'add', '@akala/commands/akala'))
-    await akala.process(buildCliContext(null, 'commands', 'add', 'sdk', '@akala/commands/commands.json'))
+    await akala.process(buildCliContext(cliLogger, 'plugins', 'add', '@akala/config/akala'))
+    await akala.process(buildCliContext(cliLogger, 'plugins', 'add', '@akala/commands/akala'))
+    await akala.process(buildCliContext(cliLogger, 'commands', 'add', 'sdk', '@akala/commands/commands.json'))
 }
