1a6c71a15501157f55f307d4b34ac4931f7ae006
npenin <github@dragon-angel.fr>
2025-02-27 20:47:29 +0000

    fix: postinstall to leverage config

--- packages/akala/postinstall.mjs
+++ packages/akala/postinstall.mjs
@@ -3,13 +3,15 @@ if (process.env.NO_AKALAPOSTINSTALL != '1')
 
 async function postinstall()
 {
-    const akala = await import('@akala/cli/cli').then(x => x.cli({ plugins: [] }));
-    const { buildCliContext } = await import('@akala/cli');
+    const config = { plugins: [] };
+    const akala = await import('@akala/cli/cli').then(x => x.cli(config));
+    const { buildCliContextFromContext, buildCliContextFromProcess } = await import('@akala/cli');
     const { logger } = await import('@akala/core');
     const cliLogger = logger('akala')
 
+    let context = buildCliContextFromProcess(cliLogger, config);
 
-    await akala.process(buildCliContext(cliLogger, 'plugins', 'add', '@akala/config/akala'))
-    await akala.process(buildCliContext(cliLogger, 'plugins', 'add', '@akala/commands/akala'))
-    await akala.process(buildCliContext(cliLogger, 'commands', 'add', 'sdk', '@akala/commands/commands.json'))
+    await akala.process(context = buildCliContextFromContext(context, 'plugins', 'add', '@akala/config/akala'))
+    await akala.process(context = buildCliContextFromContext(context, 'plugins', 'add', '@akala/commands/akala'))
+    await akala.process(context = buildCliContextFromContext(context, 'commands', 'add', 'sdk', '@akala/commands/commands.json'))
 }
