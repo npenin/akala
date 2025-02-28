315af97966ea46b9ab4e34691851e4a6b9fb92be
npenin <github@dragon-angel.fr>
2025-02-28 21:03:12 +0000

    fix: trying to fix akala cli

--- packages/akala/cli.mjs
+++ packages/akala/cli.mjs
@@ -1,27 +1,20 @@
 import { writeFile } from 'fs/promises';
 import { join } from 'path'
 import { cli as akala } from '@akala/cli/cli'
-import { buildCliContext, buildCliContextFromContext, program } from '@akala/cli';
+import { buildCliContext, buildCliContextFromContext, buildCliContextFromProcess, program } from '@akala/cli';
 import { logger, LogLevels } from '@akala/core';
 import npm from '@akala/cli/npm-helper'
 import yarn, { hasYarn } from '@akala/cli/yarn-helper'
 
 const cliLogger = logger('akala', LogLevels.info)
 
-
 const cwd = process.env.INIT_CWD || process.cwd();
 cliLogger.info('setting cwd to ' + cwd)
 const configFile = join(cwd, './.akala.json')
 
-function getCliContext(...args)
-{
-    const context = buildCliContext(cliLogger, ...args);
-    context.currentWorkingDirectory = cwd;
-    context.options.configFile = configFile;
-
-    return context;
-}
-
+const context = buildCliContextFromProcess(cliLogger, { plugins: [] });
+context.options = configFile;
+context.currentWorkingDirectory = cwd;
 
 try
 {
