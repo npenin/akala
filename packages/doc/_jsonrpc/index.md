cb199f6d0ddb7c35720d4f46d4c7ace9123434a6
npenin <github@dragon-angel.fr>
2025-03-04 06:44:19 +0000

    fix: add front matter title and parent attributes to markdown files for consistency

--- packages/doc/_jsonrpc/index.md
+++ packages/doc/_jsonrpc/index.md
@@ -1,4 +1,5 @@
 ---
 
+title: Jsonrpc
 ---
 # *Coming soon...*
--- packages/doc/src/commands/update-hierarchies.ts
+++ packages/doc/src/commands/update-hierarchies.ts
@@ -5,88 +5,110 @@ export default async function updateFrontMatter(directory: string = 'packages/do
 {
     console.log('updating hierarchies in ' + directory);
 
-    try
+    const files = await fs.readdir(directory, { withFileTypes: true });
+
+    files.sort((a, b) => a.name == 'index.md' ? 1 : b.name == 'index.md' ? -1 : a.isDirectory() && b.isDirectory() ? a.name.localeCompare(b.name) : a.isDirectory() ? 1 : -1)
+
+    if (files.length && files[0].name == 'index.md')
     {
-        const files = await fs.readdir(directory);
+        const file = files[0];
+        const fullPath = path.join(directory, file.name);
+        const data = await fs.readFile(fullPath, 'utf8');
+        let updatedContent: string = data;
+        let hasFrontMatter = updatedContent.match(/^---\n.*---/s);
+        let currentTitle: string;
 
-        for (const file of files)
+        if (hasFrontMatter)
         {
-            const fullPath = path.join(directory, file);
-            if (file === 'index.md')
+            const parentMatch = updatedContent.match(/parent:\s*(.+)$/m);
+            if (parentMatch && parentMatch[1] != parentTitle)
             {
-                const data = await fs.readFile(fullPath, 'utf8');
+                console.log(fullPath + ' has parent')
+                updatedContent = updatedContent.substring(0, parentMatch.index) + `parent: ${parentTitle}\n` + updatedContent.substring(parentMatch.index + parentMatch[0].length);
+            }
+            else if (!parentMatch && parentTitle)
+            {
+                console.log(fullPath + ' does not have parent')
+                updatedContent = updatedContent.substring(0, hasFrontMatter[0].length - 3) + `parent: ${parentTitle}\n---` + updatedContent.substring(hasFrontMatter[0].length);
+            }
 
-                const titleMatch = data.match(/title:\s*(.+)$/m);
-                const parentMatch = data.match(/parent:\s*(.+)$/m);
-                const navOrderMatch = data.match(/nav_order:\s*(\d+)$/m);
+            const titleMatch = updatedContent.match(/title:\s*(.+)$/m);
+            if (!titleMatch)
+            {
+                console.log(fullPath + ' does not have title')
+                hasFrontMatter = updatedContent.match(/^---\n.*---/s);
+                currentTitle = path.basename(directory);
+                updatedContent = updatedContent.substring(0, hasFrontMatter[0].length - 3) + `title: ${currentTitle}\n---` + updatedContent.substring(hasFrontMatter[0].length);
+            }
+            else
+                currentTitle = titleMatch[1];
+        }
+        else
+        {
+            console.log(fullPath + ' does not have front matter')
+            if (parentTitle)
+                updatedContent = data.replace(/^/s, `---\nparent: ${parentTitle}\ntitle: ${path.basename(directory)}\n---\n`);
+            else
+                updatedContent = data.replace(/^/s, `---\ntitle: ${path.basename(directory)}\n---\n`);
+            currentTitle = path.basename(directory);
+        }
+        const navOrderMatch = updatedContent.match(/nav_order:\s*(\d+)$/m);
 
-                if (parentTitle && parentMatch?.[1] != parentTitle)
+        if (updatedContent !== data)
+            await fs.writeFile(fullPath, updatedContent, 'utf8');
+
+        const parent = currentTitle;
+        const navOrder = navOrderMatch ? parseInt(navOrderMatch[1], 10) : 1;
+
+        for (const siblingFile of files)
+        {
+            if (siblingFile.name !== 'index.md' && siblingFile.name != 'getting-started.md' && siblingFile.name.endsWith('.md'))
+            {
+                const siblingPath = path.join(directory, siblingFile.name);
+                const siblingData = await fs.readFile(siblingPath, 'utf8');
+
+                let updatedContent: string = siblingData;
+                let hasFrontMatter = updatedContent.match(/^---\n.*---/s);
+                if (hasFrontMatter)
                 {
-                    const newFrontMatter = `parent: ${parent}\n`;
-                    let updatedContent: string;
-                    if (parentMatch)
-                        updatedContent = data.substring(0, parentMatch.index) + newFrontMatter + data.substring(parentMatch.index + parentMatch[0].length);
-                    else
-                        updatedContent = data.replace(/^---\n/s, `---\n${newFrontMatter}`);
-
-                    await fs.writeFile(fullPath, updatedContent, 'utf8');
-                }
-
-                if (titleMatch)
-                {
-                    const parent = titleMatch[1];
-                    const navOrder = navOrderMatch ? parseInt(navOrderMatch[1], 10) : 1;
-
-                    for (const siblingFile of files)
+                    const parentMatch = updatedContent.match(/parent:\s*(.+)$/m);
+                    if (parentMatch && parentMatch[1] != parent)
                     {
-                        if (siblingFile !== 'index.md' && siblingFile != 'getting-started.md' && siblingFile.endsWith('.md'))
-                        {
-                            const siblingPath = path.join(directory, siblingFile);
-                            const siblingData = await fs.readFile(siblingPath, 'utf8');
+                        console.log(siblingPath + ' has parent')
+                        updatedContent = updatedContent.substring(0, parentMatch.index) + `parent: ${parent}\n` + updatedContent.substring(parentMatch.index + parentMatch[0].length);
+                    }
+                    else if (!parentMatch && parentTitle)
+                    {
+                        console.log(siblingPath + ' does not have parent')
+                        updatedContent = updatedContent.substring(0, hasFrontMatter[0].length - 3) + `parent: ${parent}\n---` + updatedContent.substring(hasFrontMatter[0].length);
+                    }
 
-                            let updatedContent: string = siblingData;
-                            let hasFrontMatter = updatedContent.match(/^---\n.*---/s);
-                            if (hasFrontMatter)
-                            {
-                                const parentMatch = updatedContent.match(/parent:\s*(.+)$/m);
-                                if (parentMatch && parentMatch[1] != parent)
-                                {
-                                    console.log(siblingPath + ' has parent')
-                                    updatedContent = updatedContent.substring(0, parentMatch.index) + `parent: ${parent}\n` + updatedContent.substring(parentMatch.index + parentMatch[0].length);
-                                }
-                                else if (!parentMatch)
-                                {
-                                    console.log(siblingPath + ' does not have parent')
-                                    updatedContent = updatedContent.substring(0, hasFrontMatter[0].length - 3) + `parent: ${parent}\n---` + updatedContent.substring(hasFrontMatter[0].length);
-                                }
-
-                                const navOrderMatch = updatedContent.match(/nav_order:\s*(\d+)$/m);
-                                if (navOrderMatch && Number(navOrderMatch[1]) != navOrder + 1)
-                                {
-                                    console.log(siblingPath + ' has nav_order')
-                                    updatedContent = updatedContent.substring(0, navOrderMatch.index) + `nav_order: ${navOrder + 1}` + updatedContent.substring(navOrderMatch.index + navOrderMatch[0].length);
-                                }
-                                else if (!navOrderMatch)
-                                {
-                                    console.log(siblingPath + ' does not have nav_order')
-                                    hasFrontMatter = updatedContent.match(/^---\n.*---/s);
-                                    updatedContent = updatedContent.substring(0, hasFrontMatter[0].length - 3) + `nav_order: ${navOrder + 1}\n---` + updatedContent.substring(hasFrontMatter[0].length);
-                                }
-                            }
-                            else
-                                updatedContent = data.replace(/^/s, `---\nparent: ${parent}\nnav_order: ${navOrder + 1}\n---\n`);
-
-                            if (updatedContent !== siblingData)
-                                await fs.writeFile(siblingPath, updatedContent, 'utf8');
-                        }
+                    const navOrderMatch = updatedContent.match(/nav_order:\s*(\d+)$/m);
+                    if (navOrderMatch && Number(navOrderMatch[1]) != navOrder + 1)
+                    {
+                        console.log(siblingPath + ' has nav_order')
+                        updatedContent = updatedContent.substring(0, navOrderMatch.index) + `nav_order: ${navOrder + 1}` + updatedContent.substring(navOrderMatch.index + navOrderMatch[0].length);
+                    }
+                    else if (!navOrderMatch)
+                    {
+                        console.log(siblingPath + ' does not have nav_order')
+                        hasFrontMatter = updatedContent.match(/^---\n.*---/s);
+                        updatedContent = updatedContent.substring(0, hasFrontMatter[0].length - 3) + `nav_order: ${navOrder + 1}\n---` + updatedContent.substring(hasFrontMatter[0].length);
                     }
                 }
+                else
+                    updatedContent = data.replace(/^/s, `---\nparent: ${parent}\nnav_order: ${navOrder + 1}\n---\n`);
+
+                if (updatedContent !== siblingData)
+                    await fs.writeFile(siblingPath, updatedContent, 'utf8');
             }
-            if ((await fs.stat(fullPath)).isDirectory())
-                await updateFrontMatter(fullPath);
         }
-    } catch (err)
-    {
-        console.error(`Error: ${err}`);
+
+        if ((await fs.stat(fullPath)).isDirectory())
+            await updateFrontMatter(fullPath, currentTitle);
     }
+    else
+        for (var file of files)
+            if (file.isDirectory())
+                await updateFrontMatter(path.join(directory, file.name));
 }
