63796734af18da5d2b87fdb4a0b9527b1ba23d7c
npenin <github@dragon-angel.fr>
2025-03-17 03:44:42 +0100

    fix: update after core breaking change

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,4 +1,4 @@
-import { LogLevels, Logger, logger as LoggerBuilder, grep, map, each, getSetter } from '@akala/core';
+import { LogLevels, Logger, logger as LoggerBuilder, grep, map, each, ObservableObject } from '@akala/core';
 import program, { CliContext, NamespaceMiddleware, OptionOptions, OptionType, usageParser } from './router/index.js';
 export * from './router/index.js'
 export default program;
@@ -33,7 +33,7 @@ export function supportInteract(cli: NamespaceMiddleware)
                 const indexOfDot = err.as.indexOf('.');
                 if (indexOfDot > 0)
                 {
-                    getSetter(err.as, context.options).set(value);
+                    ObservableObject.setValue(context.options, err.as, value);
                 }
                 context.options[err.as] = value;
             }
