ebb14c845f4d8f37aa18fa77f082c85178b52caa
npenin <github@dragon-angel.fr>
2025-03-20 05:19:55 +0000

    feat: implement AWS4 signature as httpclient authentication

--- packages/aws-sdk/src/cli/$init-akala.ts
+++ packages/aws-sdk/src/cli/$init-akala.ts
@@ -1,13 +1,14 @@
 import { Metadata, metadataPluginHandler, tsPluginHandler, FileGenerator, protocolHandlers, HandlerResult, ICommandProcessor } from '@akala/commands'
 import { Policy } from '../iam.js';
 import { Processors } from '@akala/commands';
+import { sign } from '../signature.js';
 
 export default async function $initAkala()
 {
     metadataPluginHandler.use(async (options, meta: Metadata.Container & Partial<{ aws: {} }>, outputFolder) =>
     {
         meta.aws = {
-            permissions: mergeAll(meta.commands)
+            permissions: merge(meta.commands)
         }
     })
 
@@ -24,55 +25,74 @@ export default async function $initAkala()
 
         result.getMetadata = () => import(`../../services/${url.hostname}.json`);
         result.processor = new Processors.HttpClient();
+
         return Promise.resolve(result as HandlerResult<ICommandProcessor>);
-    })
+    });
+
+    Processors.authMiddleware.use(10, async (config, options, auth) =>
+    {
+        if (config.mode === 'AWS4')
+        {
+            if (typeof auth !== 'object' || !auth['accessKey'] || !auth['region'] || !auth['service'])
+                throw new Error('AWS4 authentication requires accessKey, region and service to be set');
+
+            await sign(auth['accessKey'], auth['region'], auth['service'])(options.url, options);
+        }
+    });
+
 }
 
 
 type Permissions = Policy & { use?: string[] };
-const mergedPermissions: Record<string, Permissions> = {};
 
-function mergeAll(commands: Metadata.Command[])
+export function merge(commands: Metadata.Command[])
 {
-    commands.forEach(command =>
-    {
-        if (command.config.aws?.permissions)
-            mergedPermissions[command.name] = merge(command.config.aws.permissions, commands);
-    })
-    return mergedPermissions;
-}
+    const mergedPermissions: Record<string, Permissions> = {};
 
-function merge(permissions: Permissions, commands: Metadata.Command[]): any
-{
-    if (permissions.use)
+    function mergeAll(commands: Metadata.Command[])
     {
-        permissions.use.forEach(use =>
+        commands.forEach(command =>
         {
-            let otherPermissions: Permissions;
-            if (use in mergedPermissions)
-                otherPermissions = mergedPermissions[use];
-            else
-            {
-                otherPermissions = merge(commands.find(c => c.name == use).config.aws.permissions, commands);
-            }
-            Object.keys(otherPermissions).forEach(service =>
-            {
-                if (!permissions[service])
-                    permissions[service] = { ...otherPermissions[service] };
-                else
-                    Object.entries(otherPermissions[service]).forEach(([resource, actions]) =>
-                    {
-                        if (!permissions[service][resource])
-                            permissions[service][resource] = { ...actions };
-                        else
-                            Object.entries(actions).forEach(([action, value]) =>
-                            {
-                                permissions[service][resource][action] = value;
-                            })
-                    })
-            })
-        });
-        // delete permissions.use;
+            if (command.config.aws?.permissions)
+                mergedPermissions[command.name] = merge(command.config.aws.permissions, commands);
+        })
+        return mergedPermissions;
     }
-    return permissions;
+
+    function merge(permissions: Permissions, commands: Metadata.Command[]): any
+    {
+        if (permissions.use)
+        {
+            permissions.use.forEach(use =>
+            {
+                let otherPermissions: Permissions;
+                if (use in mergedPermissions)
+                    otherPermissions = mergedPermissions[use];
+                else
+                {
+                    otherPermissions = merge(commands.find(c => c.name == use).config.aws.permissions, commands);
+                }
+                Object.keys(otherPermissions).forEach(service =>
+                {
+                    if (!permissions[service])
+                        permissions[service] = { ...otherPermissions[service] };
+                    else
+                        Object.entries(otherPermissions[service]).forEach(([resource, actions]) =>
+                        {
+                            if (!permissions[service][resource])
+                                permissions[service][resource] = { ...actions };
+                            else
+                                Object.entries(actions).forEach(([action, value]) =>
+                                {
+                                    permissions[service][resource][action] = value;
+                                })
+                        })
+                })
+            });
+            // delete permissions.use;
+        }
+        return permissions;
+    }
+
+    return mergeAll(commands)
 }
