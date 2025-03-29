fbd741f07c5efc6cab2673be576857926371a653
Nicolas <github@dragon-angel.fr>
2025-03-29 06:43:37 +0100

    fix: build

--- packages/aws-sdk/src/cli/generate-tf.ts
+++ packages/aws-sdk/src/cli/generate-tf.ts
@@ -113,7 +113,7 @@ export default async function (logger: Logger, inputPath: string, commandsName: 
         }))
     }));
 
-    if (hasYarn(inputPath))
+    if (await hasYarn(inputPath))
     {
         await fs.writeFile(join(packagingFolder, 'container-deps/nodejs/yarn.lock'), '');
         await fs.writeFile(join(packagingFolder, 'container-deps/.yarnrc.yml'), `
@@ -136,7 +136,7 @@ nodeLinker: node-modules
             if (files[zipFilePath])
                 return;
             files[zipFilePath] = true;
-            await zipFile.add(zipFilePath, new zip.Uint8ArrayReader(await fs.readFile(join(entry.parentPath, entry.name))));
+            await zipFile.add(zipFilePath, new zip.Uint8ArrayReader(new Uint8Array(await fs.readFile(join(entry.parentPath, entry.name)))));
         });
 
         zipStream.on('close', resolve);
@@ -192,7 +192,7 @@ nodeLinker: node-modules
                 if (files[zipFilePath])
                     return;
                 files[zipFilePath] = true;
-                await zipFile.add(zipFilePath, new zip.Uint8ArrayReader(await fs.readFile(join(entry.parentPath, entry.name))));
+                await zipFile.add(zipFilePath, new zip.Uint8ArrayReader(new Uint8Array(await fs.readFile(join(entry.parentPath, entry.name)))));
             });
         }
         else
@@ -207,7 +207,7 @@ nodeLinker: node-modules
                     return;
                 files[zipFilePath] = true;
 
-                await zipFile.add(zipFilePath, new zip.Uint8ArrayReader(await fs.readFile(join(entry.parentPath, entry.name))));
+                await zipFile.add(zipFilePath, new zip.Uint8ArrayReader(new Uint8Array(await fs.readFile(join(entry.parentPath, entry.name)))));
             });
 
         zipStream.on('close', resolve);
