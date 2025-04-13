3e0d64bccd2931ba035d746fef1f66a0c0eb982c
Nicolas <github@dragon-angel.fr>
2025-04-13 22:21:00 +0200

    fix: improve discovery
    BREAKING CHANGE: discover returns  a fully typed metadata object

--- packages/authentication/src/client/oidc-discover.ts
+++ packages/authentication/src/client/oidc-discover.ts
@@ -1,82 +1,78 @@
 import { Metadata } from "@akala/commands";
 import { OIDCDescriptionWithOptional } from "./oidc-state.js";
 
-export default async function (url: string | URL): Promise<Metadata.Command[]>
+export interface OIDCMetadata
 {
-    const result: Metadata.Command[] = [];
+    rootUrl: URL;
+    authorize?: Metadata.Configurations;
+    "authorize-device"?: Metadata.Configurations;
+    "get-token"?: Metadata.Configurations;
+    "user-info"?: Metadata.Configurations;
+    revoke?: Metadata.Configurations;
+}
+
+export default async function (url: string | URL): Promise<OIDCMetadata>
+{
+    const result: OIDCMetadata = { rootUrl: new URL(url) };
 
     const config = await fetch(new URL('./.well-known/openid-configuration', url)).then(r => r.ok ? r.json() : Promise.reject(new Error('The OIDC configuration could not be detected'))) as OIDCDescriptionWithOptional;
 
     if (config.authorization_endpoint)
     {
-        result.push({
-            name: 'authorize',
-            config: {
-                http: {
-                    route: config.authorization_endpoint, method: 'get',
-                    inject: [
+        result.authorize = {
+            http: {
+                route: config.authorization_endpoint, method: 'get',
+                inject: [
 
-                    ]
-                }
+                ]
             }
-        });
+        };
     }
     if (config.device_authorization_endpoint)
     {
-        result.push({
-            name: 'authorize-device',
-            config: {
-                http: {
-                    route: config.device_authorization_endpoint, method: 'get',
-                    inject: [
+        result['authorize-device'] = {
+            http: {
+                route: config.device_authorization_endpoint, method: 'get',
+                inject: [
 
-                    ]
-                }
+                ]
             }
-        });
+        };
     }
     if (config.token_endpoint)
     {
-        result.push({
-            name: 'get-token',
-            config: {
-                http: {
-                    route: config.token_endpoint, method: config.token_endpoint_auth_methods_supported[0] || 'post',
-                    inject: [
+        result['get-token'] = {
+            http: {
+                route: config.token_endpoint, method: config.token_endpoint_auth_methods_supported[0] || 'post',
+                inject: [
 
-                    ]
-                }
+                ]
             }
-        });
+        };
     }
+
     if (config.userinfo_endpoint)
     {
 
-        result.push({
-            name: 'user-info',
-            config: {
-                http: {
-                    route: config.userinfo_endpoint, method: 'get',
-                    inject: [
+        result['user-info'] = {
+            http: {
+                route: config.userinfo_endpoint, method: 'get',
+                inject: [
 
-                    ]
-                }
+                ]
             }
-        });
+        };
     }
     if (config.revocation_endpoint)
     {
-        result.push({
-            name: 'revoke',
-            config: {
-                http: {
-                    route: config.revocation_endpoint, method: 'post',
-                    inject: [
+        result.revoke = {
+            http: {
+                route: config.revocation_endpoint, method: 'post',
+                inject: [
 
-                    ]
-                }
+                ]
             }
-        });
+        };
     }
 
     return result;
