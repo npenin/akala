30996cdf3441252ec6a54e1b7559f8404aacd165
Nicolas <github@dragon-angel.fr>
2025-04-13 22:21:27 +0200

    fix: improve formatter behavior

--- packages/authentication/src/server/middlewares/AuthorizeRedirectFormatter.ts
+++ packages/authentication/src/server/middlewares/AuthorizeRedirectFormatter.ts
@@ -3,23 +3,21 @@ import { Response } from "@akala/server";
 
 export class AuthorizeRedirectFormatter implements ErrorMiddlewareAsync<[unknown, Response]>
 {
-    redirectUrl: URL;
-    redirectQueryParameter: string;
-    constructor(redirectUrl: URL | string)
+    constructor(private readonly redirectUrl: URL | string | (() => URL | string), public readonly redirectQueryParameter: string)
     {
-        if (typeof (redirectUrl) == 'string')
-            this.redirectUrl = new URL(redirectUrl);
-
-        else
-            this.redirectUrl = redirectUrl;
     }
 
     handleError(error: ErrorWithStatus, req, response: Response): MiddlewarePromise
     {
         if (error && error.statusCode === HttpStatusCode.Unauthorized && !response.headersSent)
         {
-            const url = new URL(this.redirectUrl.toString());
-            url.searchParams[this.redirectQueryParameter] = req.url;
+            let url = this.redirectUrl;
+            if (typeof (url) === 'function')
+                url = url();
+            if (typeof url === 'string')
+                url = new URL(url);
+            if (this.redirectQueryParameter)
+                url.searchParams[this.redirectQueryParameter] = req.url;
             return Promise.reject(response.redirect(url.toString()));
         }
         return Promise.resolve();
