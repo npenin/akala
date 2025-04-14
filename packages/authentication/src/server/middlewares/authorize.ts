64f876445c093cd5da9fa44bb8c05af910b7cd47
Nicolas <github@dragon-angel.fr>
2025-04-15 00:10:37 +0200

    fix: update after core breaking change

--- packages/authentication/src/server/middlewares/authorize.ts
+++ packages/authentication/src/server/middlewares/authorize.ts
@@ -1,4 +1,4 @@
-import { ErrorWithStatus, HttpStatusCode, MiddlewareAsync, MiddlewarePromise } from "@akala/core";
+import { ErrorWithStatus, HttpStatusCode, MiddlewareAsync, MiddlewarePromise, NotHandled } from "@akala/core";
 import { AuthRequest } from "../index.js";
 
 export class NonPublicMiddleware implements MiddlewareAsync<[AuthRequest<unknown>, ...unknown[]]>, MiddlewareAsync<[AuthRequest<unknown>, ...unknown[]]>
@@ -7,6 +7,6 @@ export class NonPublicMiddleware implements MiddlewareAsync<[AuthRequest<unknown
     {
         if (!req.user)
             return Promise.reject(new ErrorWithStatus(HttpStatusCode.Unauthorized, 'User is not authenticated'));
-        return Promise.resolve();
+        return NotHandled;
     }
 }
