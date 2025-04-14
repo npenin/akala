b92665cd08728bf58a58cb5515c90b6cdc277cb7
Nicolas <github@dragon-angel.fr>
2025-04-15 00:09:29 +0200

    fix: oidc discover properly returns options for authorize, token and keys

--- packages/authentication/src/client/oidc-discover.ts
+++ packages/authentication/src/client/oidc-discover.ts
@@ -1,14 +1,16 @@
 import { Metadata } from "@akala/commands";
 import { OIDCDescriptionWithOptional } from "./oidc-state.js";
+import { JWK } from "@akala/jwt";
 
 export interface OIDCMetadata
 {
     rootUrl: URL;
     authorize?: Metadata.Configurations;
-    "authorize-device"?: Metadata.Configurations;
-    "get-token"?: Metadata.Configurations;
-    "user-info"?: Metadata.Configurations;
+    authorizeDevice?: Metadata.Configurations;
+    getToken?: Metadata.Configurations;
+    userInfo?: Metadata.Configurations;
     revoke?: Metadata.Configurations;
+    keys?: Record<string, JWK>;
 }
 
 export default async function (url: string | URL): Promise<OIDCMetadata>
@@ -22,15 +24,19 @@ export default async function (url: string | URL): Promise<OIDCMetadata>
         result.authorize = {
             http: {
                 route: config.authorization_endpoint, method: 'get',
-                inject: [
-
-                ]
+                inject: [{
+                    clientId: "query.client_id",
+                    responseType: "query.response_type",
+                    redirectUri: "query.redirect_uri",
+                    scope: "query.scope",
+                }
+                ],
             }
         };
     }
     if (config.device_authorization_endpoint)
     {
-        result['authorize-device'] = {
+        result.authorizeDevice = {
             http: {
                 route: config.device_authorization_endpoint, method: 'get',
                 inject: [
@@ -41,20 +47,68 @@ export default async function (url: string | URL): Promise<OIDCMetadata>
     }
     if (config.token_endpoint)
     {
-        result['get-token'] = {
-            http: {
-                route: config.token_endpoint, method: config.token_endpoint_auth_methods_supported[0] || 'post',
-                inject: [
-
-                ]
-            }
-        };
+        switch (config.token_endpoint_auth_methods_supported?.[0])
+        {
+            case 'client_secret_post':
+                result.getToken = {
+                    http: {
+                        route: config.token_endpoint,
+                        method: 'post',
+                        contentType: 'form-urlencoded',
+                        inject: [
+                            {
+                                grantType: "body.grant_type",
+                                code: "body.code",
+                                redirectUri: "body.redirect_uri",
+                                scope: "body.scope",
+                            }
+                        ],
+                        auth: {
+                            inject: [
+                                {
+                                    client_id: "auth.clientId",
+                                    client_secret: "auth.clientSecret",
+                                }],
+                            mode: 'body',
+                        }
+                    }
+                };
+                break;
+            case 'client_secret_basic':
+                result.getToken = {
+                    http: {
+                        route: config.token_endpoint,
+                        method: 'get',
+                        inject: [
+                            {
+                                grantType: "query.grant_type",
+                                code: "query.code",
+                                redirectUri: "query.redirect_uri",
+                                scope: "query.scope",
+                            }
+                        ],
+                        auth: {
+                            inject: [
+                                {
+                                    username: "auth.clientId",
+                                    password: "auth.clientSecret",
+                                }],
+                            mode: 'basic',
+                        }
+                    }
+                };
+                break;
+            case 'client_secret_jwt':
+            case 'private_key_jwt':
+            default:
+                throw new Error(`The OIDC provider ${url} does not support the client_secret_post method for token exchange`);
+        }
     }
 
     if (config.userinfo_endpoint)
     {
 
-        result['user-info'] = {
+        result.userInfo = {
             http: {
                 route: config.userinfo_endpoint, method: 'get',
                 inject: [
@@ -73,6 +127,13 @@ export default async function (url: string | URL): Promise<OIDCMetadata>
                 ]
             }
         };
+    }
+    if (config.jwks_uri)
+    {
+        result.keys = await fetch(config.jwks_uri).then(res => res.ok ? res.json() : Promise.reject(res)).then(certs =>
+        {
+            return Object.fromEntries(certs.keys.map(k => [k.kid, k]));
+        })
     }
 
     return result;
