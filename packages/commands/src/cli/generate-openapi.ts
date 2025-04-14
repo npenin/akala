3488f25f18558877b0cfae8c7f6f1c7684f677bf
Nicolas <github@dragon-angel.fr>
2025-04-15 00:01:10 +0200

    refactor: fix sonarqube issues

--- packages/commands/src/cli/generate-openapi.ts
+++ packages/commands/src/cli/generate-openapi.ts
@@ -16,31 +16,31 @@ export default async function generate(folder?: string, name?: string, outputFil
         name = (await import(path.join(folder, './package.json'))).name;
     if (!name)
     {
-        name = path.basename(folder);
+        // name = path.basename(folder);
         discoveryOptions.isDirectory = false;
     }
 
     let output: Writable;
     let outputFolder: string;
-    ({ output, outputFile, outputFolder } = await outputHelper(outputFile, 'commands.json', true));
+    ({ output, outputFolder } = await outputHelper(outputFile, 'commands.json', true));
     discoveryOptions.relativeTo = outputFolder;
     discoveryOptions.recursive = true;
     discoveryOptions.ignoreFileWithNoDefaultExport = true;
 
-    var container = await akala.Processors.FileSystem.discoverMetaCommands(path.resolve(folder), discoveryOptions);
+    const container = await akala.Processors.FileSystem.discoverMetaCommands(path.resolve(folder), discoveryOptions);
 
-    var result: jsonObject = { paths: {} };
+    let result: jsonObject = { paths: {} };
     result.openapi = '3.1.0';
     result.info = { title: 'openAPI documentation for ' + container.name, version: '' };
-    for (var i = 0; i < container.commands.length; i++)
+    for (const command of container.commands)
     {
-        if (container.commands[i].config?.http)
+        if (command.config?.http)
         {
-            result.paths[container.commands[i].config.http.route] = result.paths[container.commands[i].config.http.route] || {};
-            var action: jsonObject = result.paths[container.commands[i].config.http.route][container.commands[i].config.http.method] = { parameters: [] };
+            result.paths[command.config.http.route] = result.paths[command.config.http.route] || {};
+            const action: jsonObject = result.paths[command.config.http.route][command.config.http.method] = { parameters: [] };
             // var keys: Key[] = [].concat(pathToRegexp(commands[i].config.http.route).keys);
-            var hasBody = false;
-            action.parameters = container.commands[i].config.http.inject.map(p =>
+            let hasBody = false;
+            action.parameters = command.config.http.inject.map(p =>
             {
                 if (typeof p == 'string')
                     if (p.startsWith('route.'))
@@ -66,8 +66,8 @@ export default async function generate(folder?: string, name?: string, outputFil
             });
             if (hasBody)
             {
-                var content = { schema: { type: 'object', properties: {} } };
-                switch (container.commands[i].config.http.type)
+                let content = { schema: { type: 'object', properties: {} } };
+                switch (command.config.http.type)
                 {
                     default:
                     case 'json':
@@ -83,10 +83,10 @@ export default async function generate(folder?: string, name?: string, outputFil
                         action.content = { 'text/json': content }
                         break;
                 }
-                container.commands[i].config.http.inject.forEach((p, i) =>
+                command.config.http.inject.forEach((p, i) =>
                 {
                     if (typeof p == 'string' && p.startsWith('body.'))
-                        content.schema.properties[p.substring('body.'.length)] = container.commands[i].config.schema?.inject[i] ? container.commands[i].config.schema?.$defs[container.commands[i].config.schema?.inject[i] as string] : {};
+                        content.schema.properties[p.substring('body.'.length)] = command.config.schema?.inject[i] ? command.config.schema?.$defs[command.config.schema?.inject[i] as string] : {};
                 });
             }
         }
