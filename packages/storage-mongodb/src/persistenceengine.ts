940f72ec9ff15d0c30b485734a908e7a554a7c6c
Nicolas <github@dragon-angel.fr>
2025-04-17 08:25:05 +0200

    feat: add raw query support

--- packages/storage-mongodb/src/persistenceengine.ts
+++ packages/storage-mongodb/src/persistenceengine.ts
@@ -1,18 +1,27 @@
 import { Expressions } from '@akala/core/expressions';
 import { ModelDefinition, PersistenceEngine } from '@akala/storage'
-import { Collection, Db } from 'mongodb';
+import { Collection, Db, Document } from 'mongodb';
 import CommandProcessor from './command-processor.js';
 import MongoDbTranslator from './expression-visitor.js';
 import { NotHandled } from '@akala/core';
 
 
-export class MongoDb extends PersistenceEngine<Db>
+export class MongoDb extends PersistenceEngine<Db, { collection: string, pipeline: Document[] }>
 {
     private db: Db;
 
     constructor()
     {
         super(new CommandProcessor());
+    }
+
+    public rawQuery<T>(query: { collection: string, pipeline: Document[] }): PromiseLike<T>
+    {
+        const collection = this.db.collection(query.collection);
+
+        const result = collection.aggregate(query.pipeline);
+
+        return result.tryNext().then(d => result.close().then(() => d.result));
     }
 
     init(connection: Db): Promise<void>
@@ -26,7 +35,7 @@ export class MongoDb extends PersistenceEngine<Db>
         const executor = new MongoDbTranslator();
         const oldVisitContant = executor.visitConstant;
         const db = this.db;
-        var collection: Collection;
+        let collection: Collection;
         executor.visitConstant = function (cte)
         {
             if (cte.value instanceof ModelDefinition)
