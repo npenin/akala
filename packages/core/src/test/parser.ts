50319022099437196b764adef6bf7e8df36c7c9c
Nicolas <github@dragon-angel.fr>
2025-04-25 09:53:30 +0200

    fix: update after StringCursor switch

--- packages/core/src/test/parser.ts
+++ packages/core/src/test/parser.ts
@@ -1,10 +1,11 @@
-import { Parser, ParsedBinary, ParsedString } from '../parser/parser.js';
+import { Parser, ParsedBinary, ParsedString, StringCursor } from '../parser/parser.js';
 import { BinaryOperator } from '../parser/expressions/binary-operator.js';
 import { Formatter, formatters } from '../formatters/index.js';
 import { EvaluatorAsFunction } from '../parser/evaluator-as-function.js';
 import { ObservableArray } from '../observables/array.js';
 import { it, describe } from 'node:test'
 import assert from 'assert/strict'
+import { delay } from '../promiseHelpers.js';
 
 //b*(c+d) ==> (b*c)+d
 
@@ -14,9 +15,10 @@ describe('parser tests', () =>
 {
     it('should do math', () =>
     {
-
-        const result = <ParsedBinary>parser.parseEval('b*c+d', false);
+        const cursor = new StringCursor('b*c+d');
+        const result = <ParsedBinary>parser.parseEval(cursor, false);
         console.log(result);
+        assert.strictEqual('b*c+d'.length, cursor.offset)
         assert.strictEqual((evaluator.eval(result))({ b: 1, c: 2, d: 3 }), 5);
     });
 
@@ -26,7 +28,7 @@ describe('parser tests', () =>
         const test = new ParsedBinary(BinaryOperator.Times, new ParsedString('b'), new ParsedBinary(BinaryOperator.Plus, new ParsedString('c'), new ParsedString('d')));
         ParsedBinary.applyPrecedence(test);
 
-        assert.strictEqual(test.toString(), '(b*(c+d))');
+        assert.strictEqual(test.toString(), '( b * ( c + d ) )');
     })
 
     it('should parse complex expressions', () =>
@@ -62,21 +64,23 @@ describe('parser tests', () =>
         assert.deepStrictEqual((evaluator.eval(parser.parse("{options:{in:'/api/@domojs/zigate/pending' # http, text:internalName, value:address}}")))({})['options'], { in: formatter.format('/api/@domojs/zigate/pending'), text: undefined, value: undefined });
     })
 
-    console.log(evaluator.eval(parser.parse('columns[0].title'))({ columns: [{ title: 'pwet' }] }))
-
-    const args = {
-        controller: {
-            async fakeServer(sort, page)
-            {
-                const result = await Promise.resolve(['x', 'y', 'z']);
-                return result.map(x => x + sort + page)
-            }
-        }, table: { sort: 'a', page: 1 }
-    };
-    setTimeout(() =>
+    it('evals', async () =>
     {
+
+        console.log(evaluator.eval(parser.parse('columns[0].title'))({ columns: [{ title: 'pwet' }] }))
+
+        const args = {
+            controller: {
+                async fakeServer(sort, page)
+                {
+                    const result = await Promise.resolve(['x', 'y', 'z']);
+                    return result.map(x => x + sort + page)
+                }
+            }, table: { sort: 'a', page: 1 }
+        };
+        await delay(10)
         const array = evaluator.eval<ObservableArray<string>>(parser.parse('controller.fakeServer(table.sort, table.page)#asyncArray'))(args);
         array.addListener(ev => console.log(array.array));
-    }, 10)
+    })
 
 })
