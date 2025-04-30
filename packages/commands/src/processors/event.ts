719726f241f884d6890224f67b90086f053d2701
Nicolas <github@dragon-angel.fr>
2025-04-30 05:01:14 +0200

    feat: add namespaced event on EventProcessor

--- packages/commands/src/processors/event.ts
+++ packages/commands/src/processors/event.ts
@@ -7,17 +7,21 @@ import { protocolHandlers as handlers } from '../protocol-handler.js';
 
 handlers.useProtocol('event', async (_url, options, inner) => ({ processor: new EventProcessor(inner.processor, options.signal), getMetadata: inner.getMetadata }));
 
+export type ProcessingCommand<T extends string> = `processing.${T}`
+export type ProcessedCommand<T extends string> = `processed.${T}`
+export type ProcessingFailedCommand<T extends string> = `processing-failed.${T}`
+
 export class EventProcessor extends EventEmitter<{
-    processing: Event<CommandMetadataProcessorSignature<unknown>, void>,
-    'processing-failed': Event<[...CommandMetadataProcessorSignature<unknown>, MiddlewareResult]>,
-    processed: Event<[...CommandMetadataProcessorSignature<unknown>, unknown]>,
-}> implements ICommandProcessor
+    processing: Event<CommandMetadataProcessorSignature<unknown>>,
+    'processing-failed': Event<[...CommandMetadataProcessorSignature<unknown>, error: MiddlewareResult]>,
+    processed: Event<[...CommandMetadataProcessorSignature<unknown>, result: unknown]>,
+}
+    & { [key in ProcessedCommand<string>]: Event<[...CommandMetadataProcessorSignature<unknown>, result: unknown]> }
+    & { [key in ProcessingCommand<string>]: Event<CommandMetadataProcessorSignature<unknown>> }
+    & { [key in ProcessingFailedCommand<string>]: Event<[...CommandMetadataProcessorSignature<unknown>, error: MiddlewareResult]> }
+> implements ICommandProcessor
 {
     public name = 'event';
-
-    //eslint-disable-next-line @typescript-eslint/prefer-as-const
-    public readonly requiresCommandName: false = false;
-
 
     constructor(public readonly processor: ICommandProcessor, signal?: AbortSignal)
     {
@@ -27,15 +31,18 @@ export class EventProcessor extends EventEmitter<{
 
     async handle(origin: Container<unknown>, command: Command, param: StructuredParameters<unknown[]>): MiddlewarePromise
     {
+        this.emit(`processing.${command.name}`, origin, command, param);
         this.emit('processing', origin, command, param);
         try
         {
             const err = await this.processor.handle(origin, command, param)
+            this.emit(`processing-failed.${command.name}`, origin, command, param, err);
             this.emit('processing-failed', origin, command, param, err);
             return err;
         }
         catch (result)
         {
+            this.emit(`processed.${command.name}`, origin, command, param, result);
             this.emit('processed', origin, command, param, result);
             throw result;
         }
