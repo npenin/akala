2ea13112bf2991335ca8df2cecc83644502e244c
Nicolas <github@dragon-angel.fr>
2025-05-01 12:05:09 +0200

    feat: add invoke formatter

--- packages/web-ui/src/index.ts
+++ packages/web-ui/src/index.ts
@@ -5,7 +5,27 @@ export * from './controls/dropdown.js'
 export * from './controls/mark.js'
 export * from './controls/table.js'
 
+import { Formatter, formatters } from '@akala/core'
 import bootstrap from './default.js'
 
 export { bootstrap };
 export default bootstrap;
+
+
+formatters.register('#invoke', class implements Formatter<(ev: Event) => unknown>
+{
+    constructor(private readonly inner: (value, ev) => unknown) { }
+
+    format(value: unknown): (ev: Event) => unknown
+    {
+        let lastEv: Event;
+        return (ev) =>
+        {
+            if (ev === lastEv)
+                return false;
+            lastEv = ev;
+            return this.inner(value, ev);
+        }
+    }
+
+})
