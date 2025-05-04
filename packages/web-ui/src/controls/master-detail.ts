a9d03536d2959d908966e969b026d049461fc52f
Nicolas <github@dragon-angel.fr>
2025-05-04 10:46:36 +0200

    feat: allow customizing the context name with the name attribute

--- packages/web-ui/src/controls/master-detail.ts
+++ packages/web-ui/src/controls/master-detail.ts
@@ -2,18 +2,19 @@ import { a, Control, DataContext, e, s, Template } from "@akala/client";
 import { EmptyBinding } from "@akala/core";
 
 
-export class MasterDetail<T> extends Control<{}, HTMLTemplateElement>
+export class MasterDetail<T> extends Control<{ name: string }, HTMLTemplateElement>
 {
     public item = new EmptyBinding<T>();
 
     public connectedCallback(): void
     {
+        const contextName = this.attribute('name') || 'item';
         const shadow = this.shadowWithStyles({ mode: 'closed' });
         const masterSlot = e('slot');
-        const detailSlot = a(s(e('slot'), { name: 'detail' }), { 'data-context': 'item' });
+        const detailSlot = a(s(e('slot'), { name: 'detail' }), { 'data-context': contextName });
         shadow.replaceChildren(masterSlot, detailSlot);
 
-        DataContext.define(this.element, { item: this.item });
+        DataContext.define(this.element, { [contextName]: this.item });
         // DataContext.defineDirect(detailSlot, DataContext.extend(context, null, 'item'));
 
         this.teardown(Template.composeAll(shadow.childNodes as NodeListOf<Element>, shadow));
