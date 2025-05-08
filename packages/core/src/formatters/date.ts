8fff85396d5cf9fa93180f45955a7b68e36c6999
Nicolas <github@dragon-angel.fr>
2025-05-08 17:21:00 +0200

    fix: date parsing

--- packages/core/src/formatters/date.ts
+++ packages/core/src/formatters/date.ts
@@ -2,6 +2,9 @@
 // import { Formatter, FormatterFactory } from './common.js';
 // import { module } from '../helpers.js';
 
+import ErrorWithStatus, { HttpStatusCode } from "../errorWithStatus.js";
+import { lazy } from "../helpers.js";
+import { StringCursor } from "../parser/parser.js";
 import { Formatter, ReversibleFormatter } from "./common.js";
 
 export interface DateFormatterSettings
@@ -18,7 +21,88 @@ const formats = {
         return this.getUTCMonth() + 1;
     },
     d: Date.prototype.getDate,
-    s: Date.prototype.getUTCSeconds
+    s: Date.prototype.getUTCSeconds,
+    sss: Date.prototype.getUTCMilliseconds,
+}
+
+const formatsFormatter = {
+    h: function (this: Date) { return formats.h.call(this).toString() },
+    m: function (this: Date) { return formats.m.call(this).toString() },
+    M: function (this: Date) { return formats.M.call(this).toString() },
+    d: function (this: Date) { return formats.d.call(this).toString() },
+    s: function (this: Date) { return formats.s.call(this).toString() },
+    sss: function (this: Date) { return formats.sss.call(this).toString() },
+    y: function (this: Date)
+    {
+        const y = formats.y.call(this);
+        if (y >= 2000)
+            return (y - 2000).toString();
+        return (y - 1900).toString();
+    },
+
+    hh: function (this: Date) { return formats.h.call(this).toString().padStart(2, '0') },
+    mm: function (this: Date) { return formats.m.call(this).toString().padStart(2, '0') },
+    MM: function (this: Date) { return formats.M.call(this).toString().padStart(2, '0') },
+    dd: function (this: Date) { return formats.d.call(this).toString().padStart(2, '0') },
+    ss: function (this: Date) { return formats.s.call(this).toString().padStart(2, '0') },
+    yy: function (this: Date)
+    {
+        return formats.y.call(this).toString().substring(2);
+    },
+    MMM: function (this: Date) { return new Intl.DateTimeFormat().formatToParts(this).find(x => x.type == "month").value },
+    ddd: function (this: Date) { return new Intl.DateTimeFormat().formatToParts(this).find(x => x.type == "weekday").value },
+    yyy: function (this: Date)
+    {
+        return formats.y.call(this).toString();
+    },
+    yyyy: function (this: Date)
+    {
+        return formats.y.call(this).toString();
+    },
+    char: function (s: string)
+    {
+        return function (this: Date)
+        {
+            return s;
+        }
+    }
+}
+
+function readUpToNDigits(s: StringCursor, n: number)
+{
+
+    let value = s.char;
+    for (; n >= 0; n--)
+    {
+        s.offset++;
+        if (!isNaN(Number(s.char)))
+            value += s.char;
+        else
+            break;
+    }
+    const result = Number(value);
+    if (isNaN(result))
+        throw new ErrorWithStatus(HttpStatusCode.BadRequest, 'The provided string does not match the expected format');
+    return result;
+}
+
+const formatsParser = {
+    h: function (s: StringCursor, date: Date) { date.setUTCHours(readUpToNDigits(s, 2)) },
+    m: function (s: StringCursor, date: Date) { date.setUTCMinutes(readUpToNDigits(s, 2)) },
+    M: function (s: StringCursor, date: Date) { date.setUTCMonth(readUpToNDigits(s, 2) - 1) },
+    d: function (s: StringCursor, date: Date) { date.setUTCDate(readUpToNDigits(s, 2)) },
+    s: function (s: StringCursor, date: Date) { date.setUTCSeconds(readUpToNDigits(s, 2)) },
+    sss: function (s: StringCursor, date: Date) { date.setUTCMilliseconds(readUpToNDigits(s, 8)) },
+    y: function (s: StringCursor, date: Date) { const n = readUpToNDigits(s, 2); n >= 50 ? date.setUTCFullYear(n + 1900) : date.setUTCFullYear(n + 2000); },
+    hh: function (s: StringCursor, date: Date) { date.setUTCHours(readUpToNDigits(s, 2)) },
+    mm: function (s: StringCursor, date: Date) { date.setUTCMinutes(readUpToNDigits(s, 2)) },
+    MM: function (s: StringCursor, date: Date) { date.setUTCMonth(readUpToNDigits(s, 2) - 1) },
+    dd: function (s: StringCursor, date: Date) { date.setUTCDate(readUpToNDigits(s, 2)) },
+    ss: function (s: StringCursor, date: Date) { date.setUTCSeconds(readUpToNDigits(s, 2)) },
+    yy: function (s: StringCursor, date: Date) { const n = readUpToNDigits(s, 2); n >= 50 ? date.setUTCFullYear(n + 1900) : date.setUTCFullYear(n + 2000); },
+    yyy: function (s: StringCursor, date: Date) { date.setUTCFullYear(readUpToNDigits(s, 4)) },
+    yyyy: function (s: StringCursor, date: Date) { date.setUTCFullYear(readUpToNDigits(s, 4)) },
+    skip: function (s: StringCursor, date: Date) { s.offset++; }
 }
 
 /**
@@ -31,6 +115,26 @@ export function formatParser(format: string)
 {
     if (!format)
         format = 'yyyy-MM-dd';
+
+    const init = lazy(() =>
+    {
+        const formatters: ((this: Date) => string)[] = [];
+        const parsers: ((s: StringCursor, date: Date) => void)[] = [];
+        let offset = 0;
+        for (let i = offset; i < format.length; i++)
+        {
+            if (i > 0 && format[i - 1] != format[i] || i == format.length - 1)
+            {
+                const f = format.substring(offset, i)
+                formatters.push(formatsFormatter[f] ?? formatsFormatter.char(f));
+                parsers.push(formatsParser[f] ?? formatsParser.skip);
+                offset = i;
+            }
+        }
+        return { formatters, parsers };
+    });
+
+
     return {
         /**
          * Formats a Date object into a string using the configured format.
@@ -40,48 +144,7 @@ export function formatParser(format: string)
          */
         format(value: Date): string
         {
-            let result = '';
-            let offset = 0;
-            let currentFormat: string;
-            for (let i = offset; i < format.length; i++)
-            {
-                if (i === offset)
-                    currentFormat = format[i];
-
-                if (currentFormat === format[i])
-                    continue;
-
-                if (currentFormat in formats)
-                {
-                    let tmp = formats[currentFormat].call(value);
-                    switch (i - offset)
-                    {
-                        case 1:
-                            if (currentFormat === 'y')
-                                tmp %= 100;
-                            result += tmp;
-                            break;
-                        case 2:
-                            if (currentFormat === 'y')
-                                tmp %= 100;
-                            result += tmp.toString().padStart(2, '0');
-                            break;
-                        case 4:
-                            if (currentFormat !== 'y')
-                                throw new Error(`Unsupported format ${format.substring(0, i)}`);
-                            result += tmp;
-                            break;
-                        default:
-                            throw new Error(`Unsupported format ${format.substring(0, i)}`);
-                    }
-                }
-                else
-                {
-                    result += format.substring(offset, i);
-                }
-                offset = i + 1;
-            }
-            return result;
+            return init().formatters.reduce((previous, current) => previous + current.call(value), '')
         },
 
         /**
@@ -93,73 +156,11 @@ export function formatParser(format: string)
          */
         parse(value: string): Date
         {
-            const result = new Date(0, 0, 0, 0, 0, 0, 0);
-            let formatOffset = 0;
-            let valueOffset = 0;
-            let currentFormat: string;
-            for (let i = formatOffset; i <= format.length; i++)
+            const result = new Date(Date.UTC(0, 0, 0, 0, 0, 0, 0));
+            if (typeof value == 'string' && value)
             {
-                if (i === formatOffset)
-                    currentFormat = format[i];
-
-                if (currentFormat === format[i] && currentFormat in formats)
-                    continue;
-
-                if (currentFormat in formats)
-                {
-                    let tmp: number;
-                    if (i == format.length)
-                        tmp = Number(value.substring(valueOffset));
-                    else
-                    {
-                        tmp = Number(value.substring(valueOffset, value.indexOf(format[i], valueOffset)));
-                        valueOffset = value.indexOf(format[i], valueOffset) + 1;
-                    }
-                    if (isNaN(tmp))
-                        throw new Error(`Invalid date value: ${value} (${value.substring(valueOffset, i)}) in format ${format}`);
-
-                    if (currentFormat === 'M')
-                        tmp--;
-
-                    switch (i - formatOffset)
-                    {
-                        case 1:
-                        case 2:
-                        case 4:
-                            if (currentFormat === 'y' && tmp < 100)
-                                if (tmp < 75)
-                                    tmp = tmp + 2000;
-                                else
-                                    tmp = tmp + 1900;
-
-                            switch (currentFormat)
-                            {
-                                case 'h':
-                                    result.setHours(tmp);
-                                    break;
-                                case 'm':
-                                    result.setMinutes(tmp);
-                                    break;
-                                case 'y':
-                                    result.setFullYear(tmp);
-                                    break;
-                                case 'M':
-                                    result.setMonth(tmp);
-                                    break;
-                                case 'd':
-                                    result.setDate(tmp);
-                                    break;
-                                case 's':
-                                    result.setSeconds(tmp);
-                                    break;
-                            }
-                            break;
-                        case 3:
-                        default:
-                            throw new Error(`Unsupported format ${format.substring(0, i)}`);
-                    }
-                }
-                formatOffset = i + 1;
+                const s = new StringCursor(value);
+                init().parsers.forEach(current => current(s, result))
             }
             return result;
         }
