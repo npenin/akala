f4cc614cf011579aabe10d7d9614afd4e8ecce70
Nicolas <github@dragon-angel.fr>
2025-05-09 20:48:45 +0200

    fix: update after core breaking change

--- packages/server/src/translator.ts
+++ packages/server/src/translator.ts
@@ -3,28 +3,31 @@ import * as path from 'path';
 
 defaultInjector.registerFactory('$translator', async function (): Promise<Translator>
 {
-    const language = defaultInjector.resolve('$language');
+    const language = defaultInjector.resolve<string | Intl.Locale>('$language');
     let translations: { [key: string]: string };
     if (language)
         translations = await import(path.join(__dirname, 'i18n.' + defaultInjector.resolve<string>('$language') + '.json'));
     else
         translations = {};
 
-    return function (key: string | { key: string, fallback: string }, ...parameters: unknown[])
-    {
-        let fallback: string;
-        if (typeof key == 'object')
+    return {
+        locale: language,
+        translate: function (key: string | { key: string, fallback: string }, ...parameters: unknown[])
         {
-            fallback = key.fallback;
-            key = key.key;
+            let fallback: string;
+            if (typeof key == 'object')
+            {
+                fallback = key.fallback;
+                key = key.key;
+            }
+            else
+                fallback = key;
+            if (!parameters)
+                return translations[key] || fallback;
+            return (translations[key] || fallback).replace(/\{\d+\}/g, function (m)
+            {
+                return parameters[m];
+            })
         }
-        else
-            fallback = key;
-        if (!parameters)
-            return translations[key] || fallback;
-        return (translations[key] || fallback).replace(/\{\d+\}/g, function (m)
-        {
-            return parameters[m];
-        })
     }
 }, true);
