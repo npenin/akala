4da63aa79855953592f233fdae42e56b4953cd64
Nicolas <github@dragon-angel.fr>
2025-05-21 07:40:50 +0200

    fix: npm fs handler using import.meta.resolve

--- packages/fs/src/index.ts
+++ packages/fs/src/index.ts
@@ -1,8 +1,5 @@
 import { FSFileSystemProvider } from './fs.js';
 import fsHandler from './index.browser.js';
-import { createRequire } from 'module'
-import path from 'path'
-import { pathToFileURL } from 'url';
 
 export * from './index.browser.js';
 
@@ -11,9 +8,9 @@ fsHandler.useProtocol('npm', async url =>
 {
     const fragments = url.pathname.split('/');
     const packageName = fragments[1][0] == '@' ? fragments.slice(1, 3).join('/') : fragments[1];
-    const resolvedUrl = createRequire(path.resolve('.')).resolve(url.pathname.substring(1));
+    const resolvedUrl = import.meta.resolve(url.pathname.substring(1));
     const root = resolvedUrl.substring(0, resolvedUrl.length - url.pathname.length + packageName.length + 2);
-    const fs = new FSFileSystemProvider(pathToFileURL(root), true);
+    const fs = new FSFileSystemProvider(new URL(root), true);
     fs.resolvePath = path =>
     {
         if (URL.canParse(path))
