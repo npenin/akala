e96ea22126b491ce1031e49ad26a69647b0a4756
Nicolas <github@dragon-angel.fr>
2025-06-20 06:54:29 +0200

    fix: improve discover using @akala/fs refactor: revisit CLI usage for discover

--- packages/pm/src/cli-commands/discover.ts
+++ packages/pm/src/cli-commands/discover.ts
@@ -1,9 +1,7 @@
-import { join, isAbsolute, basename, resolve } from "path";
-import { existsSync, promises as fs } from "fs";
 import pmContainer from '../container.js';
 import map from './map.js'
-import { createRequire } from "module";
 import { logger } from "@akala/core";
+import fsHandler, { OpenFlags } from "@akala/fs";
 
 
 type Unpromise<T> = T extends Promise<infer X> ? X : never;
@@ -12,55 +10,14 @@ type mapReturn = Unpromise<ReturnType<typeof map>>;
 const log = logger('discover');
 
 
-export default async function discover(packageName: string, folder: string, pm: pmContainer.container): Promise<mapReturn[] | mapReturn>
+export default async function discover(path: string | URL, pm: pmContainer.container, name?: string): Promise<mapReturn[] | mapReturn>
 {
     // eslint-disable-next-line prefer-rest-params
     log.debug(arguments);
 
-    let path = folder || process.cwd();
+    const moduleFs = await fsHandler.process(new URL(path));
 
-    log.debug(path);
-
-    let tmpRequire: ReturnType<typeof createRequire> | undefined = undefined;
-    if (existsSync(path))
-        if (isAbsolute(packageName))
-        {
-            const stats = await fs.stat(packageName)
-            if (stats.isFile())
-                tmpRequire = createRequire(packageName);
-            else
-                tmpRequire = createRequire(join(packageName, './package.json'));
-            packageName = basename(packageName);
-        }
-        else
-        {
-            path = resolve(path);
-            const stats = await fs.stat(join(path, packageName))
-            if (stats.isFile())
-                tmpRequire = createRequire(join(path, packageName));
-            else
-                tmpRequire = createRequire(join(path, packageName, './package.json'));
-        }
-
-    if (typeof tmpRequire == 'undefined')
-        tmpRequire = createRequire(join(path, './package.json'));
-
-    function tryModuleRequireResolve(p: string)
-    {
-        try
-        {
-            return moduleRequire?.resolve(p)
-        }
-        catch (e)
-        {
-            return null;
-        }
-    }
-
-    const moduleRequire = tmpRequire;
-
-    const packageConfig = moduleRequire('./package.json');
-    delete moduleRequire.cache[moduleRequire.resolve('./package.json')];
+    const packageConfig = await moduleFs.readFile<any>('./package.json', { encoding: 'json' })
 
     if (packageConfig.commands)
     {
@@ -70,9 +27,9 @@ export default async function discover(packageName: string, folder: string, pm: 
                 if (Array.isArray(packageConfig))
                     throw new Error('commands property must be of type object or string');
 
-                return await Promise.all(Object.keys(packageConfig.commands).map(v => pm.dispatch('map', v, moduleRequire?.resolve('./' + packageConfig.commands[v]), 'nodejs', path, { commandable: true })));
+                return await Promise.all(Object.keys(packageConfig.commands).map(v => pm.dispatch('map', v, new URL(packageConfig.commands[v], moduleFs.root), 'nodejs', null, { commandable: true })));
             case 'string':
-                return await pm.dispatch('map', packageName, moduleRequire.resolve('./' + packageConfig.commands), 'nodejs', path, { commandable: true });
+                return await pm.dispatch('map', name ?? packageConfig.name, new URL(packageConfig.commands, moduleFs.root), 'nodejs', null, { commandable: true });
             default:
                 throw new Error('commands property must be of type object or string');
         }
@@ -85,17 +42,17 @@ export default async function discover(packageName: string, folder: string, pm: 
             case 'object':
                 if (Array.isArray(packageConfig))
                     throw new Error('bin property must be of type object or string');
-                return Promise.all(Object.keys(packageConfig.bin).map(v => pm.dispatch('map', v, moduleRequire.resolve('./' + packageConfig.bin[v]), 'nodejs', path, { commandable: false })));
+                return Promise.all(Object.keys(packageConfig.bin).map(v => pm.dispatch('map', v, new URL(packageConfig.bin[v], moduleFs.root), 'nodejs', null, { commandable: false })));
             case 'string':
-                return await pm.dispatch('map', packageName, moduleRequire.resolve('./' + packageConfig.bin), 'nodejs', path, { commandable: false });
+                return await pm.dispatch('map', name ?? packageConfig.name, new URL('./' + packageConfig.bin, moduleFs.root), 'nodejs', null, { commandable: false });
             default:
                 throw new Error('bin property must be of type object or string');
         }
     }
 
-    const commandsJsonFile = tryModuleRequireResolve('./commands.json');
+    const commandsJsonFile = await moduleFs.access('./commands.json', OpenFlags.Read).then(() => true, () => false);
     if (commandsJsonFile)
-        return pm.dispatch('map', packageName, commandsJsonFile, 'nodejs', path, { commandable: true });
+        return pm.dispatch('map', name ?? packageConfig.name, new URL('./commands.json', moduleFs.root), 'nodejs', null, { commandable: true });
 
-    return pm.dispatch('map', packageName, moduleRequire.resolve('./' + packageConfig.main), 'nodejs', path, { commandable: false });
+    return pm.dispatch('map', name ?? packageConfig.name, new URL('./' + packageConfig.main, moduleFs.root), 'nodejs', null, { commandable: false });
 }
