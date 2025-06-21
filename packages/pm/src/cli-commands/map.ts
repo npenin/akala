e732371fc99ccdbef1d793e8da2a20fc8d1b0c65
Nicolas <github@dragon-angel.fr>
2025-06-21 06:02:40 +0200

    fix: redefine discover and map commands

--- packages/pm/src/cli-commands/map.ts
+++ packages/pm/src/cli-commands/map.ts
@@ -1,11 +1,11 @@
 import State, { SidecarMetadata } from '../state.js';
 import { isAbsolute, resolve } from "path";
 
-export default async function map<TName extends string>(this: State, name: TName, targetPath: string | URL, runtime: SidecarMetadata['type'], cwd?: string, options?: { commandable?: boolean })
+export default async function map<TName extends string>(config: State['config'], name: TName, targetPath: string | URL, runtime: SidecarMetadata['type'], cwd?: string, options?: { commandable?: boolean })
 {
     if (typeof targetPath == 'string' && !URL.canParse(targetPath) && !isAbsolute(targetPath))
         targetPath = resolve(cwd || process.cwd(), targetPath);
-    this.config.containers.set(name, { type: runtime, path: targetPath.toString(), commandable: !!options?.commandable });
-    await this.config.commit();
+    config.containers.set(name, { type: runtime, path: targetPath.toString(), commandable: !!options?.commandable });
+    await config.commit();
     return name
 }
