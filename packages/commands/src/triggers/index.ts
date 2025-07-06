042d4609cffa7ca019bcf3d83766820c77bb7d28
Nicolas <github@dragon-angel.fr>
2025-07-06 07:19:57 +0200

    feat: add pubsub triggers

--- packages/commands/src/triggers/index.ts
+++ packages/commands/src/triggers/index.ts
@@ -1,3 +1,5 @@
 import { processTrigger as cli, addOptions as addCliOptions, registerCommand } from './cli.js'
 
 export { cli, addCliOptions, registerCommand }
+
+export { asyncTrigger as pubsubAsync, syncTrigger as pubsubSync } from './pubsub.js'
---
+++ packages/commands/src/triggers/pubsub.ts
@@ -0,0 +1,59 @@
+import * as Metadata from '../metadata/index.js'
+import { Trigger } from '../model/trigger.js';
+import { AsyncEventBus, EventBus } from '@akala/core';
+import { Container } from '../model/container.js'
+
+declare module '@akala/commands'
+{
+    export interface ConfigurationMap
+    {
+        pubsub: PubSubConfiguration;
+    }
+}
+
+export interface PubSubConfiguration extends Metadata.Configuration
+{
+    topic: string;
+}
+
+
+export function registerCommand<TState>(cmd: Metadata.Command, c: Container<TState>, pubsub: EventBus)
+{
+    if (cmd.config?.pubsub)
+    {
+        pubsub.on(cmd.config.pubsub.topic, args =>
+        {
+            return c.handle(c, cmd.name, {
+                params: args,
+                _trigger: 'pubsub'
+            });
+        });
+    }
+}
+
+export async function registerAsyncCommand<TState>(cmd: Metadata.Command, c: Container<TState>, pubsub: AsyncEventBus)
+{
+    if (cmd.config?.pubsub)
+    {
+        await pubsub.on(cmd.config.pubsub.topic, args =>
+        {
+            return c.handle(c, cmd.name, {
+                params: args,
+                _trigger: 'pubsub'
+            });
+        });
+    }
+}
+
+export const asyncTrigger = new Trigger('pubsub-async', async (c, pubsub: AsyncEventBus) =>
+{
+    const meta: Metadata.Container = await c.dispatch('$metadata', true);
+    return Promise.all([...meta.commands, c.resolve('$metadata')].map(cmd => registerAsyncCommand(cmd, c, pubsub)));
+});
+
+
+export const syncTrigger = new Trigger('pubsub', async (c, pubsub: EventBus) =>
+{
+    const meta: Metadata.Container = await c.dispatch('$metadata', true);
+    return [...meta.commands, c.resolve('$metadata')].forEach(cmd => registerCommand(cmd, c, pubsub));
+});
