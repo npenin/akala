f9e02c340bb3089177a2d554dc115ed756f69f6c
Nicolas <github@dragon-angel.fr>
2025-08-14 02:34:15 +0200

    feat: expose expression visitors

--- packages/core/src/parser/expressions/visitors/index.ts
+++ packages/core/src/parser/expressions/visitors/index.ts
@@ -1,3 +1,4 @@
 export * from './expression-visitor.js'
-// export * from './expression-comparer.js'
-// export * from './expression-simplifyer.js'
+export * from './expression-comparer.js'
+export * from './expression-simplifyer.js'
+export * from './expression-updater.js'
