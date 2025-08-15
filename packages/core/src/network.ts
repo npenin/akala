46e922334d325ccfb5b6c0d3fbfaedf9db46c3a6
Nicolas <github@dragon-angel.fr>
2025-08-15 21:58:06 +0200

    feat: implement WebSocketAdapter and move SocketAdapter interfaces to network module

---
+++ packages/core/src/network.ts
@@ -0,0 +1,21 @@
+import type { EventBus, SpecialEvents } from "./events/event-bus.js";
+import type { IEvent } from "./events/shared.js";
+import type { IsomorphicBuffer } from "./helpers.js";
+
+export interface SocketAdapterEventMap
+{
+    message: string | IsomorphicBuffer;
+    open: Event;
+    error: Event;
+    close: CloseEvent;
+}
+
+export type SocketAdapterAkalaEventMap = { [key in keyof SocketAdapterEventMap]: IEvent<[SocketAdapterEventMap[key]], void> }
+
+export interface SocketAdapter extends EventBus<SocketAdapterAkalaEventMap & Partial<SpecialEvents>>
+{
+    readonly open: boolean;
+    close(): void;
+    send(data: string | IsomorphicBuffer): void;
+    pipe(socket: SocketAdapter): void;
+}
---
+++ packages/core/src/network.websocket.ts
@@ -0,0 +1,124 @@
+import type { AllEventKeys } from "./events/event-bus.js";
+import { EventEmitter, type AllEvents } from "./events/event-emitter.js";
+import type { EventListener, EventOptions } from "./events/shared.js";
+import { SocketAdapterAkalaEventMap, SocketAdapter } from "./network.js";
+import { type Subscription, StatefulSubscription } from "./teardown-manager.js";
+
+/**
+ * json-rpc-ws connection
+ *
+ * @constructor
+ * @param {Socket} socket - web socket for this connection
+ * @param {Object} parent - parent that controls this connection
+ */
+
+
+export class WebSocketAdapter extends EventEmitter<SocketAdapterAkalaEventMap> implements SocketAdapter
+{
+    constructor(private readonly socket: WebSocket)
+    {
+        super();
+    }
+
+    pipe(socket: SocketAdapter)
+    {
+        this.on('message', (message) => socket.send(message));
+        this.on('close', () => socket.close());
+    }
+
+    get open(): boolean
+    {
+        return this.socket.readyState == WebSocket.OPEN;
+    }
+
+    close(): void
+    {
+        this.socket.close();
+    }
+
+    send(data: string): void
+    {
+        this.socket.send(data);
+    }
+
+    private readonly messageListeners: [(ev: unknown) => void, (ev: unknown) => void][] = [];
+
+    public off<const TEvent extends AllEventKeys<SocketAdapterAkalaEventMap>>(
+        event: TEvent,
+        handler: EventListener<AllEvents<SocketAdapterAkalaEventMap>[TEvent]>
+    ): boolean
+    {
+        switch (event)
+        {
+            case 'message':
+                {
+                    let listeners = this.messageListeners;
+                    if (handler)
+                        listeners = listeners.filter(f => f[0] == handler);
+                    listeners.forEach(l => this.socket.removeEventListener('message', l[1]));
+                }
+                break;
+            case 'close':
+            case 'error':
+            case 'open':
+                //eslint-disable-next-line @typescript-eslint/no-explicit-any
+                this.socket.removeEventListener(event, handler as any);
+                break;
+            default:
+                throw new Error(`Unsupported event ${String(event)}`);
+        }
+        return true;
+    }
+
+    public on<const TEvent extends AllEventKeys<SocketAdapterAkalaEventMap>>(
+        event: TEvent,
+        handler: EventListener<AllEvents<SocketAdapterAkalaEventMap>[TEvent]>,
+        options?: EventOptions<AllEvents<SocketAdapterAkalaEventMap>[TEvent]>
+    ): Subscription
+    {
+        switch (event)
+        {
+            case 'message':
+                {
+                    const x = function (ev) { return handler.call(this, ev.data); };
+                    this.messageListeners.push([handler, x]);
+                    this.socket.addEventListener('message', x, options);
+                    return new StatefulSubscription(() =>
+                    {
+                        this.messageListeners.splice(this.messageListeners.findIndex(x => x[0] === handler), 1);
+                        this.socket.removeEventListener('message', x);
+                    }).unsubscribe;
+                }
+
+            case 'close':
+            case 'error':
+            case 'open':
+                //eslint-disable-next-line @typescript-eslint/no-explicit-any
+                this.socket.addEventListener(event, handler as any);
+                return new StatefulSubscription(() =>
+                {
+                    this.socket.removeEventListener(event, handler as any);
+                }).unsubscribe;
+            default:
+                throw new Error(`Unsupported event ${String(event)}`);
+        }
+    }
+
+    public once<const TEvent extends AllEventKeys<SocketAdapterAkalaEventMap>>(
+        event: TEvent,
+        handler: EventListener<AllEvents<SocketAdapterAkalaEventMap>[TEvent]>
+    ): Subscription
+    {
+        switch (event)
+        {
+            case 'message':
+                return this.on(event, handler, { once: true } as EventOptions<AllEvents<SocketAdapterAkalaEventMap>[TEvent]>);
+            case 'close':
+            case 'error':
+            case 'open':
+                return this.on(event, handler, { once: true } as EventOptions<AllEvents<SocketAdapterAkalaEventMap>[TEvent]>);
+            default:
+                throw new Error(`Unsupported event ${event?.toString()}`);
+        }
+    }
+}
