ec78e774b8e6fcb0ab275044dcc36d9a285f6dc7
Nicolas <github@dragon-angel.fr>
2025-08-16 08:16:53 +0200

    feat: add jsonnd socket adapter

--- packages/json-rpc-ws/src/ws/websocket.ts
+++ packages/json-rpc-ws/src/ws/websocket.ts
@@ -1,9 +1,30 @@
 'use strict';
 
 import { default as ClientBase } from './shared-client.js';
-import { WebSocketAdapter, type SocketAdapter } from '@akala/core';
+import type { SocketAdapter } from '@akala/core';
 import { Connection } from '../browser.js'
+import { IsomorphicBuffer, SocketProtocolAdapter, WebSocketAdapter } from '@akala/core';
 import debug from 'debug';
+
+/**
+ * json-rpc-ws connection
+ *
+ * @constructor
+ * @param {Socket} socket - web socket for this connection
+ * @param {Object} parent - parent that controls this connection
+ */
+
+export class JsonNDRpcSocketAdapter<T> extends SocketProtocolAdapter<T> implements SocketAdapter<T>
+{
+    constructor(socket: SocketAdapter)
+    {
+        super({
+            receive: (data: string | IsomorphicBuffer) => JSON.parse((data instanceof IsomorphicBuffer ? data.toString('utf8') : data)),
+            send: (data: T) => JSON.stringify(data) + '\n',
+        }, socket);
+    }
+
+}
 
 export default class Client extends ClientBase<ReadableStream, { protocols?: string | string[] }>
 {
@@ -19,7 +40,7 @@ export default class Client extends ClientBase<ReadableStream, { protocols?: str
 
     public static connect(address: string, options?: { protocols?: string | string[] }): SocketAdapter
     {
-        return new WebSocketAdapter(new WebSocket(address.replace(/^http/, 'ws'), options?.protocols));
+        return new JsonNDRpcSocketAdapter(new WebSocketAdapter(new WebSocket(address.replace(/^http/, 'ws'), options?.protocols)));
     }
 }
 
