a69088b1163bf8e3c61b8052baa002796e61aa21
Nicolas <github@dragon-angel.fr>
2025-08-17 10:02:51 +0200

    refactor: streamline install function by removing npm and yarn helper logic

--- packages/pm/src/cli-commands/install.ts
+++ packages/pm/src/cli-commands/install.ts
@@ -1,28 +1,20 @@
 import type State from '../state.js';
-import npmHelper from '@akala/cli/npm-helper';
-import yarnHelper, { hasYarn } from '@akala/cli/yarn-helper';
 import { Container } from "@akala/commands";
 import discover from './discover.js';
+import { xpm } from '@akala/cli';
 
 export default async function install(this: State, packageName: string, pm: Container<State>): ReturnType<typeof discover>
 {
-    if (process.versions['pnp'] || await hasYarn())
-    {
-        await yarnHelper.install(packageName);
-        if (!this.config.has('setup'))
-            this.config.set('setup', { packages: [] })
-        if (!this.config.has('setup.packages'))
-            this.config.set('setup.packages', [])
+    const xPM = await xpm(process.cwd());
+    await xPM.install(packageName);
 
-        if (this.config.setup?.packages?.indexOf(packageName) == -1)
-            this.config.setup.packages.push(packageName);
-        return await pm.dispatch('discover', packageName, !process.versions['pnp'] && 'node_modules' || undefined);
-    }
-    else
-    {
-        await npmHelper.install(packageName);
-        if (this.config.setup.packages.indexOf(packageName) == -1)
-            this.config.setup.packages.push(packageName);
-        return await pm.dispatch('discover', packageName, 'node_modules');
-    }
+    if (!this.config.has('setup'))
+        this.config.set('setup', { packages: [] })
+    if (!this.config.has('setup.packages'))
+        this.config.set('setup.packages', [])
+
+    if (this.config.setup?.packages?.indexOf(packageName) == -1)
+        this.config.setup.packages.push(packageName);
+
+    return await pm.dispatch('discover', packageName, !process.versions['pnp'] && 'node_modules' || undefined);
 }
