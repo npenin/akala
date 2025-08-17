eba770b81e9a7854418ad519d379470cbd747191
Nicolas <github@dragon-angel.fr>
2025-08-17 10:03:04 +0200

    fix: ensure safe access to setup packages in install function

--- packages/pm/src/cli-commands/install.ts
+++ packages/pm/src/cli-commands/install.ts
@@ -13,7 +13,7 @@ export default async function install(this: State, packageName: string, pm: Cont
     if (!this.config.has('setup.packages'))
         this.config.set('setup.packages', [])
 
-    if (this.config.setup?.packages?.indexOf(packageName) == -1)
+    if (this.config.setup.packages.indexOf(packageName) == -1)
         this.config.setup.packages.push(packageName);
 
     return await pm.dispatch('discover', packageName, !process.versions['pnp'] && 'node_modules' || undefined);
