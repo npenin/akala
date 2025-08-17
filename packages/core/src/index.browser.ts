79e83bdf74e96fe54a8095dbc69d32e79630314d
Nicolas <github@dragon-angel.fr>
2025-08-17 15:04:55 +0200

    feat: add TcpSocketAdapter for TCP socket handling and update package exports

---
+++ packages/core/src/index.browser.ts
@@ -0,0 +1,65 @@
+
+export * from './injectors/shared.js';
+export * from './injectors/simple-injector.js';
+export * from './injectors/typed-injector.js';
+export * from './injectors/reflection-injector.js';
+export * from './injectors/middleware-injector.js';
+export * from './injectors/expression-injector.js';
+export * from './middlewares/shared.js';
+export * from './middlewares/composite-async.js';
+export * from './middlewares/composite-sync.js';
+export * from './middlewares/composite-with-priority-async.js';
+export * from './middlewares/composite-with-priority-sync.js';
+export * from './middlewares/indexed-async.js';
+export * from './middlewares/indexed-sync.js';
+export * from './teardown-manager.js';
+// export * from './global-injector';
+export * from './factory.js';
+export * from './http.js';
+export * from './service.js';
+import sequencify, { type Task, type Tasks } from './sequencify.js';
+export { sequencify, type Task, type Tasks }
+// export { Bound, PossiblyBound } from './binder.js';
+export * from './events/index.js';
+export * from './parser/parser.js';
+export { default as Sort, type SortDirection, ParserFormatter } from './parser/formatters.js'
+export type * from './type-helper.js'
+export * from './helpers.js'
+export * from './network.js'
+export * from './network.websocket.js'
+export * from './router/index.js'
+export * from './queue.js'
+import * as base64 from './base64.js'
+export { base64 };
+
+import * as packagejson from './package.js'
+export { packagejson };
+
+export * from './observables/shared.js'
+export * from './observables/array.js'
+export * from './observables/object.js'
+
+export * from './module.js';
+import * as expressions from './parser/expressions/index.js';
+export { expressions };
+import * as parser from './parser/evaluator-as-function.js';
+export { parser };
+export * from './promiseHelpers.js';
+export { each as eachAsync, type NextFunction, map as mapAsync, grep as grepAsync, AggregateErrors } from './eachAsync.js';
+export { each, grep, map, type Proxy } from './each.js';
+export * from './distinct.js'
+export * from './interpolate.js';
+import * as introspect from './reflect.js';
+export { introspect };
+export * from './chain.js';
+export * from './polymorph.js';
+export * from './logger.js'
+export * from './errorWithStatus.js'
+
+export * from './formatters/index.js'
+
+export * from './url-handler.js'
+import * as UrlTemplate from './uri-template/index.js'
+export { UrlTemplate };
+
+export * from './case-helpers.js'
---
+++ packages/core/src/network.tcp.ts
@@ -0,0 +1,132 @@
+import type { AllEventKeys } from "./events/event-bus.js";
+import { EventEmitter, type AllEvents } from "./events/event-emitter.js";
+import type { EventListener, EventOptions } from "./events/shared.js";
+import { IsomorphicBuffer } from "./helpers.js";
+import { SocketAdapterAkalaEventMap, SocketAdapter } from "./network.js";
+import { type Subscription, StatefulSubscription } from "./teardown-manager.js";
+import { Socket } from 'net';
+
+export class TcpSocketAdapter extends EventEmitter<SocketAdapterAkalaEventMap> implements SocketAdapter
+{
+    constructor(private readonly socket: Socket)
+    {
+        super();
+    }
+
+    pipe(socket: SocketAdapter)
+    {
+        this.on('message', (message) => socket.send(message));
+        this.on('close', () => socket.close());
+    }
+
+    get open(): boolean
+    {
+        return !this.socket.closed
+    }
+
+    close(): Promise<void>
+    {
+        return new Promise(resolve => this.socket.end(resolve));
+    }
+
+    send(data: string | IsomorphicBuffer): Promise<void>
+    {
+        return new Promise((resolve, reject) => this.socket.write(data instanceof IsomorphicBuffer ? data.toArray() : data, err => err ? reject(err) : resolve()));
+    }
+
+    private readonly messageListeners: [(ev: unknown) => void, (ev: unknown) => void][] = [];
+
+    public off<const TEvent extends AllEventKeys<SocketAdapterAkalaEventMap>>(
+        event: TEvent,
+        handler: EventListener<AllEvents<SocketAdapterAkalaEventMap>[TEvent]>
+    ): boolean
+    {
+        switch (event)
+        {
+            case 'message':
+                {
+                    let listeners = this.messageListeners;
+                    if (handler)
+                        listeners = listeners.filter(f => f[0] == handler);
+                    var result = false;
+                    for (const listener of listeners)
+                    {
+                        this.socket.off('message', listener[1]);
+                        result = !!this.messageListeners.splice(this.messageListeners.indexOf(listener), 1)?.length || result;
+                    }
+                }
+                break;
+            case 'close':
+            case 'error':
+            case 'open':
+                //eslint-disable-next-line @typescript-eslint/no-explicit-any
+                this.socket.off(event, handler as any);
+                break;
+            default:
+                throw new Error(`Unsupported event ${String(event)}`);
+        }
+        return true;
+    }
+
+    public on<const TEvent extends AllEventKeys<SocketAdapterAkalaEventMap>>(
+        event: TEvent,
+        handler: EventListener<AllEvents<SocketAdapterAkalaEventMap>[TEvent]>,
+        options?: EventOptions<AllEvents<SocketAdapterAkalaEventMap>[TEvent]>
+    ): Subscription
+    {
+        switch (event)
+        {
+            case 'message':
+                {
+                    const x = function (ev) { return (handler as EventListener<SocketAdapterAkalaEventMap['message']>).call(this, typeof ev.data === 'string' ? ev.data : IsomorphicBuffer.fromArrayBuffer(ev.data)); };
+                    this.messageListeners.push([handler, x]);
+                    if (options?.once)
+                        this.socket.once('data', x);
+                    else
+                        this.socket.on('data', x);
+                    return new StatefulSubscription(() =>
+                    {
+                        this.messageListeners.splice(this.messageListeners.findIndex(x => x[0] === handler), 1);
+                        this.socket.off('message', x);
+                    }).unsubscribe;
+                }
+
+            case 'close':
+            case 'error':
+            case 'open':
+                //eslint-disable-next-line @typescript-eslint/no-explicit-any
+                if (options?.once)
+                    this.socket.once(event, handler);
+                else
+                    this.socket.on(event, handler);
+                return new StatefulSubscription(() =>
+                {
+                    this.socket.off(event, handler);
+                }).unsubscribe;
+            case Symbol.dispose:
+                return super.on(event, handler, options);
+            default:
+                throw new Error(`Unsupported event ${String(event)}`);
+        }
+    }
+
+    public once<const TEvent extends AllEventKeys<SocketAdapterAkalaEventMap>>(
+        event: TEvent,
+        handler: EventListener<AllEvents<SocketAdapterAkalaEventMap>[TEvent]>
+    ): Subscription
+    {
+        switch (event)
+        {
+            case 'message':
+                return this.on(event, handler, { once: true } as EventOptions<AllEvents<SocketAdapterAkalaEventMap>[TEvent]>);
+            case 'close':
+            case 'error':
+            case 'open':
+            case Symbol.dispose:
+                return this.on(event, handler, { once: true } as EventOptions<AllEvents<SocketAdapterAkalaEventMap>[TEvent]>);
+            default:
+                let x: never = event;
+                throw new Error(`Unsupported event ${x}`);
+        }
+    }
+}
