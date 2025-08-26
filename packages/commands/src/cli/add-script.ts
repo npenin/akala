f2049a9983d2b7c73c617f899f3fb49778f14f8d
Nicolas <github@dragon-angel.fr>
2025-08-26 07:32:40 +0200

    style: remove extra comment

--- packages/commands/src/cli/add-script.ts
+++ packages/commands/src/cli/add-script.ts
@@ -7,8 +7,6 @@ export default async function addScript(name?: string, folder?: string, metadata
 {
     folder = folder || '';
 
-    //eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    //@ts-ignore
     //eslint-disable-next-line @typescript-eslint/no-var-requires
     const pkg = (await import(path.join(process.cwd(), './package.json'), { with: { type: 'json' } })).default;
 
