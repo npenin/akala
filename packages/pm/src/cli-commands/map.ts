945218777a28bc70fb71bb408751db6a55955d63
Nicolas <github@dragon-angel.fr>
2025-08-30 06:51:03 +0200

    feat: add depencencies during map

--- packages/pm/src/cli-commands/map.ts
+++ packages/pm/src/cli-commands/map.ts
@@ -1,12 +1,34 @@
+import fsHandler from '@akala/fs';
 import type State from '../state.js';
 import type { SidecarMetadata } from '../state.js';
 import { isAbsolute, resolve } from "path";
+import { pathToFileURL } from 'url';
+import { Metadata } from '@akala/commands';
 
-export default async function map<TName extends string>(config: State['config'], name: TName, targetPath: string | URL, runtime: SidecarMetadata['type'], cwd?: string, options?: { commandable?: boolean })
+export default async function map<TName extends string>(config: State['config'], name: TName, targetPath: string | URL, runtimeName: SidecarMetadata['type'], cwd?: string, options?: { commandable?: boolean })
 {
     if (typeof targetPath == 'string' && !URL.canParse(targetPath) && !isAbsolute(targetPath))
         targetPath = resolve(cwd || process.cwd(), targetPath);
-    config.containers.set(name, { type: runtime, path: targetPath.toString(), commandable: !!options?.commandable });
+
+    let dependencies: string[];
+    if (!!options?.commandable)
+    {
+        const url = targetPath instanceof URL ? targetPath : !URL.canParse(targetPath) ? pathToFileURL(targetPath) : new URL(targetPath);
+
+        const fs = await fsHandler.process(url);
+
+        const container = await fs.readFile<Metadata.Container>(url, { encoding: 'json' });
+
+        if (container.dependencies)
+            dependencies = container.dependencies;
+    }
+
+    config.containers.set(name, {
+        type: runtimeName,
+        path: targetPath.toString(),
+        commandable: !!options?.commandable,
+        dependencies
+    });
     await config.commit();
     return name
 }
