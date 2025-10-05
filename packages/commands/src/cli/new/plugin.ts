d81be7e4d4627927597c288110d5a195dc74a25d
Nicolas <github@dragon-angel.fr>
2025-10-05 22:46:05 +0200

    style: remove extra line breaks

--- packages/commands/src/cli/new/plugin.ts
+++ packages/commands/src/cli/new/plugin.ts
@@ -32,8 +32,6 @@ export default function (config: Configuration, program: NamespaceMiddleware)
             if (pkg.exports)
                 pkg.exports['./akala'] = target;
             else
-
-
                 await pkgHandle.writeFile(JSON.stringify(pkg, null, 4));
         }
     }
